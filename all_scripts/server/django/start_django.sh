727de09d2688230df878345ad4f63cd282f4b54b
utarsuno <utarsuno@gmail.com>
2017-08-20 16:19:11 -0500

    Script updates for running and stopping Django.

---
+++ all_scripts/server/django/start_django.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /home/git_repos/quasar_source/all_scripts/universal_scripts;
+source universal_functions.sh;
+
+# Helps see exactly where the script's output starts.
+print_dashed_line_with_text "start_django start"
+
+#  __        ___  ___ ___         __        ___  __        __      ___    __   __  ___
+# /__`  /\  |__  |__   |  \ /    /  ` |__| |__  /  ` |__/ /__`    |__  | |__) /__`  |
+# .__/ /~~\ |    |___  |   |     \__, |  | |___ \__, |  \ .__/    |    | |  \ .__/  |
+
+terminate_if_system_is_not_ubuntu
+
+#  __   __   __     __  ___          __   __     __      __   ___       __
+# /__` /  ` |__) | |__)  |     |    /  \ / _` | /  `    |__) |__  |    /  \ |  |
+# .__/ \__, |  \ | |     |     |___ \__/ \__> | \__,    |__) |___ |___ \__/ |/\|
+
+nohup python3 /home/git_repos/quasar_source/quasar_site/manage.py runserver 0.0.0.0:1337 &
+
+# Helps to see exactly where the script's output ends.
+print_dashed_line_with_text "end_django end for ${HOST_NAME}"
