4ffd47242457261972ab56c0b151219a39677d81
utarsuno <utarsuno@gmail.com>
2017-08-21 14:25:58 -0500

    Django updates 03.

--- quasar_site/quasar_site/urls.py
+++ quasar_site/quasar_site/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+
+from quasar_site.quasar_web_server import views as v
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('quasar_web_server.urls'))
+    url(r'^hello_world$', v.GET_hello_world, name='hello_world')
 ]
