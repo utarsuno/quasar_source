722b7e2a6eb3de3911103cd186aca718e34d53ab
utarsuno <utarsuno@gmail.com>
2017-08-22 12:49:34 -0500

    Trying to render a plane.

--- quasar_source_code/quasar_site/static/js/custom/hello_world.js
+++ quasar_source_code/quasar_site/static/js/custom/hello_world.js
@@ -1,35 +1,45 @@
-var scene = new THREE.Scene();
+'use strict'
+
+/* global THREE */
+
+var scene = new THREE.Scene()
 
 // Variables for documentation purposes. This design will probably eventually change.
-var field_of_view = 90;
-var window_width  = window.innerWidth;
-var window_height = window.innerHeight;
-var aspect_ratio  = window_width / window_height;
-var near_clipping = 0.1;
-var far_clipping  = 1000;
+var field_of_view = 90
+var window_width  = window.innerWidth
+var window_height = window.innerHeight
+var aspect_ratio  = window_width / window_height
+var near_clipping = 0.1
+var far_clipping  = 1000
 
-var camera = new THREE.PerspectiveCamera(field_of_view, aspect_ratio, near_clipping, far_clipping);
+var camera   = new THREE.PerspectiveCamera(field_of_view, aspect_ratio, near_clipping, far_clipping)
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer()
 
-renderer.setSize(window_width, window_height);
+renderer.setSize(window_width, window_height)
 
-document.body.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement)
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry = new THREE.BoxGeometry( 1, 1, 1 )
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+var cube 	 = new THREE.Mesh( geometry, material )
+scene.add(cube)
 
-camera.position.z = 5;
+// Going to try to create a plane here.
+var plane_geometry = new THREE.PlaneGeometry(300, 300)
+var plane_material = new THREE.MeshBasicMaterial({color: 0x0000ff})
+var plane_mesh     = new THREE.Mesh(plane_geometry, plane_material)
+scene.add(plane_mesh)
+
+camera.position.z = 5
 
 var animate = function () {
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate)
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+	cube.rotation.x += 0.1
+	cube.rotation.y += 0.1
 
-	renderer.render(scene, camera);
-};
+	renderer.render(scene, camera)
+}
 
-animate();
+animate()
