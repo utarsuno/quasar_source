55437b03829790a66dc16d5a196132405ee938cb
utarsuno <utarsuno@gmail.com>
2017-08-22 12:56:40 -0500

    Testing stats.

--- quasar_source_code/quasar_site/static/js/custom/hello_world.js
+++ quasar_source_code/quasar_site/static/js/custom/hello_world.js
@@ -1,8 +1,13 @@
 'use strict'
 
-/* global THREE */
+/* global THREE, Stats */
 
 var scene = new THREE.Scene()
+
+// Trying to add stats.
+var stats = new Stats()
+stats.showPanel(0)
+
 
 // Variables for documentation purposes. This design will probably eventually change.
 var field_of_view = 90
@@ -19,13 +24,12 @@ var renderer = new THREE.WebGLRenderer()
 renderer.setSize(window_width, window_height)
 
 document.body.appendChild(renderer.domElement)
+document.body.appendChild(stats.dom)
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 )
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
-var cube 	 = new THREE.Mesh( geometry, material )
+var cube 	 = new THREE.Mesh(geometry, material)
 scene.add(cube)
-
-console.log('JS update test.')
 
 // Going to try to create a plane here.
 var plane_geometry = new THREE.PlaneGeometry(300, 300)
@@ -33,15 +37,19 @@ var plane_material = new THREE.MeshBasicMaterial({color: 0x0000ff})
 var plane_mesh     = new THREE.Mesh(plane_geometry, plane_material)
 scene.add(plane_mesh)
 
-camera.position.z = 5
+camera.position.z = 10
 
 var animate = function () {
 	requestAnimationFrame(animate)
+
+	stats.begin()
 
 	cube.rotation.x += 0.1
 	cube.rotation.y += 0.1
 
 	renderer.render(scene, camera)
+
+	stats.end()
 }
 
 animate()
