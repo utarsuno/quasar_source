7e778a5ff8b4c66971d39de03e0c7417a42f82e8
utarsuno <utarsuno@gmail.com>
2017-08-22 20:53:25 -0500

    quick chat test 10

--- quasar_source_code/quasar_site/quasar_web_server/views.py
+++ quasar_source_code/quasar_site/quasar_web_server/views.py
@@ -42,6 +42,7 @@ def message_POST(request):
 	:param request: Contains information regarding the request sent in.
 	:return: The HttpResponse. Just a server_reply_response_string.
 	"""
+	print(request.POST.get['message'])
 	received_message = request.POST.get['message']
 	print('Got the message : ' + str(received_message))
 	received_ip    = get_client_ip(request)
