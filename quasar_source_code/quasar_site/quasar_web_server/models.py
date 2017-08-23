9dc325d3c1af6987932968d3770d5a9bbc71d03e
utarsuno <utarsuno@gmail.com>
2017-08-22 20:13:09 -0500

    quick chat test 00

---
+++ quasar_source_code/quasar_site/quasar_web_server/models.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+"""This module, modules.py, defines Django database object models."""
+
+from django.db import models
+from django.utils import timezone
+
+'''
+class Message(models.Model):
+    handle    = models.TextField()
+    message   = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+'''
