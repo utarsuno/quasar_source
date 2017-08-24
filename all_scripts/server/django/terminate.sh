a7f6e4fcff346adc04d96064803928aa8ea7d19c
utarsuno <utarsuno@gmail.com>
2017-08-23 19:16:51 -0500

    Script updates 12

--- all_scripts/server/django/terminate.sh
+++ all_scripts/server/django/terminate.sh
@@ -10,13 +10,14 @@ print_dashed_line_with_text "terminate django script start for : ${HOST_NAME}"
 # /__`  /\  |__  |__   |  \ /    /  ` |__| |__  /  ` |__/ /__`    |__  | |__) /__`  |
 # .__/ /~~\ |    |___  |   |     \__, |  | |___ \__, |  \ .__/    |    | |  \ .__/  |
 
+terminate_if_not_sudo
 terminate_if_system_is_not_ubuntu
 
 #  __   __   __     __  ___          __   __     __      __   ___       __
 # /__` /  ` |__) | |__)  |     |    /  \ / _` | /  `    |__) |__  |    /  \ |  |
 # .__/ \__, |  \ | |     |     |___ \__/ \__> | \__,    |__) |___ |___ \__/ |/\|
 
-pkill -f "runserver"
+sudo pkill -f "runserver"
 
 # Helps to see exactly where the script's output ends.
 print_dashed_line_with_text "terminate django script end for : ${HOST_NAME}"
