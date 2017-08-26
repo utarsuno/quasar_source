d6a1c12e874b5d1fe310cc03416aa52c9b0ec15c
utarsuno <utarsuno@gmail.com>
2017-08-26 18:42:04 -0500

    Testing CDN link.

---
+++ quasar_source_code/math_utilities/log_properties.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+"""This module, log_properties.py, outlines logarithm properties."""
+
+# Create this as a page on Three.js !!!
+
+# Product rule :
+# logb(MN) = logb(M) + logb(N)
+
+# Quotient rule :
+# logb(M/N) = logb(M) - logb(N)
+
+# Power rule :
+# logb(M^p) = p * logb(M)
+
+# ??? rule :
+# logb(x^.5) = .5 * logb(x)
