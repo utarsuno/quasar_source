02b261047dcae150f81eecda6d84e574e5ff02e9
utarsuno <utarsuno@gmail.com>
2017-08-26 21:02:33 -0500

    Log rule display testing 39

--- quasar_source_code/quasar_site/static/js/custom/renderer_api.js
+++ quasar_source_code/quasar_site/static/js/custom/renderer_api.js
@@ -28,12 +28,12 @@ RendererAPI.prototype = {
             this.get_window_properties()
             this.near_clipping = 0.1
             this.far_clipping  = 1000
-            this.renderer      = new THREE.WebGLRenderer({antialias: true})
+            this.renderer      = new THREE.WebGLRenderer({antialias: true, alpha:true})
             this.renderer.setPixelRatio(window.devicePixelRatio)
             this.renderer.setSize(this.window_width, this.window_height)
             this.renderer.setClearColor(0xECF8FF)
 
-            //this.renderer.domElement.style.position = 'absolute'
+            this.renderer.domElement.style.position = 'absolute'
             this.renderer.domElement.style.zIndex = 0
             this.renderer.domElement.style.top = 0
 
