2f998c0df2673ec9ad1895dab82ceeef0ca5b65d
utarsuno <utarsuno@gmail.com>
2017-09-07 19:46:34 -0500

    Django updating 2

--- quasar_source_code/quasar_site_django/quasar_web_server/routing.py
+++ quasar_source_code/quasar_site_django/quasar_web_server/routing.py
@@ -3,7 +3,8 @@
 """Documentation TODO :"""
 
 from channels.routing import route
-from quasar_web_server.routing import ws_connect, ws_disconnect
+#from quasar_web_server.routing import ws_connect, ws_disconnect
+from quasar_source_code.quasar_site_django.quasar_web_server.consumers import ws_connect, ws_disconnect
 
 channel_routing = [
 	route('websocket.connect', ws_connect),
