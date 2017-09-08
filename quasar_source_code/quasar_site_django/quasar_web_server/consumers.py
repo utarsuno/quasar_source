6849da652868f1782ff2bad65eb1cad233855956
utarsuno <utarsuno@gmail.com>
2017-09-07 20:10:57 -0500

    Django updating 10

--- quasar_source_code/quasar_site_django/quasar_web_server/consumers.py
+++ quasar_source_code/quasar_site_django/quasar_web_server/consumers.py
@@ -9,6 +9,10 @@ def ws_connect(message):
 	Group('users').add(message.reply_channel)
 
 
+def ws_message(message):
+	print('JUST GOT THE MESSAGE : ' + str(message))
+
+
 def ws_disconnect(message):
 	Group('users').discard(message.reply_channel)
 
--- quasar_source_code/quasar_site_django/quasar_web_server/routing.py
+++ quasar_source_code/quasar_site_django/quasar_web_server/routing.py
@@ -8,5 +8,6 @@ from quasar_web_server.consumers import ws_connect, ws_disconnect
 
 channel_routing = [
 	route('websocket.connect', ws_connect),
+	route('websocket.receive', ws_message),
 	route('websocket.disconnect', ws_disconnect),
 ]
