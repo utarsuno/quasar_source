e688b39314f0801286547a6d2ab6319e02281625
utarsuno <utarsuno@gmail.com>
2017-09-07 20:15:16 -0500

    Django updating 13

--- quasar_source_code/quasar_site_django/quasar_web_server/consumers.py
+++ quasar_source_code/quasar_site_django/quasar_web_server/consumers.py
@@ -3,12 +3,15 @@
 """This module, consumers.py, is used to handle basic connections between the client and server."""
 
 from channels import Group
+from channels.sessions import channel_session
 
 
+@channel_session
 def ws_connect(message):
 	Group('users').add(message.reply_channel)
 
 
+@channel_session
 def ws_message(message):
 	print('JUST GOT THE MESSAGE : ' + str(message))
 	Group('users').send({
@@ -16,6 +19,7 @@ def ws_message(message):
 	})
 
 
+@channel_session
 def ws_disconnect(message):
 	Group('users').discard(message.reply_channel)
 
