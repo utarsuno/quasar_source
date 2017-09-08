ea5958c676bd407fc5bbdce4f49966c602d5dc2c
utarsuno <utarsuno@gmail.com>
2017-09-07 21:59:56 -0500

    Django updating 34

--- quasar_source_code/quasar_site_django/static/js/custom/web_sockets/world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/web_sockets/world.js
@@ -44,7 +44,7 @@ Player.prototype = {
     update: function(x_position, z_position) {
         this.x_position = x_position
         this.z_position = z_position
-        this.cube_model.model.position(this.x_position, 10, this.z_position)
+        this.cube_model.model.position.set(this.x_position, 10, this.z_position)
     }
 
 }
