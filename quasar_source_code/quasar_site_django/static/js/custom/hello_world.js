46fdfa632ed257cba8faeed51debfdf69a9bf176
utarsuno <utarsuno@gmail.com>
2017-09-08 22:38:41 -0500

    General updating 09

--- quasar_source_code/quasar_site_django/static/js/custom/hello_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/hello_world.js
@@ -54,7 +54,7 @@ var plane_material = new THREE.MeshLambertMaterial({color: 0xccffcc, side: THREE
 var plane_mesh     = new THREE.Mesh(plane_geometry, plane_material)
 scene.add(plane_mesh)
 var ground_normal = plane_mesh.normal
-
+console.log('GROUND NORMAL IS : ' + ground_normal)
 
 var fps_controls = new FPSControls(camera, ground_normal)
 
