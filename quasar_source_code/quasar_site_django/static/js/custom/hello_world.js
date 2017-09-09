a29038be31f4dddcab0052d7aa5c873b4ac680a3
utarsuno <utarsuno@gmail.com>
2017-09-08 19:20:41 -0500

    Testing text texture 04

--- quasar_source_code/quasar_site_django/static/js/custom/hello_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/hello_world.js
@@ -214,8 +214,9 @@ loader.load('/home/git_repos/quasar_source/quasar_source_code/quasar_site_django
 
 var text_position = new THREE.Vector3(40, 20, 30)
 var text_rotation = new THREE.Vector3(0, 0, 0)
-var text_plane = new PlaneAPI(400, 400, text_position, text_rotation, renderer_api.renderer, scene)
+var text_plane = new PlaneAPI(400, 400, text_position, text_rotation)
 text_plane.create_dynamic_text('Hello World!?!')
+scene.add(text_plane.mesh)
 
 
 
