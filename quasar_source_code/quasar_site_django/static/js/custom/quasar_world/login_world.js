66ec119994ac2bc5b19c9f20f6c3b5e60b3100b4
utarsuno <utarsuno@gmail.com>
2017-09-20 19:23:26 -0500

    js updating 10.

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/login_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/login_world.js
@@ -16,24 +16,28 @@ LoginWorld.prototype = {
         var plane_geometry = new THREE.PlaneGeometry(2000, 2000, 100, 100)
         plane_geometry.applyMatrix(new THREE.Matrix4().makeRotationX(- Math.PI / 2))
         //var plane_material = new THREE.MeshBasicMaterial({color: 0x0000ff})
-        var plane_material = new THREE.MeshBasicMaterial({color: 0xccffcc, side: THREE.FrontSide, wireframe: true})
+        var plane_material = new THREE.MeshLambartMaterial({color: 0xccffcc, side: THREE.FrontSide, wireframe: false})
         var plane_mesh     = new THREE.Mesh(plane_geometry, plane_material)
         this.add_to_scene(plane_mesh)
 
         // Add lights.
+        /*
         var color1 = '#b9ffd2'
         var color2 = '#090920'
         var light = new THREE.HemisphereLight(color1, color2, .5)
         this.add_to_scene(light)
+        */
 
         var light3 = new THREE.PointLight(0xccffcc, .8, 0)
-        light3.position.set(5, 50, 5)
+        light3.position.set(5, 100, 5)
         this.add_to_scene(light3)
 
         // Add the text.
         var text_plane = new Floating2DText(200, 50, 'Hello Worlddddddddddd')
         text_plane.create(this.scene)
-
+        var position_vector = new THREE.Vector3(50, 50, 50)
+        var look_at_vector = new THREE.Vector3(0, 0, 0)
+        text_plane.update_position_and_look_at(position_vector, look_at_vector)
     },
 
     add_to_scene: function(object) {
