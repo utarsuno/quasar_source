078a4b4040b7f40e0fbc8e01c3e82e0f2bef5d6e
utarsuno <utarsuno@gmail.com>
2017-09-26 00:59:38 -0500

    Updating django backend 07.

--- quasar_source_code/entities/database/entity_database.py
+++ quasar_source_code/entities/database/entity_database.py
@@ -66,7 +66,7 @@ class EntityDatabaseAPI(object):
 
 	def create_owner(self, name: str, password: str, email: str):
 		"""Places an owner into the owners table."""
-		self._owners.insert_row({'name': name, 'password': password, 'email'})
+		self._owners.insert_row({'name': name, 'password': password, 'email': email})
 
 	def get_all_owners(self):
 		"""Returns a list of all the owners."""
