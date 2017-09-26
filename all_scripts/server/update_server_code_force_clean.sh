d3894df4de618a551a76dd2f6a88c59ec034abbd
utarsuno <utarsuno@gmail.com>
2017-09-26 17:59:21 -0500

    General updating 10.

--- all_scripts/server/update_server_code_force_clean.sh
+++ all_scripts/server/update_server_code_force_clean.sh
@@ -26,6 +26,9 @@ if [[ "${reslog}" != "" ]] ; then
 
     # This resets to master.
     git reset --hard origin/master;
+
+    # Remove files that are not tracked. The -d flag is needed for removing directories as well.
+    git clean -fd;
 else
     # We do not have to update the code.
     print_script_text "The code base is already up to date so a pull will not be performed."
