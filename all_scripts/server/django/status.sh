be0a6c661426b5a11c2fd4f06a52fff99ba4b990
utarsuno <utarsuno@gmail.com>
2017-09-28 19:40:04 -0500

    general updating 16

---
+++ all_scripts/server/django/status.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /home/git_repos/quasar_source/all_scripts/universal_scripts;
+source universal_functions.sh;
+
+# Helps see exactly where the script's output starts.
+print_dashed_line_with_text "status django script start for : ${HOST_NAME}"
+
+#  __        ___  ___ ___         __        ___  __        __      ___    __   __  ___
+# /__`  /\  |__  |__   |  \ /    /  ` |__| |__  /  ` |__/ /__`    |__  | |__) /__`  |
+# .__/ /~~\ |    |___  |   |     \__, |  | |___ \__, |  \ .__/    |    | |  \ .__/  |
+
+terminate_if_system_is_not_ubuntu
+
+#  __   __   __     __  ___          __   __     __      __   ___       __
+# /__` /  ` |__) | |__)  |     |    /  \ / _` | /  `    |__) |__  |    /  \ |  |
+# .__/ \__, |  \ | |     |     |___ \__/ \__> | \__,    |__) |___ |___ \__/ |/\|
+
+path_to_script=/home/git_repos/quasar_source/all_scripts/universal_scripts/is_program_running.py
+is_django_running=$(python3 ${path_to_script} 'runserver')
+
+if [ "${is_django_running}" == "true" ]; then
+  echo 'Django is running!'
+fi
+
+# Helps to see exactly where the script's output ends.
+print_dashed_line_with_text "status django script end for : ${HOST_NAME}"
