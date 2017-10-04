cda848493326fee7d80fccfec03e3d4aad5a7e1d
utarsuno <utarsuno@gmail.com>
2017-10-03 23:48:27 -0500

    Mouse smooth step movement testing 09

--- quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
+++ quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
@@ -64,8 +64,8 @@ FPSControls.prototype = {
 
         this.velocity = new THREE.Vector3()
 
-        this.mouse_movement_x_buffer = new SmoothStep(this.yaw.rotation.y, 2.0)
-        this.mouse_movement_y_buffer = new SmoothStep(this.pitch.rotation.x, 2.0)
+        this.mouse_movement_x_buffer = new SmoothStep(this.yaw.rotation.y, 1.0)
+        this.mouse_movement_y_buffer = new SmoothStep(this.pitch.rotation.x, 1.0)
 
         document.addEventListener('keypress', this.on_key_press.bind(this), false)
         document.addEventListener('mousemove', this.on_mouse_move.bind(this), false)
