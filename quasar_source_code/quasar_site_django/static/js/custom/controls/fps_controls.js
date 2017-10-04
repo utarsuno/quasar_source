475d525310cf52df6390ac4e1c4d30e3c39a2a43
utarsuno <utarsuno@gmail.com>
2017-10-03 23:32:35 -0500

    Mouse smooth step movement testing 02

--- quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
+++ quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
@@ -64,8 +64,8 @@ FPSControls.prototype = {
 
         this.velocity = new THREE.Vector3()
 
-        this.mouse_movement_x_buffer = new SmoothStep(this.yaw.rotation.y)
-        this.mouse_movement_y_buffer = new SmoothStep(this.pitch.rotation.x)
+        this.mouse_movement_x_buffer = new SmoothStep(this.yaw.rotation.y, 3.0)
+        this.mouse_movement_y_buffer = new SmoothStep(this.pitch.rotation.x, 3.0)
 
         document.addEventListener('keypress', this.on_key_press.bind(this), false)
         document.addEventListener('mousemove', this.on_mouse_move.bind(this), false)
@@ -293,8 +293,10 @@ FPSControls.prototype = {
             var movement_y = event.movementY || event.mozMovementY || event.webkitMovementY || 0
 
             // TODO : Smooth step this portion out.
-            this.yaw.rotation.y   -= movement_x * 0.002
-            this.pitch.rotation.x -= movement_y * 0.002
+            //this.yaw.rotation.y   -= movement_x * 0.002
+            //this.pitch.rotation.x -= movement_y * 0.002
+            this.yaw.rotation.y = this.mouse_movement_x_buffer.get_current_value()
+            this.pitch.rotation.x = this.mouse_movement_y_buffer.get_current_value()
 
             this.mouse_movement_x_buffer.add_force(movement_x * -0.002)
             this.mouse_movement_y_buffer.add_force(movement_y * -0.002)
--- quasar_source_code/quasar_site_django/static/js/custom/core/smooth_step.js
+++ quasar_source_code/quasar_site_django/static/js/custom/core/smooth_step.js
@@ -19,8 +19,8 @@ function smoothstep(edge0, edge1, x) {
     return x * x * (3 - 2 * x)
 }
 
-function SmoothStep(current_value) {
-    this.__init__(current_value)
+function SmoothStep(current_value, time_needed_for_each_force) {
+    this.__init__(current_value, time_needed_for_each_force)
 }
 
 SmoothStep.prototype = {
@@ -28,21 +28,35 @@ SmoothStep.prototype = {
     buffer: null,
 
     current_value: null,
+    time_needed_for_each_force: null,
 
-    __init__: function(current_value) {
+    __init__: function(current_value, time_needed_for_each_force) {
         this.buffer = []
         this.current_value = current_value
+        this.time_needed_for_each_force = time_needed_for_each_force
     },
 
-    add_force: function(magnitude, time_needed) {
-        this.buffer.push([magnitude, 0.0, time_needed])
+    add_force: function(magnitude) {
+        this.buffer.push([magnitude, 0.0])
     },
 
     update: function(delta) {
-        console.log('Delta is : ' + delta)
         for (var i = 0; i < this.buffer.length; i++) {
             this.buffer[i][1] += delta
+            if (this.buffer[i][1] >= this.time_needed_for_each_force) {
+                this.current_value += this.buffer[i][0]
+                // Remove this position.
+                this.buffer.splice(i, 1)
+            }
         }
+    },
+
+    get_current_value: function() {
+        var value_instance = this.current_value
+        for (var x = 0; x < this.buffer.length; x++) {
+            value_instance += smoothstep(0.0, this.time_needed_for_each_force, this.buffer[x][1])
+        }
+        return value_instance
     }
 
 }
