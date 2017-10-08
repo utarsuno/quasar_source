2c5146d9e16855227dae25cfd438a305a5339894
utarsuno <utarsuno@gmail.com>
2017-10-08 18:06:53 -0500

    general updating 06

--- quasar_source_code/quasar_site_django/static/js/custom/global/globals.js
+++ quasar_source_code/quasar_site_django/static/js/custom/global/globals.js
@@ -34,10 +34,16 @@ function is_email_valid(email) {
     return re.test(email)
 }
 
-function get_cookie(field_name) {
-    var re = new RegExp(name + '=([^;]+)')
-    var value = re.exec(document.cookie)
-    return (value != null) ? unescape(value[1]) : null
+// Base code from : https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+function get_cookie(name) {
+    var nameEQ = name + '='
+    var ca = document.cookie.split(';')
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length)
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length)
+    }
+    return null
 }
 
 // Base code from : https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
