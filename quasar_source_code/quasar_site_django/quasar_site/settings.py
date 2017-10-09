5d5452897e4616f0aa8e026a005fe7b528fb97f8
utarsuno <utarsuno@gmail.com>
2017-10-08 19:34:14 -0500

    general updating 16

--- quasar_source_code/quasar_site_django/quasar_site/settings.py
+++ quasar_source_code/quasar_site_django/quasar_site/settings.py
@@ -64,8 +64,6 @@ CHANNEL_LAYERS = {
 #
 
 MIDDLEWARE = [
-    'django_cookies.CookiePreHandlerMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,8 +71,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'django_cookies.CookiePostHandlerMiddleware',
 ]
 
 ROOT_URLCONF = 'quasar_site.urls'
