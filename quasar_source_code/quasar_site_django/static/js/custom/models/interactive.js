374545d017bf0315673f923812feb3adc9ba62e1
utarsuno <utarsuno@gmail.com>
2017-10-10 16:25:14 -0500

    design cleanup 05

--- quasar_source_code/quasar_site_django/static/js/custom/models/interactive.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/interactive.js
@@ -1,12 +1,10 @@
 'use strict'
 
-function Interactive(source_object) {
-    this.__init__(source_object)
+function Interactive() {
+    this.__init__()
 }
 
 Interactive.prototype = {
-
-    source_object      : null,
 
     engage_function    : null,
     disengage_function : null,
@@ -15,26 +13,24 @@ Interactive.prototype = {
     being_looked_at    : null,
     being_engaged_with : null,
 
-    __init__: function(source_object) {
-        this.source_object      = source_object
-
+    __init__: function() {
         this.being_looked_at    = false
         this.being_engaged_with = false
     },
 
     look_away: function() {
         this.being_looked_at = false
-        this.source_object.state_change_look_at(false)
+        this.state_change_look_at(false)
     },
 
     look_at: function() {
         this.being_looked_at = true
-        this.source_object.state_change_look_at(true)
+        this.state_change_look_at(true)
     },
 
     disengage: function(player) {
         this.being_engaged_with = false
-        this.source_object.state_change_engage(false, player)
+        this.state_change_engage(false, player)
         if (this.disengage_function !== null) {
             this.disengage_function()
         }
@@ -46,7 +42,7 @@ Interactive.prototype = {
 
     engage: function(player) {
         this.being_engaged_with = true
-        this.source_object.state_change_engage(true, player)
+        this.state_change_engage(true, player)
         if (this.engage_function !== null) {
             this.engage_function()
         }
