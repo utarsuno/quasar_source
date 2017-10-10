0e00b61ea9d5edee4ce87625cb46048b7e9d8d57
utarsuno <utarsuno@gmail.com>
2017-10-09 19:55:38 -0500

    Graphical + backend updating 05

--- quasar_source_code/quasar_site_django/static/js/custom/models/interactive_wall.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/interactive_wall.js
@@ -15,14 +15,26 @@ InteractiveWall.prototype = {
 
     wall: null,
 
+    is_visible: null,
+
+    close_button: null,
+
     __init__: function(w, h, position, look_at, scene) {
+        this.is_visible = true
+
         this.scene = scene
         this.width = w
         this.height = h
         this.object_3d = new THREE.Object3D()
 
+        // Base wall.
         this.wall = new PlaneAPI(this.width, this.height)
         this.object_3d.add(this.wall.mesh)
+
+        // Close button.
+        this.close_button = new CheckBox(true, this.scene)
+        this.close_button.update_position_and_look_at(position, look_at)
+
 
         this.object_3d.position.x = position.x + this.width / 2
         this.object_3d.position.y = position.y
@@ -30,5 +42,10 @@ InteractiveWall.prototype = {
         this.object_3d.lookAt(new THREE.Vector3(look_at.x + this.width / 2, look_at.y, look_at.z))
 
         this.scene.add(this.object_3d)
+    },
+
+    toggle_visibility: function() {
+        this.is_visible = !this.is_visible
+        this.object_3d.visible = this.is_visible
     }
 }
