6977dce2793df0d1123cb9e266f8f1ed6c9d3b54
utarsuno <utarsuno@gmail.com>
2017-10-10 00:00:47 -0500

    Graphical + backend updating 24

--- quasar_source_code/quasar_site_django/static/js/custom/models/interactive_wall.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/interactive_wall.js
@@ -77,8 +77,10 @@ InteractiveWall.prototype = {
         })
         if (this.is_visible) {
             this.close_button.set_to_visible()
+            this.title.set_to_visible()
         } else {
             this.close_button.set_to_invisible()
+            this.title.set_to_invisible()
         }
     },
 
@@ -90,8 +92,8 @@ InteractiveWall.prototype = {
 
     add_title: function(title) {
         this.title = new Floating3DText(this.width / 2, title, TYPE_TITLE, this.scene)
-        var title_position = new THREE.Vector3(this.object3D.position.x, this.object3D.position.y + this.height / 2 - this.title.height / 2, this.object3D.position.z + 1)
-        var title_look_at = new THREE.Vector3(this.look_at.x, this.look_at.y, this.look_at.z + 2)
+        var title_position = new THREE.Vector3(this.object3D.position.x - this.width / 2, this.object3D.position.y + this.height / 2 - this.title.height / 2, this.object3D.position.z + 1)
+        var title_look_at = new THREE.Vector3(this.look_at.x, this.look_at.y + this.height / 2 - this.title.height / 2, this.look_at.z + 2)
         this.title.update_position_and_look_at(title_position, title_look_at)
     }
 }
