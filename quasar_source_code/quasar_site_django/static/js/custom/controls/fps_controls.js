832dbc165019c82b6b8ca29fe8e720a34e5de44b
utarsuno <utarsuno@gmail.com>
2017-10-14 16:13:59 -0500

    general updating 21

--- quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
+++ quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
@@ -338,7 +338,7 @@ FPSControls.prototype = {
         console.log('Y difference 3 : ' + y_angle)
 
 
-        this.mouse_movement_y_buffer.add_force(-1.0 * ((d.y - look_at_normal.y) * (1.0 + Math.PI / 180.0)))
+        this.mouse_movement_y_buffer.add_force(-1.0 * ((d.y - look_at_normal.y) * (1.0 + (TWO_PI / 180.0))))
         //this.mouse_movement_y_buffer.add_force(y_angle * (Math.PI / 180.0))
 
         this.mouse_movement_x_buffer.add_force(-1.0 * (look_at_angle - angle))
