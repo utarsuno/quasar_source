ac0c5ed29d2d8b8bb19c63da0f24c65f3c9fb17d
utarsuno <utarsuno@gmail.com>
2017-10-15 21:01:48 -0500

    establishing dynamic entity editing and viewing 60

--- quasar_source_code/quasar_site_django/static/js/custom/entities/entity.js
+++ quasar_source_code/quasar_site_django/static/js/custom/entities/entity.js
@@ -60,6 +60,8 @@ Entity.prototype = {
         this.name            = name
         this.keys_and_values = keys_and_values
 
+        console.log(this.keys_and_values)
+
         console.log(ENTITY_PROPERTY_ID)
         console.log(ENTITY_PROPERTY_ID in keys_and_values)
         console.log(keys_and_values.hasOwnProperty(ENTITY_PROPERTY_ID))
