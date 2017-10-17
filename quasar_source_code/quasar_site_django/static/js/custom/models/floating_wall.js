6a94e054fcd2a91c6db4ab03dba84a4e20373011
utarsuno <utarsuno@gmail.com>
2017-10-17 18:58:41 -0500

    general refactoring 23

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_wall.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_wall.js
@@ -61,14 +61,7 @@ FloatingWall.prototype = {
         this.create_entity = new Floating2DText(this.width, 'Create Entity', TYPE_BUTTON, this.scene)
         this.create_entity.update_position_and_look_at(this.get_position_for_row(0, this.get_y_position_for_row(1), 0, 1), this.get_look_at_for_row(0, this.get_y_position_for_row(1), 0, 1))
         this.create_entity.set_engage_function(this.create_entity_button_pressed.bind(this))
-
-        // Create entity wall.
-        var entity_wall_position = this.get_position_for_row(0, 0, 0, 1)
-        var entity_wall_look_at = this.get_look_at_for_row(0, 0, 0, 1)
-        this.create_entity_wall = new EntityEditor(null, entity_wall_position, entity_wall_look_at, this.scene)
-        this.create_entity_wall.set_to_invisible()
-
-
+        
         // Save changes button.
         this.save_changes = new Floating2DText(this.width, 'Save Changes', TYPE_BUTTON, this.scene)
         this.save_changes.update_position_and_look_at(this.get_position_for_row(0, this.get_y_position_for_row(2), 0, 1), this.get_look_at_for_row(0, this.get_y_position_for_row(2), 0, 1))
