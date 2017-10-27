0aa648550e1fe532d7073f55fa5b43683aa0c66e
utarsuno <utarsuno@gmail.com>
2017-10-27 16:04:38 -0500

    general updating 20

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/world_manager.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/world_manager.js
@@ -101,7 +101,7 @@ WorldManager.prototype = {
     // TODO : Add error checking.
     load_specific_texture: function(texture_url, variable_to_map_to) {
         var v = variable_to_map_to
-        var t = this.texture_was_loaded()
+        var t = this.texture_was_loaded
         var ta = new THREE.TextureLoader().load(texture_url,
         //function when resource is loaded
             function(texture) {
@@ -116,7 +116,6 @@ WorldManager.prototype = {
                 console.log(xhr)
             }
         )
-        n += 1
     }
 
 }
