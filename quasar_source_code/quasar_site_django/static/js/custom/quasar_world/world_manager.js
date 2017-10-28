b7ae88a3c73e4d8829113465fb558707ffc51233
utarsuno <utarsuno@gmail.com>
2017-10-28 18:59:23 -0500

    general updating 13

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/world_manager.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/world_manager.js
@@ -56,9 +56,9 @@ WorldManager.prototype = {
         this.planet_home = new Planet(new THREE.Vector3(80, 80, 80))
         this.planet_login = new Planet(new THREE.Vector3(20, 20, 20))
 
-        this.world_settings.add_to_scene(this.planet_settings.mesh)
-        this.planet_login.add_to_scene(this.planet_login.mesh)
-        this.planet_login.add_to_scene(this.planet_home.mesh)
+        this.world_login.add_to_scene(this.planet_settings.mesh)
+        this.world_login.add_to_scene(this.planet_login.mesh)
+        this.world_login.add_to_scene(this.planet_home.mesh)
 
         this.world_home.add_to_scene(this.planet_settings.mesh)
         this.world_home.add_to_scene(this.planet_login.mesh)
