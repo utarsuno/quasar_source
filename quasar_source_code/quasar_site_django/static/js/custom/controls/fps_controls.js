a2d2c8fb037d8f06036333f42272db8e590c9c1f
utarsuno <utarsuno@gmail.com>
2017-11-13 16:58:50 -0600

    fixing player.look_at 6

--- quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
+++ quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
@@ -312,8 +312,8 @@ FPSControls.prototype = {
         this.mouse_movement_x_buffer.set_value(current_x_value + (-1.0 * (look_at_angle - angle)));
 
         this.mouse_movement_y_buffer.clear_buffer();
-        var y_angle = Math.atan2(look_at_normal.y, d.y);
-        this.mouse_movement_y_buffer.set_value(y_angle);
+        //this.mouse_movement_y_buffer.set_value(look_at_normal.y);
+        this.mouse_movement_y_buffer.set_value(d.y + ((d.y - look_at_normal.y) * HALF_PIE));
 
         l(look_at_normal.y);
 
