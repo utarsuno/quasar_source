0d70a1c9e692387a2bf83291fd359ae5c70bcf74
utarsuno <utarsuno@gmail.com>
2017-11-13 21:15:43 -0600

    fixing player.look_at 10

--- quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
+++ quasar_source_code/quasar_site_django/static/js/custom/controls/fps_controls.js
@@ -305,14 +305,15 @@ FPSControls.prototype = {
 
         var angle = Math.atan2(x_rotation.y, x_rotation.x);
 
-        // TODO : Fix this method!!
+        // TODO : Don't clear the values, use add force instead!
 
         var current_x_value = this.mouse_movement_x_buffer.get_current_value();
-        this.mouse_movement_x_buffer.clear_buffer();
-        this.mouse_movement_x_buffer.set_value(current_x_value + (-1.0 * (look_at_angle - angle)));
+        //this.mouse_movement_x_buffer.clear_buffer();
+        //this.mouse_movement_x_buffer.set_value(current_x_value + (-1.0 * (look_at_angle - angle)));
+
+        this.mouse_movement_x_buffer.add_force((-1.0 * (look_at_angle - angle)))
 
         this.mouse_movement_y_buffer.clear_buffer();
-        //this.mouse_movement_y_buffer.set_value(look_at_normal.y);
         this.mouse_movement_y_buffer.set_value(Math.asin(look_at_normal.y));
 
         l(look_at_normal.y);
