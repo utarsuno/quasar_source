e9fcf52c0b36de5762e5ddad1be57f980e5222ab
utarsuno <utarsuno@gmail.com>
2017-11-21 22:42:18 -0600

    general updating 16

--- quasar_source_code/entities/database/entity_database_testing.py
+++ quasar_source_code/entities/database/entity_database_testing.py
@@ -2,13 +2,19 @@
 
 """Temporary file while entity_database is being refactored"""
 
+# Owner dictionary key mappings.
+OWNER_KEY_NAME      = 'name'
+OWNER_KEY_PASSWORD  = 'password'
+OWNER_KEY_EMAIL     = 'email'
+OWNER_KEYS_REQUIRED = [OWNER_KEY_PASSWORD, OWNER_KEY_NAME, OWNER_KEY_EMAIL]
+
 
 from quasar_source_code.entities.database.entity_database import EntityDatabaseAPI
 
 db = EntityDatabaseAPI()
 db.connect()
 
-db.create_owner({'apple'})
+db.create_owner({OWNER_KEY_NAME: 'test3', OWNER_KEY_EMAIL: 'test3@test3.com', OWNER_KEY_PASSWORD: 'test3'})
 
 owners = db.get_all_owners()
 for o in owners:
