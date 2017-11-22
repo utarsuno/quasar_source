9300d9420711818caf6090f2dd7401be38553e5f
utarsuno <utarsuno@gmail.com>
2017-11-22 01:39:00 -0600

    general updating 32

--- quasar_source_code/entities/database/entity_database.py
+++ quasar_source_code/entities/database/entity_database.py
@@ -127,6 +127,8 @@ class EntityDatabaseAPI(object):
 	def _update_owners_cache(self):
 		"""Reloads the owners."""
 		owners = self._owners_collection.get_all()
+		print('OWNERS ARE')
+		print(owners)
 		for o in owners:
 			self._owners_cache.append(EntityOwner(o))
 
