acc3e6b871dd0f95ba3ae639988e233ab986de38
utarsuno <utarsuno@gmail.com>
2017-11-21 23:21:32 -0600

    general updating 19

--- quasar_source_code/entities/database/entity_database_testing.py
+++ quasar_source_code/entities/database/entity_database_testing.py
@@ -7,7 +7,7 @@ OWNER_KEY_NAME      = 'name'
 OWNER_KEY_PASSWORD  = 'password'
 OWNER_KEY_EMAIL     = 'email'
 OWNER_KEYS_REQUIRED = [OWNER_KEY_PASSWORD, OWNER_KEY_NAME, OWNER_KEY_EMAIL]
-
+OWNER_KEY_ID        = '_id'
 
 from quasar_source_code.entities.database.entity_database import EntityDatabaseAPI
 
@@ -17,6 +17,8 @@ db.connect()
 #db.create_owner({OWNER_KEY_NAME: 'test4', OWNER_KEY_EMAIL: 'test3@test3.com', OWNER_KEY_PASSWORD: 'test3'})
 owner_id = db.get_owner_id_by_name('test4')
 print(owner_id)
+
+db.update_owner({OWNER_KEY_ID: owner_id, 'email': 'apple@apple.com', 'no': 'yes'})
 
 owners = db.get_all_owners()
 for o in owners:
