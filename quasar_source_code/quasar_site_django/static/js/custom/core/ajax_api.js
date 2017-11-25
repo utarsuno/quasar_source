125513a0335580fe079398dbcd976323f1fc1642
utarsuno <utarsuno@gmail.com>
2017-11-25 00:41:08 -0600

    code cleanup 36

--- quasar_source_code/quasar_site_django/static/js/custom/core/ajax_api.js
+++ quasar_source_code/quasar_site_django/static/js/custom/core/ajax_api.js
@@ -27,8 +27,11 @@ PostHelper.prototype = {
         // From : https://stackoverflow.com/questions/9713058/send-post-data-using-xmlhttprequest
         const http = new XMLHttpRequest();
         http.open('POST', this.url, true);
-        http.setRequestHeader('Content-type', 'application/json');
-        http.send(JSON.stringify(post_data));
+        //"application/x-www-form-urlencoded"
+        //http.setRequestHeader('Content-type', 'application/json');
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        //http.send(JSON.stringify(post_data));
+        http.send('json_name=' + JSON.stringify(post_data));
 
         http.onload = function() {
             l('POST onload response :');
