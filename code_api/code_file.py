76072ccf7961508a7a0655b754cc731ac4439a41
utarsuno <utarsuno@gmail.com>
2017-11-25 22:45:27 -0600

    general updating + cleanup 25

--- code_api/code_file.py
+++ code_api/code_file.py
@@ -3,6 +3,9 @@
 """This module, code_file.py, represents an abstraction layer to a file of code."""
 
 from code_api import lines_of_code as loc
+from quasar_source_code.universal_code import useful_file_operations as ufo
+# Current 3rd party used to handle minification.
+from jsmin import jsmin
 
 
 class CodeFile(object):
@@ -10,5 +13,59 @@ class CodeFile(object):
 
 	def __init__(self, file_path):
 		self._file_path = file_path
+		self._file_size = ufo.get_file_size_in_bytes(self._file_path)
 		self._lines_of_code = loc.get_lines_of_code_from_file(self._file_path)
 
+	@property
+	def file_size(self):
+		"""Returns the size of this file in bytes."""
+		return self._file_size
+
+
+class CodeFileJavaScript(CodeFile):
+	"""Represents a single JavaScript file."""
+
+	def __init__(self, file_path):
+		super().__init__(file_path)
+		self._minified_js = None
+		self._minified_file_path = self._file_path.replace('.js', '.min.js')
+
+	def get_minified_javascript_text(self):
+		"""Gets the minified version of the Javascript file provided."""
+		if self._minified_js is None:
+			with open(self._file_path) as js_file:
+				self._minified_js = jsmin(js_file.read())
+		return self._minified_js
+
+	def create_minified_version(self):
+		"""Creates the minified version of this Javascript file."""
+		ufo.create_file_or_override(self.get_minified_javascript_text(), self._minified_file_path)
+
+'''
+def produce_quasar_minified_javascript_files():
+	"""Produces the *.min.js files."""
+	all_javascript_files = _get_all_javascript_files()
+
+	total_original_size = 0
+	total_reduced_size  = 0
+
+	for f in all_javascript_files:
+		//minified_file_path = f.replace('.js', '.min.js')
+		total_original_size += f.file_size
+
+		#print('Currently parsing {' + str(file_name) + '} - size{' + str(original_file_size) + '}')
+
+		m = get_minifed_javascript_text(f)
+		ufo.create_file_or_override(m, minified_file_path)
+		minified_file_size = ufo.get_file_size_in_bytes(minified_file_path)
+
+		#print('Created {' + ufo.get_file_basename(minified_file_path) + '} - size{' + str(minified_file_size) + '}')
+		total_reduced_size += minified_file_size
+
+		#print()
+
+	print('Total size before : ' + str(total_original_size))
+	print('New size : ' + str(total_reduced_size))
+	print('Size reduction % : ' + str(1.0 - (total_reduced_size / total_original_size)))
+
+'''
--- code_api/quasar/quasar_code.py
+++ code_api/quasar/quasar_code.py
@@ -4,8 +4,6 @@
 
 from quasar_source_code.universal_code import useful_file_operations as ufo
 
-# Currently 3rd party used to handle minification.
-from jsmin import jsmin
 # Pretty printing!
 from lazyme.string import color_print
 # Used to get arguments passed into the program.
@@ -27,24 +25,33 @@ ARGUMENT_BUILD_PRODUCTION = '-bp'
 ARGUMENT_RUN_ANALYSIS     = '-ra'
 
 
-def get_minifed_javascript_text(javascript_file_path):
-	"""Gets the minified version of the Javascript file provided."""
-	with open(javascript_file_path) as js_file:
-		minified = jsmin(js_file.read())
-	return minified
-
-
-def _get_all_javascript_files():
-	"""Returns a list of CodeFile objects of all the Quasar Javascript files."""
+def _get_all_javascript_files(get_minified_files):
+	"""Returns a list of CodeFile objects of all the Quasar javascript files (with a flag to determine if minified files returned or non-minified files returned."""
 	all_javascript_files_path = CODE_SOURCE_BASE + 'quasar_source_code/quasar_site_django/static/js/custom'
 	all_javascript_files = ufo.get_all_file_paths_inside_directory(all_javascript_files_path)
 	files_to_ignore = ['helvetiker_regular.typeface.json.js']
 	files_to_return = []
-	for f in all_javascript_files:
-		file_name = ufo.get_file_basename(f)
-		if file_name not in files_to_ignore and '.min.js' not in file_name:
-			files_to_return.append(cf.CodeFile(f))
+	if get_minified_files:
+		for f in all_javascript_files:
+			file_name = ufo.get_file_basename(f)
+			if file_name not in files_to_ignore and '.min.js' in file_name:
+				files_to_return.append(cf.CodeFile(f))
+	else:
+		for f in all_javascript_files:
+			file_name = ufo.get_file_basename(f)
+			if file_name not in files_to_ignore and '.min.js' not in file_name:
+				files_to_return.append(cf.CodeFile(f))
 	return files_to_return
+
+
+def _get_all_javascript_files():
+	"""Returns a list of CodeFile objects of all the Quasar Javascript files."""
+	return _get_all_javascript_files(False)
+
+
+def _get_all_javascript_production_files():
+	"""Returns a list of CodeFile objects of all the Quasar production Javascript files."""
+	return _get_all_javascript_files(True)
 
 
 def _get_all_python_files():
@@ -53,40 +60,24 @@ def _get_all_python_files():
 	files_to_ignore = ['__init__.py']
 
 
-
-#
 def produce_quasar_minified_javascript_files():
 	"""Produces the *.min.js files."""
 	all_javascript_files = _get_all_javascript_files()
-
 	total_original_size = 0
-	total_reduced_size  = 0
-
 	for f in all_javascript_files:
-		original_file_size = ufo.get_file_size_in_bytes(f)
-		minified_file_path = f.replace('.js', '.min.js')
-		total_original_size += original_file_size
+		total_original_size += f.file_size
+		f.create_minified_version()
 
-		#print('Currently parsing {' + str(file_name) + '} - size{' + str(original_file_size) + '}')
+	print('Total size of dev Javascript files : {' + str(total_original_size) + '}')
 
-		m = get_minifed_javascript_text(f)
-		ufo.create_file_or_override(m, minified_file_path)
-		minified_file_size = ufo.get_file_size_in_bytes(minified_file_path)
-
-		#print('Created {' + ufo.get_file_basename(minified_file_path) + '} - size{' + str(minified_file_size) + '}')
-		total_reduced_size += minified_file_size
-
-		#print()
-
-	print('Total size before : ' + str(total_original_size))
-	print('New size : ' + str(total_reduced_size))
-	print('Size reduction % : ' + str(1.0 - (total_reduced_size / total_original_size)))
+	#print('Total size before : ' + str(total_original_size))
+	#print('New size : ' + str(total_reduced_size))
+	#print('Size reduction % : ' + str(1.0 - (total_reduced_size / total_original_size)))
 
 
 def run_analysis():
 	"""Prints analysis report on the Quasar Source code base."""
 	all_javascript_files = _get_all_javascript_files()
-
 
 
 # Check if this file is being ran as a script.
