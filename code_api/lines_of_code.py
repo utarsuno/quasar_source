f8780bf4a90bc3c0ccab4a384feaed81c56fd065
utarsuno <utarsuno@gmail.com>
2017-11-25 23:57:59 -0600

    building code management tools 08

--- code_api/lines_of_code.py
+++ code_api/lines_of_code.py
@@ -19,6 +19,9 @@ class LineOfCode(object):
 	@property
 	def blank_line(self) -> bool:
 		"""Returns a boolean indicating if this line of code is actually just an empty line."""
+		print('LINE : ')
+		print(self._text)
+		print(len(self._text))
 		if self._text and self._text.strip():
 			return False
 		return True
