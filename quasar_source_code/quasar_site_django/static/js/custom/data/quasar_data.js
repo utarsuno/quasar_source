8c86af33ac4fd0b116e6a45aebace975588303a0
utarsuno <utarsuno@gmail.com>
2017-11-26 21:45:43 -0600

    general updating 22

--- quasar_source_code/quasar_site_django/static/js/custom/data/quasar_data.js
+++ quasar_source_code/quasar_site_django/static/js/custom/data/quasar_data.js
@@ -62,7 +62,7 @@ GlobalPostCall.prototype = {
             if (lines[i].includes('EW{{')) {
 
                 var temp = lines[i].replace('EW{{', '{').replace('}}', '}');
-                temp = temp.substring(temp.indexOf('- {'));
+                temp = temp.substring(temp.indexOf('- {') + 2);
 
                 console.log(temp);
 
