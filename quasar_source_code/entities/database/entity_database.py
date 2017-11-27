4c3143933cd4fe2a5717ca5faacdf04bc9ed1d5a
utarsuno <utarsuno@gmail.com>
2017-11-26 21:33:53 -0600

    general updating 16

--- quasar_source_code/entities/database/entity_database.py
+++ quasar_source_code/entities/database/entity_database.py
@@ -182,7 +182,9 @@ class EntityDatabaseAPI(object):
 		return_data = ''
 		for o in self._owners_cache:
 			return_data += str(o) + '\n'
-			return_data += o.get_entities_as_string_lines()
+			d = o.get_entities_as_string_lines()
+			for l in d:
+				return_data += l
 		return return_data
 
 	def get_data_on_all_owners(self):
