37c23d98c1d98e29f26745b844a806837494a929
utarsuno <utarsuno@gmail.com>
2017-11-30 02:24:58 -0600

    general updating 52

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/home_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/home_world.js
@@ -99,6 +99,9 @@ HomeWorld.prototype = {
                 break;
             }
 
+            l('Color chosen is :');
+            l(color);
+
             var day_of_week_word = get_day_of_week_as_word(get_just_date_object_of_date_of_n_days_offset(i - 7));
             var full_date_of_day = get_today_with_n_days_offset(i - 7);
 
