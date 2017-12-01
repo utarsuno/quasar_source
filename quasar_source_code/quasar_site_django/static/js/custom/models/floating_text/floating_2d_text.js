d97839c9f96f2744b80fc51494ed6eefed9e9528
utarsuno <utarsuno@gmail.com>
2017-12-01 01:06:16 -0600

    general updating 115

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_2d_text.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_2d_text.js
@@ -20,7 +20,7 @@ Floating2DText.prototype = {
 
     _update_text: function() {
         if (this.type == TYPE_BUTTON) {
-            this.dynamic_texture.clear('black').drawText(this.text, this.width / 2 + this.get_text_length() / 2, this.height, this.current_color, 'black');
+            this.dynamic_texture.clear('black').drawText(this.text, this.width / 2 + this.get_text_length() / 2, this.font_size, this.current_color, 'black');
         } else {
             this.dynamic_texture.clear('black').drawText(this.text, 0, this.font_size, this.current_color, 'black');
         }
@@ -29,7 +29,7 @@ Floating2DText.prototype = {
 
     _update_color: function() {
         if (this.type == TYPE_BUTTON) {
-            this.dynamic_texture.clear('black').drawText(this.text, this.width / 2 + this.get_text_length() / 2, this.height, this.current_color, 'black');
+            this.dynamic_texture.clear('black').drawText(this.text, this.width / 2 + this.get_text_length() / 2, this.font_size, this.current_color, 'black');
         } else {
             this.dynamic_texture.clear('black').drawText(this.text, 0, this.font_size, this.current_color, 'black');
         }
@@ -37,7 +37,7 @@ Floating2DText.prototype = {
     },
 
     initialize: function() {
-        if (this.type === TYPE_TITLE) {
+        if (this.type === TYPE_TITLE || this.type == TYPE_CONSTANT_TEXT || this.type == TYPE_CHECK_BOX) {
             this.height = 32;
         } else {
             this.height = 16;
