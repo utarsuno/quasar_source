1c2822fb5886327447e77a8efe42a3dd8b895571
utarsuno <utarsuno@gmail.com>
2017-12-01 00:07:22 -0600

    general updating 95

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/login_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/login_world.js
@@ -158,12 +158,12 @@ LoginWorld.prototype = {
         login_wall_normal.normalize();
 
         this.login_wall = new FloatingWall(login_wall_width, login_wall_height, login_wall_position, login_wall_normal, this);
-        this.login_button = this.login_wall.add_floating_2d_text(login_wall_width, 'login', TYPE_BUTTON, 0, 2, 10, 0);
+        this.login_button = this.login_wall.add_floating_2d_text(login_wall_width, 'login', TYPE_BUTTON, 0, 2, 8, 0);
         this.login_username_label = this.login_wall.add_floating_2d_text(login_wall_width / 4, 'username', TYPE_CONSTANT_TEXT, 0, 2, 1, 0);
-        this.login_username_input = this.login_wall.add_floating_2d_text(login_wall_width * (3 / 2), '', TYPE_INPUT_REGULAR, login_wall_width / 4, 2, 4, 0);
+        this.login_username_input = this.login_wall.add_floating_2d_text(login_wall_width * (3 / 4), '', TYPE_INPUT_REGULAR, login_wall_width / 4, 2, 1, 0);
 
         this.login_password_label = this.login_wall.add_floating_2d_text(login_wall_width / 4, 'password', TYPE_CONSTANT_TEXT, 0, 2, 2, 0);
-        this.login_password_input = this.login_wall.add_floating_2d_text(login_wall_width * (3 / 2), '', TYPE_INPUT_PASSWORD, login_wall_width / 4, 2, 3, 0);
+        this.login_password_input = this.login_wall.add_floating_2d_text(login_wall_width * (3 / 4), '', TYPE_INPUT_PASSWORD, login_wall_width / 4, 2, 2, 0);
         /*
 
         var edit_entity_save_changes_button = this.current_entity_editor.add_floating_2d_text(512, 'save changes', TYPE_BUTTON, 0, 2, key_values.length + 1, 0);
@@ -177,10 +177,10 @@ LoginWorld.prototype = {
         //this.login_wall.add_
 
 
-        var wall_create_account_width = 500;
-        var wall_create_account_height = 400;
-        var wall_create_account_position = new THREE.Vector3(300, 400, 650);
-        var wall_create_account_look_at = new THREE.Vector3(0, 400, 0);
+        var wall_create_account_width = 300;
+        var wall_create_account_height = 200;
+        var wall_create_account_position = new THREE.Vector3(300, wall_create_account_height, 650);
+        var wall_create_account_look_at = new THREE.Vector3(0, wall_create_account_height, 0);
         var wall_create_account_normal = new THREE.Vector3(wall_create_account_look_at.x - wall_create_account_position.x, wall_create_account_look_at.y - wall_create_account_position.y, wall_create_account_look_at.z - wall_create_account_position.z);
         wall_create_account_normal.normalize();
 
