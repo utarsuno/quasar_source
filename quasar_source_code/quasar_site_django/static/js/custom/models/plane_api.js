a1c421bd0b8c6e5eb8d43d8ed6e6ccb01aa375d7
utarsuno <utarsuno@gmail.com>
2017-12-02 10:38:33 -0600

    general updating and cleanup 45

--- quasar_source_code/quasar_site_django/static/js/custom/models/plane_api.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/plane_api.js
@@ -18,10 +18,12 @@ PlaneAPI.prototype = {
         this.width    = w;
         this.height   = h;
 
+        this.object3D = new THREE.Object3D();
+
         // PlaneGeometry takes in a width, height, optionalWidthSegments (default 1), optionalHeightSegments (default 1)
         this.geometry = new THREE.PlaneGeometry(this.width, this.height);
 
-        //this.object3d.add(this.geometry.mesh)
+        this.object3d.add(this.geometry.mesh)
 
         this.create();
     },
@@ -39,16 +41,33 @@ PlaneAPI.prototype = {
         //var mat = new THREE.LineBasicMaterial({color: 0xFFC0CB, linewidth: 3});
         //var wireframe = new THREE.LineSegments(geo, mat);
         //this.mesh.add(wireframe);
-    }
+    },
 
-
-
-    /*
     update_position_and_look_at: function(position_vector, look_at_position) {
         this.object3D.position.x = position_vector.x;
         this.object3D.position.y = position_vector.y;
         this.object3D.position.z = position_vector.z;
-        this.object3D.lookAt(look_at_position);
+
+        var look_at = new THREE.Vector3(look_at_position.x, look_at_position.y + this.height / 2, this.look_at_position.z);
+
+        this.object3D.lookAt(look_at);
     }
-    */
+
 };
+
+
+
+/*
+        var relative_x_shift = this.get_relative_x_shift(x_offset);
+        var y_position = this.get_y_position_for_row(row) + additional_y_offset;
+
+        var new_position = new THREE.Vector3(position_vector.x + relative_x_shift.x, position_vector.y + this.height / 2 + y_position + relative_x_shift.y, position_vector.z + relative_x_shift.z);
+        new_position.addScaledVector(this.depth_start, z_offset);
+
+        var new_look_at = new THREE.Vector3(position_vector.x + relative_x_shift.x, position_vector.y + this.height / 2 + y_position + relative_x_shift.y, position_vector.z + relative_x_shift.z);
+        new_look_at.addScaledVector(this.depth_start, z_offset);
+        new_look_at.addScaledVector(this.normal, 10);
+
+        floating_2d_text.update_position_and_look_at(new_position, new_look_at);
+
+ */
