d1a1054483bd8d284a79ac350378a7a444440259
utarsuno <utarsuno@gmail.com>
2017-12-04 20:05:40 -0600

    general updating 87

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_wall.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_wall.js
@@ -223,6 +223,7 @@ FloatingWall.prototype = {
             }
         } else {
             if (MANAGER_WORLD.current_world.floating_cursor.current_cursor.userData.name == CURSOR_TYPE_HAND) {
+                l('zzdasdfas');
                 this.perform_action(CURSOR_TYPE_HAND);
             }
         }
