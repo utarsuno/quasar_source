611a04130a7a235cd139b4b86a7930dd3da90038
utarsuno <utarsuno@gmail.com>
2017-12-24 00:09:33 -0600

    general updating 37

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_2d_text.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_2d_text.js
@@ -17,9 +17,12 @@ Floating2DText.prototype = {
 
     _update_text: function() {
         if (this.type === TYPE_BUTTON || this.type === TYPE_CHECK_BOX || this.type === TYPE_TITLE) {
-            this.dynamic_texture.clear('black').drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, 'black');
+            //this.dynamic_texture.clear('black').drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, 'black');
+            this.dynamic_texture.drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, 'black');
+
         } else {
-            this.dynamic_texture.clear('black').drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, 'black');
+            //this.dynamic_texture.clear('black').drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, 'black');
+            this.dynamic_texture.drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, 'black');
         }
         this.dynamic_texture.needsUpdate = true;
     },
@@ -110,10 +113,6 @@ Floating2DText.prototype = {
         */
 
         //
-        var outlineMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.BackSide } );
-        var outlineMesh = new THREE.Mesh(this.geometry, outlineMaterial);
-        outlineMesh.scale.multiplyScalar(1.1);
-        this.object3D.add(outlineMesh);
         //
 
         this.object3D.add(this.mesh);
