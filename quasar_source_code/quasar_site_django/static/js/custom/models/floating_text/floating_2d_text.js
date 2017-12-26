4587cae1989f70c607663df9c9f7177220326cd0
utarsuno <utarsuno@gmail.com>
2017-12-26 12:50:48 -0600

    general updating 162

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_2d_text.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_2d_text.js
@@ -14,23 +14,27 @@ Floating2DText.prototype = {
     background_color: null,
 
     _update_text: function() {
-        if (this.type === TYPE_BUTTON || this.type === TYPE_CHECK_BOX || this.type === TYPE_TITLE || this.type === TYPE_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE) {
-            this.dynamic_texture.clear(this.background_color).drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
-        } else {
-            this.dynamic_texture.clear(this.background_color).drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+        if (this.text !== ICON_LEFT && this.text !== ICON_RIGHT) {
+            if (this.type === TYPE_BUTTON || this.type === TYPE_CHECK_BOX || this.type === TYPE_TITLE || this.type === TYPE_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE) {
+                this.dynamic_texture.clear(this.background_color).drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+            } else {
+                this.dynamic_texture.clear(this.background_color).drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+            }
+            this.dynamic_texture.needsUpdate = true;
         }
-        this.dynamic_texture.needsUpdate = true;
     },
 
     // NOTE : Values are different for testing purposes.
 
     _update_color: function() {
-        if (this.type === TYPE_BUTTON || this.type === TYPE_CHECK_BOX || this.type === TYPE_TITLE || this.type === TYPE_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE) {
-            this.dynamic_texture.clear(this.background_color).drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
-        } else {
-            this.dynamic_texture.clear(this.background_color).drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+        if (this.text !== ICON_LEFT && this.text !== ICON_RIGHT) {
+            if (this.type === TYPE_BUTTON || this.type === TYPE_CHECK_BOX || this.type === TYPE_TITLE || this.type === TYPE_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE) {
+                this.dynamic_texture.clear(this.background_color).drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+            } else {
+                this.dynamic_texture.clear(this.background_color).drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+            }
+            this.dynamic_texture.needsUpdate = true;
         }
-        this.dynamic_texture.needsUpdate = true;
     },
 
     _update_width: function(width) {
