3ec9ea3dc68c9e44dd753b35455a8b8178ef48e0
utarsuno <utarsuno@gmail.com>
2017-12-25 22:27:37 -0600

    general updating 133

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_wall.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_wall.js
@@ -307,7 +307,7 @@ FloatingWall.prototype = {
     add_3D_title: function(title_name, color) {
         this.floating_3d_title = new Floating3DText(title_name, TYPE_TITLE, this.scene);
         //var x_shift    = this.get_relative_x_shift(-1.0 * (this.floating_3d_title.width / 2.0));
-        var x_shift = this.get_relative_x_shift(0);
+        var x_shift = this.get_relative_x_shift(-1.0 * (this.floating_3d_title.width / 2.0));
         var y_position = this.get_position_for_row(x_shift.x, x_shift.y + this.floating_3d_title.height / 2, x_shift.z);
         this.floating_3d_title.update_position_and_normal(y_position, this.normal);
 
