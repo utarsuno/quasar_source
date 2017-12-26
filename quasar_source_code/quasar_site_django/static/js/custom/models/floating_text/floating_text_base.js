87409761e554aa2495b7400d454ac92ea7f8318a
utarsuno <utarsuno@gmail.com>
2017-12-25 20:43:10 -0600

    general updating 128

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_text_base.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_text_base.js
@@ -6,8 +6,8 @@ function FloatingText(width, text, type, scene, current_color) {
         if (this.is_2D_text) {
             return this.dynamic_texture.getTextLength(this.text);
         } else {
-            var box = this.text_geometry.computeBoundingBox();
-            //var box = new THREE.Box3().setFromObject(this.text_geometry);
+            //var box = this.text_geometry.computeBoundingBox();
+            var box = new THREE.Box3().setFromObject(this.current_text_object);
             l(box);
             l(box.min);
             l(box.max);
