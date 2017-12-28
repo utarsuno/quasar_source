5ff10a4be7f665c58a7a10ec48a643160c311a58
utarsuno <utarsuno@gmail.com>
2017-12-28 02:16:47 -0600

    finance code updating 78

--- finance/finance.c
+++ finance/finance.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 // #include <stdlib.h>
 
-
 int main(const int argc, const char * argv[]) {
 	const float * all_opens = (float *) argv[1];
 
-    printf("Hello World{%f}\n", nums[0]);
-    printf("Hello World{%f}\n", nums[1]);
-    printf("Hello World{%f}\n", nums[2]);
-    printf("Hello World{%f}\n", nums[3]);
+    printf("Hello World{%f}\n", all_opens[0]);
+    printf("Hello World{%f}\n", all_opens[1]);
+    printf("Hello World{%f}\n", all_opens[2]);
+    printf("Hello World{%f}\n", all_opens[3]);
 
     return 0;
 }
