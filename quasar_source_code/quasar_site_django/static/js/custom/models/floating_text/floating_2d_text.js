f3e92fecf919ff73fe6579e4a367b83e25ad0289
utarsuno <utarsuno@gmail.com>
2017-12-28 22:55:11 -0600

    general updating 20

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_2d_text.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_text/floating_2d_text.js
@@ -16,9 +16,9 @@ Floating2DText.prototype = {
     _update_text: function() {
         if (this.text !== ICON_LEFT && this.text !== ICON_RIGHT && this.text !== ICON_CROSS) {
             if (this.type === TYPE_BUTTON || this.type === TYPE_CHECK_BOX || this.type === TYPE_TITLE || this.type === TYPE_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE) {
-                this.dynamic_texture.clear(this.background_color).drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+                this.dynamic_texture.clear().drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
             } else {
-                this.dynamic_texture.clear(this.background_color).drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+                this.dynamic_texture.clear().drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
             }
             this.dynamic_texture.needsUpdate = true;
         }
@@ -29,9 +29,9 @@ Floating2DText.prototype = {
     _update_color: function() {
         if (this.text !== ICON_LEFT && this.text !== ICON_RIGHT && this.text !== ICON_CROSS) {
             if (this.type === TYPE_BUTTON || this.type === TYPE_CHECK_BOX || this.type === TYPE_TITLE || this.type === TYPE_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE_CONSTANT || this.type === TYPE_SUPER_TITLE) {
-                this.dynamic_texture.clear(this.background_color).drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+                this.dynamic_texture.clear().drawText(this.text, this.texture_width / 2 - this.get_text_length() / 2, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
             } else {
-                this.dynamic_texture.clear(this.background_color).drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
+                this.dynamic_texture.clear().drawText(this.text, 0, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.background_color);
             }
             this.dynamic_texture.needsUpdate = true;
         }
@@ -64,7 +64,7 @@ Floating2DText.prototype = {
             });
             this.material.transparent = true;
         } else {
-            this.texture_width = get_nearest_power_of_two_for_number(this.width * 1);
+            this.texture_width = get_nearest_power_of_two_for_number(this.width * 2);
             var texture_height = get_nearest_power_of_two_for_number(this.height * 2);
             //var font_size = Math.round(texture_height * .8);
             this.font_size = texture_height;
