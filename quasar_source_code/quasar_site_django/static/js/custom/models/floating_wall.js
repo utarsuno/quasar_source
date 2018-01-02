a2cae34035b38581a66a47b128c8e0c229869520
utarsuno <utarsuno@gmail.com>
2018-01-02 01:24:14 -0600

    general updating 31

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_wall.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_wall.js
@@ -492,8 +492,8 @@ FloatingWall.prototype = {
             // 0 - COLOR_HEX_INDEX
             c = c[0];
         }
-        this.mesh.material.color.setHex(c);
-        this.mesh.material.needsUpdate = true;
+        this.material.color.setHex(c);
+        this.material.needsUpdate = true;
     },
 
     wheel_event: function(delta) {
