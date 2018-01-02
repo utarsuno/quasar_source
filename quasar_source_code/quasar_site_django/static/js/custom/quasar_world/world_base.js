fef2769cbfb4177dd89fcbe7c8f319ee4aeaddaa
utarsuno <utarsuno@gmail.com>
2018-01-02 13:05:32 -0600

    general updating 71

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/world_base.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/world_base.js
@@ -300,8 +300,7 @@ function World(planet_name) {
             if (!this.currently_looked_at_object.is_engaged()) {
                 this.currently_looked_at_object.engage();
             }
-        }
-        if (MANAGER_WORLD.current_floating_cursor.engaged) {
+        } else if (MANAGER_WORLD.current_floating_cursor.engaged) {
             MANAGER_WORLD.current_floating_cursor.disengage();
         }
     };
