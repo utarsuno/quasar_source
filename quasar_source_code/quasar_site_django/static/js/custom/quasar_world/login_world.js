5cd9d8a45ddbb88680bc2dc7bb52783d9b6f23e4
utarsuno <utarsuno@gmail.com>
2018-01-02 03:51:24 -0600

    general updating 58

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/login_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/login_world.js
@@ -215,12 +215,12 @@ LoginWorld.prototype = {
         this.login_username_label = this.login_wall.add_floating_2d_text(0, 1 / 3, 'username', TYPE_CONSTANT_TEXT, 0);
         this.login_username_input = this.login_wall.add_floating_2d_text(1 / 3, 1, '', TYPE_INPUT_REGULAR, 0);
         this.login_username_input.set_syntax_type(TEXT_SYNTAX_FOUR_MINIMUM);
-        this.login_username_input.set_value_changed_function(this._text_error_check.bind(this, this.login_username_input, ERROR_CHECK_TYPE_LOGIN, this.login_errors));
+        this.login_username_input.set_value_changed_function(this._text_error_check.bind(this, this.login_username_input.mesh.uuid, ERROR_CHECK_TYPE_LOGIN, this.login_errors));
 
         this.login_password_label = this.login_wall.add_floating_2d_text(0, 1 / 3, 'password', TYPE_CONSTANT_TEXT, 1);
         this.login_password_input = this.login_wall.add_floating_2d_text(1 / 3, 1, '', TYPE_INPUT_PASSWORD, 1);
         this.login_password_input.set_syntax_type(TEXT_SYNTAX_PASSWORD);
-        this.login_password_input.set_value_changed_function(this._text_error_check.bind(this, this.login_password_input, ERROR_CHECK_TYPE_LOGIN, this.login_errors));
+        this.login_password_input.set_value_changed_function(this._text_error_check.bind(this, this.login_password_input.mesh.uuid, ERROR_CHECK_TYPE_LOGIN, this.login_errors));
 
         // TODO :
         //this.login_remember_username_label = this.login_wall.add_floating_2d_text(login_wall_width / 2, 'remember username', TYPE_CONSTANT_TEXT, 0, 2, 2, 0);
@@ -248,22 +248,22 @@ LoginWorld.prototype = {
         this.create_account_username_label = this.wall_create_account.add_floating_2d_text(0, 1 / 3, 'username', TYPE_CONSTANT_TEXT, 0);
         this.create_account_username_input = this.wall_create_account.add_floating_2d_text(1 / 3, 1, '', TYPE_INPUT_REGULAR, 0);
         this.create_account_username_input.set_syntax_type(TEXT_SYNTAX_FOUR_MINIMUM);
-        this.create_account_username_input.set_value_changed_function(this._text_error_check.bind(this, this.create_account_username_input, ERROR_CHECK_TTPE_CREATE_ACCOUNT, this.create_errors));
+        this.create_account_username_input.set_value_changed_function(this._text_error_check.bind(this, this.create_account_username_input.mesh.uuid, ERROR_CHECK_TTPE_CREATE_ACCOUNT, this.create_errors));
 
         this.create_account_email_label = this.wall_create_account.add_floating_2d_text(0, 1 / 3, 'email', TYPE_CONSTANT_TEXT, 1);
         this.create_account_email_input = this.wall_create_account.add_floating_2d_text(1 / 3, 1, '', TYPE_INPUT_REGULAR, 1);
         this.create_account_email_input.set_syntax_type(TEXT_SYNTAX_EMAIL);
-        this.create_account_email_input.set_value_changed_function(this._text_error_check.bind(this, this.create_account_email_input, ERROR_CHECK_TTPE_CREATE_ACCOUNT, this.create_errors));
+        this.create_account_email_input.set_value_changed_function(this._text_error_check.bind(this, this.create_account_email_input.mesh.uuid, ERROR_CHECK_TTPE_CREATE_ACCOUNT, this.create_errors));
 
         this.create_account_password_label = this.wall_create_account.add_floating_2d_text(0, 1 / 3, 'password', TYPE_CONSTANT_TEXT, 2);
         this.create_account_password_input = this.wall_create_account.add_floating_2d_text(1 / 3, 1, '', TYPE_INPUT_PASSWORD, 2);
         this.create_account_password_input.set_syntax_type(TEXT_SYNTAX_PASSWORD);
-        this.create_account_password_input.set_value_changed_function(this._text_error_check.bind(this, this.create_account_password_input, ERROR_CHECK_TTPE_CREATE_ACCOUNT, this.create_errors));
+        this.create_account_password_input.set_value_changed_function(this._text_error_check.bind(this, this.create_account_password_input.mesh.uuid, ERROR_CHECK_TTPE_CREATE_ACCOUNT, this.create_errors));
 
         this.create_account_password_repeat_label = this.wall_create_account.add_floating_2d_text(0, 1 / 3, 'repeat password', TYPE_CONSTANT_TEXT, 3);
         this.create_account_password_repeat_input = this.wall_create_account.add_floating_2d_text(1 / 3, 1, '', TYPE_INPUT_PASSWORD, 3);
         this.create_account_password_repeat_input.set_syntax_type(TEXT_SYNTAX_PASSWORD);
-        this.create_account_password_repeat_input.set_value_changed_function(this._text_error_check.bind(this, this.create_account_password_repeat_input, ERROR_CHECK_TTPE_CREATE_ACCOUNT, this.create_errors));
+        this.create_account_password_repeat_input.set_value_changed_function(this._text_error_check.bind(this, this.create_account_password_repeat_input.mesh.uuid, ERROR_CHECK_TTPE_CREATE_ACCOUNT, this.create_errors));
 
         this.create_account_button = this.wall_create_account.add_floating_2d_text(.25, .75, 'create account', TYPE_BUTTON, 5);
         this.create_account_button.set_engage_function(this.create_account_button_pressed.bind(this));
