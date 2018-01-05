59597c7cd7c344a0d95bf63ee2907b7f6a31a2a2
utarsuno <utarsuno@gmail.com>
2018-01-05 16:04:28 -0600

    general updating 33

---
+++ utility_server/quasar/quasar_server_test.py
@@ -0,0 +1,15 @@
+
+
+
+from utility_server.quasar import quasar_server as qs
+from entities import base_entity as be
+
+
+quasar_server = qs.QuasarServer()
+quasar_server.connect()
+
+owner_data = {be.ENTITY_PROPERTY_USERNAME: 'aaaa',
+              be.ENTITY_PROPERTY_EMAIL   : 'bbbb@bbbb.com',
+              be.ENTITY_PROPERTY_PASSWORD: 'cccc'}
+
+result = quasar_server.create_entity_owner(owner_data)
