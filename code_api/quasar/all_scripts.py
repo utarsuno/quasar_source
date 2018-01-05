d988ab758ee97ec368d85fae6b77b431612821ff
utarsuno <utarsuno@gmail.com>
2018-01-05 12:56:03 -0600

    general updating 14

--- code_api/quasar/all_scripts.py
+++ code_api/quasar/all_scripts.py
@@ -178,13 +178,13 @@ entity_terminate = entity.add_code_file(TERMINATE)
 entity_terminate.require_start_and_stop_print()
 entity_terminate.add_required_safety_check(ssg.SAFETY_CHECK_ONLY_ALLOW_SUDO)
 entity_terminate.add_required_safety_check(ssg.SAFETY_CHECK_ONLY_ALLOW_UBUNTU)
-entity_terminate.add_main_logic('''sudo pkill -f "runserver"''')
+entity_terminate.add_main_logic('''sudo pkill -f "/home/git_repos/quasar_source/entities/server/entity_server.py"''')
 
 # Entity run script.
 entity_live_run = entity.add_code_file(LIVE_RUN)
 entity_live_run.require_start_and_stop_print()
 entity_live_run.add_required_safety_check(ssg.SAFETY_CHECK_ONLY_ALLOW_UBUNTU)
-entity_live_run.add_main_logic('''is_entity_server_running=$(python3 /home/git_repos/quasar_source/all_scripts/universal/is_program_running.py 'quasar_live_run_flag')
+entity_live_run.add_main_logic('''is_entity_server_running=$(python3 /home/git_repos/quasar_source/all_scripts/universal/is_program_running.py '/home/git_repos/quasar_source/entities/server/entity_server.py')
 if [ "${is_entity_server_running}" == "true" ]; then
   echo 'Entity server is already running!'
 else
@@ -197,7 +197,7 @@ entity_run_in_background = entity.add_code_file(RUN_IN_BACKGROUND)
 entity_run_in_background.require_start_and_stop_print()
 entity_run_in_background.add_required_safety_check(ssg.SAFETY_CHECK_ONLY_ALLOW_UBUNTU)
 entity_run_in_background.add_required_safety_check(ssg.SAFETY_CHECK_ONLY_ALLOW_SUDO)
-entity_run_in_background.add_main_logic('''is_entity_server_running=$(python3 /home/git_repos/quasar_source/all_scripts/universal/is_program_running.py 'quasar_live_run_flag')
+entity_run_in_background.add_main_logic('''is_entity_server_running=$(python3 /home/git_repos/quasar_source/all_scripts/universal/is_program_running.py '/home/git_repos/quasar_source/entities/server/entity_server.py')
 if [ "${is_entity_server_running}" == "true" ]; then
   echo 'Entity server is already running!'
 else
