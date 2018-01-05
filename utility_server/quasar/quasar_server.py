d92065a38eb3a0be1a6c3d68e46ca7e3fbf4000f
utarsuno <utarsuno@gmail.com>
2018-01-05 14:22:36 -0600

    general updating 19

---
+++ utility_server/quasar/quasar_server.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+"""This module, quasar_server.py, is the utility quasar client to the entity server."""
+
+from utility_server import utility_servers as us
+
+
+class QuasarServer(object):
+	"""Represents the Quasar utility server."""
+
+	def __init__(self):
+		#self._host_server_data = ufo.get_ini_section_dictionary(pm.get_config_ini(), SERVER_QUASAR)
+		#self._host_server = HostServer(SERVER_QUASAR, self._host_server_data[ADDRESS], self._host_server_data[PORT])
+
+		# Client connection to the entity data server.
+		self._entity_server_data = ufo.get_ini_section_dictionary(pm.get_config_ini(), SERVER_ENTITY)
+		self._entity_server_connection = ClientConnection('client:' + SERVER_ENTITY, self._entity_server_data[ADDRESS], self._entity_server_data[PORT])
+
+	def run(self):
+		"""Runs the Quasar server."""
+		self._entity_server_connection.attempt_connection()
+
+		response = self._entity_server_connection.send_message('Hello from client 0')
+		print('Client got this response : ' + response)
+		time.sleep(2)
+		self._entity_server_connection.send_message('Hello from client 1')
+		print('Client got this response : ' + response)
+		time.sleep(4)
+		self._entity_server_connection.send_message('Hello from client 2')
+		print('Client got this response : ' + response)
