10604d9332687a3568f8c13c8d3500d72e321b8e
utarsuno <utarsuno@gmail.com>
2018-01-06 16:38:07 -0600

    general updating 101

--- servers/quasar/quasar_server.py
+++ servers/quasar/quasar_server.py
@@ -7,7 +7,8 @@ from servers import utility_servers as us
 import time
 from universal_code import path_manager as pm
 from universal_code import useful_file_operations as ufo
-
+from entities import base_entity as be
+from universal_code import system_os as so
 
 class QuasarServer(object):
 	"""Represents the Quasar utility server."""
@@ -66,3 +67,46 @@ class QuasarServer(object):
 	def delete_entity_owner(self, username):
 		"""Deletes the entity owner found by username match."""
 		return self._send_command_to_entity_server(us.SERVER_COMMAND_DELETE_ENTITY_OWNER, username)
+
+
+
+'''___  __   __                  ___     __                         __
+  |__  /  \ |__)    |    | \  / |__     |__) |  | |\ | |\ | | |\ | / _`
+  |    \__/ |  \    |___ |  \/  |___    |  \ \__/ | \| | \| | | \| \__> '''
+
+COMMAND_DELETE_OWNER = 'do'
+COMMAND_CREATE_OWNER = 'co'
+COMMAND_PRINT_ALL_DATA = 'pd'
+
+
+arguments = so.get_all_program_arguments()
+if len(arguments) == 1:
+	flag = arguments[0]
+	first_match_hit = False
+	command = ''
+	data = ''
+	for c in flag:
+		if c == ':':
+			if not first_match_hit:
+				first_match_hit = True
+			else:
+				data += c
+		else:
+			if first_match_hit:
+				data += c
+			else:
+				command += c
+
+	quasar_server = QuasarServer()
+	quasar_server.connect()
+
+	if command == COMMAND_CREATE_OWNER:
+		owner_data = {be.ENTITY_PROPERTY_USERNAME: data,
+		              be.ENTITY_PROPERTY_EMAIL   : data + '@' + data + '.com',
+		              be.ENTITY_PROPERTY_PASSWORD: data}
+		print(quasar_server.create_entity_owner(owner_data))
+	elif command == COMMAND_PRINT_ALL_DATA:
+		print('Printing all data!')
+		quasar_server.get_all_data()
+	elif command == COMMAND_DELETE_OWNER:
+		print(quasar_server.delete_entity_owner(data))
