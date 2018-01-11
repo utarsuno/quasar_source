9538fead27e53dc6fbda722b03d476486a56e65b
utarsuno <utarsuno@gmail.com>
2018-01-10 22:51:06 -0600

    general updating 114

--- quasar_source_code/quasar_site_django/static/js/custom/models/inheritables/button.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/inheritables/button.js
@@ -4,6 +4,9 @@ function InheritableButton() {
 
     // Used for buttons.
     this.disable = function() {
+        l('Previous color is : ');
+        l(this.previous_default_color);
+        l(this.default_color);
         this.previous_default_color = this.default_color;
         this.set_default_color(COLOR_RED);
         this._disabled = true;
