bef268b8a83cca941e9f054186d9e725fdbe54e2
utarsuno <utarsuno@gmail.com>
2018-01-12 15:23:24 -0600

    general updating 52

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/world_base.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/world_base.js
@@ -22,16 +22,16 @@ function World(planet_name) {
     // Base code from : https://codepen.io/asjas/pen/pWawPm
     var Element = function ( id, x, y, z, ry ) {
         var div = document.createElement( 'div' );
-        div.style.width = '480px';
-        div.style.height = '360px';
+        div.style.width = '4800px'; // was 480
+        div.style.height = '3600px'; // was 360
         div.style.backgroundColor = '#000';
         var iframe = document.createElement( 'iframe' );
-        iframe.style.width = '480px';
-        iframe.style.height = '360px';
-        iframe.style.border = '0px';
+        iframe.style.width = '4800px';
+        iframe.style.height = '3600px';
+        iframe.style.border = '50px';
         iframe.src = [ 'https://www.youtube.com/embed/', id, '?rel=0'   ].join( '' );
         div.appendChild( iframe );
-        var object = new THREE.CSS3DObject( div );
+        var object = new THREE.CSS3DObject(div);
         object.position.set( x, y, z );
         object.rotation.y = ry;
         return object;
