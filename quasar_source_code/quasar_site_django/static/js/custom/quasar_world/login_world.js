710b4eb7393d305946fc83f053abd47e3b4802aa
utarsuno <utarsuno@gmail.com>
2018-01-12 01:37:40 -0600

    general updating 33

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/login_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/login_world.js
@@ -107,7 +107,7 @@ LoginWorld.prototype = {
         var login_wall_normal = new THREE.Vector3(login_wall_look_at.x - login_wall_position.x, login_wall_look_at.y - login_wall_position.y, login_wall_look_at.z - login_wall_position.z);
 
         this.login_wall = new FloatingWall(login_wall_width, login_wall_height, login_wall_position, login_wall_normal, this, false);
-        this.login_wall.add_3D_title('Login', TYPE_SUPER_TITLE, null, 0);
+        this.login_wall.add_3D_title('Login', TYPE_TITLE, null, 0);
 
         this.login_username_label = this.login_wall.add_floating_2d_text(0, 1 / 3, 'username', TYPE_CONSTANT, 0);
         this.login_username_input = this.login_wall.add_floating_2d_text(1 / 3, 1, '', TYPE_INPUT, 0, [TEXT_SYNTAX_STANDARD_LENGTH]);
@@ -140,7 +140,7 @@ LoginWorld.prototype = {
         var wall_create_account_normal = new THREE.Vector3(wall_create_account_look_at.x - wall_create_account_position.x, wall_create_account_look_at.y - wall_create_account_position.y, wall_create_account_look_at.z - wall_create_account_position.z);
 
         this.wall_create_account = new FloatingWall(wall_create_account_width, wall_create_account_height, wall_create_account_position, wall_create_account_normal, this, false);
-        this.wall_create_account.add_3D_title('Create Account', TYPE_SUPER_TITLE, null, 0);
+        this.wall_create_account.add_3D_title('Create Account', TYPE_TITLE, null, 0);
 
         this.create_account_username_label = this.wall_create_account.add_floating_2d_text(0, 1 / 3, 'username', TYPE_CONSTANT, 0);
         this.create_account_username_input = this.wall_create_account.add_floating_2d_text(1 / 3, 1, '', TYPE_INPUT, 0, [TEXT_SYNTAX_STANDARD_LENGTH]);
