2046a9dcb971c81d7a9708a8460327d5d79dc97a
utarsuno <utarsuno@gmail.com>
2018-01-18 21:39:53 -0600

    broad refactoring 144

--- quasar_source_code/quasar_site_django/static/js/custom/models/floating_cursor.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/floating_cursor.js
@@ -7,21 +7,40 @@ function FloatingCursor(world) {
 FloatingCursor.prototype = {
 
     __init__: function(world) {
-        this.world = world;
+        this.world                 = world;
+        this._cursors              = null;
         this.currently_attached_to = null;
+
+        this._current_cursor       = null;
+        this._previous_cursor      = null;
     },
 
     attach: function(object_to_attach_to) {
-        l('TODO : Cursor attach!');
+        this.currently_attached_to = object_to_attach_to;
+
+        if (this.currently_attached_to.type === TYPE_BUTTON || this.currently_attached_to.type === TYPE_CHECK_BOX) {
+            this._current_cursor = this._cursors[CURSOR_TYPE_HAND];
+        } else {
+            this._current_cursor = this._cursors[CURSOR_TYPE_POINTER];
+        }
+
+        this._current_cursor.set_to_visible();
     },
 
     detach: function() {
-        l('TODO : Cursor detach!');
+        this.currently_attached_to = null;
+        this._current_cursor.set_to_invisible();
     },
 
     update_position: function(p) {
         //l('TODO : Update cursor position!');
-        l(p.point);
+        //l(p.point);
+
+        var normal = this.currently_attached_to.get_normal();
+
+        this.cursor_wall.set_position(p.point.x, p.point.y, p.point.z, false);
+        this.cursor_wall.set_normal(normal.x, normal.y, normal.z, false);
+        this.cursor_wall.refresh_position_and_look_at();
     },
 
 
@@ -118,7 +137,8 @@ FloatingCursor.prototype = {
      | |\ | |  |  |  /\  |       |    /  \  /\  |  \
      | | \| |  |  | /~~\ |___    |___ \__/ /~~\ |__/ */
     create: function() {
-        this.cursor_wall = new FloatingWall(16, 16, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), this.world, false);
+        this.cursor_wall = new FloatingWall(16, 16, null, null, this.world, false);
+        this.cursor_wall.set_to_invisible();
         this._create_cursor(CURSOR_TYPE_HORIZONTAL);
         this._create_cursor(CURSOR_TYPE_VERTICAL);
         this._create_cursor(CURSOR_TYPE_HAND);
@@ -129,8 +149,9 @@ FloatingCursor.prototype = {
 
     _create_cursor: function(cursor_type) {
         var c = this.cursor_wall.add_row_2D_text([0, 1], 0, cursor_type, TYPE_ICON);
-
         c.set_attachment_depth_offset(1);
+        c.set_to_invisible();
+        this._cursors[cursor_type] = c;
     }
 
 };
