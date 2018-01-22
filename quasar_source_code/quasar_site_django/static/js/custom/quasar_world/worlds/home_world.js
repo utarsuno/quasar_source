031cf485cca0b441d140e212fc8444d34ed9e700
utarsuno <utarsuno@gmail.com>
2018-01-22 11:21:11 -0600

    general updating 46

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/home_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/home_world.js
@@ -101,10 +101,12 @@ HomeWorld.prototype = {
         var month_day_wall = new FloatingWall(w, h, p, n, this, false);
 
         month_day_wall.add_row_3D_text(false, -1, day.get_day_number(), TYPE_SUPER_TITLE, this.month_days.get_day_color_by_index(index));
-        
+
         if (present) {
             month_day_wall.add_row_3D_text(false, -2, 'Today', TYPE_SUPER_TITLE, this.month_days.get_day_color_by_index(index));
         }
+
+        month_day_wall.refresh_position_and_look_at();
 
         return month_day_wall;
     },
