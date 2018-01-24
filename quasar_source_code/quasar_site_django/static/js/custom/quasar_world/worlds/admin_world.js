137d301e408a292dc4022d602ee2984549b12d64
utarsuno <utarsuno@gmail.com>
2018-01-23 18:22:26 -0600

    general updating 95

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/admin_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/admin_world.js
@@ -9,10 +9,21 @@ AdminWorld.prototype = {
     __init__: function () {
         World.call(this);
 
-
+        
 
     },
 
     create: function() {
+    },
+
+    enter_world: function() {
+        CURRENT_PLAYER.disengage();
+        if (!GUI_PAUSED_MENU.currently_displayed) {
+            CURRENT_PLAYER.enable_controls();
+        }
+        CURRENT_PLAYER.set_position_xyz(0, 100, 0);
+    },
+
+    exit_world: function() {
     }
 };
