45ce9ec46b413b096fc7152bdbfd10d0bdf8331e
utarsuno <utarsuno@gmail.com>
2018-01-26 13:21:59 -0600

    general updating 155

--- quasar_source_code/quasar_site_django/static/js/custom/models/inheritables/visibility.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/inheritables/visibility.js
@@ -40,10 +40,30 @@ function Visibility() {
 
     this.display_self_and_all_child_attachments_recursively = function() {
         this.set_to_visible();
+        for (var a = 0; a < this.attachments.length; a++) {
+            if (!this.attachments[a].manual_visibility) {
+                this.attachments[a].display_self_and_all_child_attachments_recursively();
+            }
+        }
+    };
+
+    /*
+    this.display_self_and_all_child_attachments_recursively = function() {
+        this.set_to_visible();
         var all_attachments = this._get_all_attachments_recursively();
         for (var a = 0; a < all_attachments.length; a++) {
             if (!all_attachments[a].manual_visibility) {
                 all_attachments[a].set_to_visible();
+            }
+        }
+    };
+    */
+
+    this.hide_self_and_all_child_attachments_recursively = function() {
+        this.set_to_invisible();
+        for (var a = 0; a < this.attachments.length; a++) {
+            if (!this.attachments[a].manual_visibility) {
+                this.attachments[a].hide_self_and_all_child_attachments_recursively();
             }
         }
     };
