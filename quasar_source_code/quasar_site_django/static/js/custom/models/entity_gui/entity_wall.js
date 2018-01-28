d6ed3267b1c6cd0dbaebc1d1f350998e2cb2b708
utarsuno <utarsuno@gmail.com>
2018-01-28 12:36:05 -0600

    adding schedule features 28

--- quasar_source_code/quasar_site_django/static/js/custom/models/entity_gui/entity_wall.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/entity_gui/entity_wall.js
@@ -167,13 +167,11 @@ EntityWall.prototype = {
     },
 
     _delete_entity_field: function(row_number, field_name) {
-        l(this.date_selector.wall_date_selector.attachment_parent);
-
         if (field_name === ENTITY_PROPERTY_DUE_DATE) {
+            l(this.date_selector.wall_date_selector.attachment_parent);
             this.date_selector.wall_date_selector.detach_from_parent();
+            l(this.date_selector.wall_date_selector.attachment_parent);
         }
-
-        l(this.date_selector.wall_date_selector.attachment_parent);
 
         this.wall_create_new_entity.delete_row(row_number);
 
