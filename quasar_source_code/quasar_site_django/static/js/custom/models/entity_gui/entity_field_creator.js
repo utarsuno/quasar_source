c6d449f00f8158e0ce6fd3f75ea3c15d9a0f5f75
utarsuno <utarsuno@gmail.com>
2018-02-04 00:32:27 -0600

    schedule features 58

--- quasar_source_code/quasar_site_django/static/js/custom/models/entity_gui/entity_field_creator.js
+++ quasar_source_code/quasar_site_django/static/js/custom/models/entity_gui/entity_field_creator.js
@@ -18,6 +18,7 @@ EntityFieldCreator.prototype = {
     _add_entity_field_button_pressed: function() {
         this.create();
         this.wall_add_new_field.force_display_self_and_all_child_attachments_recursively();
+        l('Display the add new entity field!');
     },
 
     create: function() {
