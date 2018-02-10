5b41f9570fbb4a904dbb6895705b383bf680f356
utarsuno <utarsuno@gmail.com>
2018-02-10 16:17:47 -0600

    schedule view abstraction 09

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/world_manager.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/world_manager.js
@@ -107,7 +107,7 @@ WorldManager.prototype = {
 
         world.floating_cursor.create();
 
-        var skybox_geometry = new THREE.BoxGeometry(20000, 20000, 20000);
+        var skybox_geometry = new THREE.BoxGeometry(22500, 22500, 22500);
         var skybox_cube = new THREE.Mesh(skybox_geometry, MANAGER_LOADING.sky_box_material);
         skybox_cube.position.set(0, 0, 0);
         world.add_to_scene(skybox_cube);
