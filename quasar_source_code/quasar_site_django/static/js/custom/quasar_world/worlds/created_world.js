4295a5c2561924503df416ae9472ec6b35fd68d7
utarsuno <utarsuno@gmail.com>
2018-02-16 19:06:28 -0600

    shared worlds update 07

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/created_world.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/created_world.js
@@ -40,7 +40,7 @@ CreatedWorld.prototype = {
         // TODO : This functionality.
         current_row.add_2D_button([0, 1], 'Share With Player', null, null);
 
-        this.loaded = true;
+        this.world_wall.refresh_position_and_look_at();
     },
 
     prepare_for_save: function() {
