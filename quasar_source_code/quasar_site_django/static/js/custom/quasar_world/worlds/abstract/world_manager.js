626de8ba85cd1a5309eb12fee53f70055cdf7721
utarsuno <utarsuno@gmail.com>
2018-02-16 20:33:20 -0600

    shared worlds update 12

--- quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/abstract/world_manager.js
+++ quasar_source_code/quasar_site_django/static/js/custom/quasar_world/worlds/abstract/world_manager.js
@@ -97,12 +97,6 @@ WorldManager.prototype = {
         // TODO : Make sure dynamic worlds are dealt with.
     },
 
-    /*
-            MANAGER_WORLD.add_world_to_teleport_menu(created_world);
-        add_personal_teleport_button
-
-     */
-
     add_world_to_teleport_menu: function(world) {
         this.world_home.player_menu.add_personal_teleport_button(world);
         this.world_settings.player_menu.add_personal_teleport_button(world);
