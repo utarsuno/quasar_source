67fa52832d61e3653135b0d8a810f1650fcb06b6
utarsuno <utarsuno@gmail.com>
2018-02-28 21:25:11 -0600

    general updating 37

--- quasar_site_django/static/js/custom/worlds/worlds/abstract/world_input.js
+++ quasar_site_django/static/js/custom/worlds/worlds/abstract/world_input.js
@@ -5,6 +5,39 @@ function WorldInput() {
     this.key_down_event_for_interactive_objects = function(event) {
         if (event.keyCode === KEY_CODE_TAB) {
             this.tab_to_next_interactive_object();
+            //event.preventDefault()
+            event.stopPropagation();
+        } else if (this.currently_looked_at_object !== null) {
+            //this.currently_looked_at_object.parse_keycode(event);
+        }
+
+        if (this.currently_looked_at_object !== null) {
+            if (this.currently_looked_at_object.is_engaged() || !this.currently_looked_at_object.needs_engage_for_parsing_input) {
+                this.currently_looked_at_object.parse_keycode(event);
+            }
+        }
+        if (event.keyCode === KEY_CODE_ENTER) {
+            if (this.currently_looked_at_object !== null) {
+                if (!this.currently_looked_at_object.is_engaged()) {
+                    if (this.currently_looked_at_object.hasOwnProperty('_disabled')) {
+                        if (!this.currently_looked_at_object['_disabled']) {
+                            this.currently_looked_at_object.engage();
+                        }
+                    } else {
+                        this.currently_looked_at_object.engage();
+                    }
+                }
+            }
+        }
+
+        // No defaults will be useful (for now).
+        event.preventDefault();
+    };
+
+
+    this.key_down_event_for_interactive_objectsOLLDDD = function(event) {
+        if (event.keyCode === KEY_CODE_TAB) {
+            this.tab_to_next_interactive_object();
             event.stopPropagation();
         } else if (is_defined(this.currently_looked_at_object)) {
             if (!this.currently_looked_at_object.is_engaged() || !this.currently_looked_at_object.needs_engage_for_parsing_input) {
