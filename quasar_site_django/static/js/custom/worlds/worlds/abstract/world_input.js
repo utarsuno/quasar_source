696c6d4e189e160ec019512bb48c22583cc840ab
utarsuno <utarsuno@gmail.com>
2018-02-28 21:23:24 -0600

    general updating 36

--- quasar_site_django/static/js/custom/worlds/worlds/abstract/world_input.js
+++ quasar_site_django/static/js/custom/worlds/worlds/abstract/world_input.js
@@ -7,18 +7,43 @@ function WorldInput() {
             this.tab_to_next_interactive_object();
             event.stopPropagation();
         } else if (is_defined(this.currently_looked_at_object)) {
-
             if (!this.currently_looked_at_object.is_engaged() || !this.currently_looked_at_object.needs_engage_for_parsing_input) {
                 // Object is currently engaged or does not need engage for parsing input.
                 this.currently_looked_at_object.parse_keycode(event);
             } else {
                 // Object is not currently engaged.
-                if (this.currently_looked_at_object.hasOwnProperty('_disabled')) {
-                    if (!this.currently_looked_at_object['_disabled']) {
+                if (event.keyCode === KEY_CODE_ENTER) {
+                    if (this.currently_looked_at_object.hasOwnProperty('_disabled')) {
+                        if (!this.currently_looked_at_object['_disabled']) {
+                            this.currently_looked_at_object.engage();
+                        }
+                    } else {
                         this.currently_looked_at_object.engage();
                     }
-                } else {
-                    this.currently_looked_at_object.engage();
+                }
+            }
+        }
+
+        // No defaults will be useful (for now).
+        event.preventDefault();
+    };
+
+    this.key_down_event_for_interactive_objectsOLD = function(event) {
+        if (this.currently_looked_at_object !== null) {
+            if (this.currently_looked_at_object.is_engaged() || !this.currently_looked_at_object.needs_engage_for_parsing_input) {
+                this.currently_looked_at_object.parse_keycode(event);
+            }
+        }
+        if (event.keyCode === KEY_CODE_ENTER) {
+            if (this.currently_looked_at_object !== null) {
+                if (!this.currently_looked_at_object.is_engaged()) {
+                    if (this.currently_looked_at_object.hasOwnProperty('_disabled')) {
+                        if (!this.currently_looked_at_object['_disabled']) {
+                            this.currently_looked_at_object.engage();
+                        }
+                    } else {
+                        this.currently_looked_at_object.engage();
+                    }
                 }
             }
         }
