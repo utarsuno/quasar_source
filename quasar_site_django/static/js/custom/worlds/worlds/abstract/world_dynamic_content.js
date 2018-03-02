97b39bdcfc37a5875402336503bad64178053861
utarsuno <utarsuno@gmail.com>
2018-03-02 17:28:46 -0600

    general updating 65

--- quasar_site_django/static/js/custom/worlds/worlds/abstract/world_dynamic_content.js
+++ quasar_site_django/static/js/custom/worlds/worlds/abstract/world_dynamic_content.js
@@ -25,16 +25,23 @@ function WorldDynamicContent() {
     };
 
     this.load_dynamic_content = function() {
-        // Load all entity walls.
-        var entity_wall_entities = this.entity.get_children_of_type(ENTITY_TYPE_ENTITY_WALL);
-        for (var ew = 0; ew < entity_wall_entities.length; ew++) {
-            var entity_wall = new EntityWall(this, entity_wall_entities[ew]);
-            this.entity_walls.push(entity_wall);
+
+        // Load all the entity walls. These are the base objects that have sub objects to load.
+        var base_wall_entities = this.entity.get_children_of_type(ENTITY_TYPE_WALL);
+        for (var w = 0 ; w < base_wall_entities.length; w++) {
+
+            // Check the type of object to load based off the immediate child of this wall entity.
+            var child_entity = base_wall_entities[w].get_child();
+
+            if (child_entity.get_type() === ENTITY_TYPE_ENTITY_WALL) {
+                this.load_entity_wall(child_entity);
+            } else {
+                // TODO :
+                l('TODO : COVER LOADING FUNCTIONALITY OF THIS ENTITY CHILD TYPE!!');
+            }
         }
 
         this.dynamic_content_loaded = true;
-
-        // TODO : Load all floating pictures.
     };
 
 
@@ -42,10 +49,14 @@ function WorldDynamicContent() {
      |__  |\ |  |  |  |  \ /    |  |  /\  |    |
      |___ | \|  |  |  |   |     |/\| /~~\ |___ |___ */
     this.create_new_entity_wall = function(this_context) {
-        l('Creating a new entity wall with the following context:');
-        l(this_context);
         var entity_wall = new EntityWall(this_context);
         this_context.entity_walls.push(entity_wall);
+    };
+
+    // TODO : Eventually change the naming of entity wall entities lol.
+    this.load_entity_wall = function(entity_wall_entity) {
+        var entity_wall = new EntityWall(this, entity_wall_entity);
+        this.entity_walls.push(entity_wall);
     };
 
 }
