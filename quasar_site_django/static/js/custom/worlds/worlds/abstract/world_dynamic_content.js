6047c280a2441efc84f6c46c5237d503b6101312
utarsuno <utarsuno@gmail.com>
2018-03-04 23:02:21 -0600

    video click events 40

--- quasar_site_django/static/js/custom/worlds/worlds/abstract/world_dynamic_content.js
+++ quasar_site_django/static/js/custom/worlds/worlds/abstract/world_dynamic_content.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// TEMPORARY MEASURE
-var video_id = 0;
-var videos = [];
-
 function WorldDynamicContent() {
 
     this.floating_pictures = [];
     this.entity_walls      = [];
+
+    this.number_of_videos_to_load = 0;
+    this.number_of_videos_loaded  = 0;
+    this.video_entities = [];
     this.videos            = [];
 
     this.css_scene_added = false;
@@ -32,7 +32,6 @@ function WorldDynamicContent() {
     };
 
     this.load_dynamic_content = function() {
-
         // Load all the entity walls. These are the base objects that have sub objects to load.
         var base_wall_entities = this.entity.get_children_of_type(ENTITY_TYPE_WALL);
         for (var w = 0 ; w < base_wall_entities.length; w++) {
@@ -43,11 +42,17 @@ function WorldDynamicContent() {
             if (child_entity.get_type() === ENTITY_TYPE_ENTITY_WALL) {
                 this.load_entity_wall(child_entity);
             } else if (child_entity.get_type() === ENTITY_TYPE_VIDEO) {
-                this.load_video_wall(child_entity);
+                this.video_entities.push(child_entity);
+                this.number_of_videos_to_load += 1;
+                //this.load_video_wall(child_entity);
             }else {
                 // TODO :
                 l('TODO : COVER LOADING FUNCTIONALITY OF THIS ENTITY CHILD TYPE!!');
             }
+        }
+
+        if (this.number_of_videos_to_load > 0) {
+            this.load_all_videos();
         }
 
         this.dynamic_content_loaded = true;
@@ -71,62 +76,33 @@ function WorldDynamicContent() {
     /*       __   ___  __
      \  / | |  \ |__  /  \    |  |  /\  |    |
       \/  | |__/ |___ \__/    |/\| /~~\ |___ |___ */
-    this.add_css_scene = function() {
+    this.load_all_videos = function() {
         this.css_scene = new THREE.Scene();
-        this.container = document.getElementById('video_one');
+        this.container = document.getElementById('video_container');
         this.container.appendChild(MANAGER_RENDERER.css_renderer.domElement);
-
-
         this.video_group = new THREE.Group();
-        this.css_scene.add(this.video_group);
 
-        //this.container2 = document.getElementById('video_two');
-        //this.container2.appendChild(MANAGER_RENDERER.css_renderer.domElement);
+        for (var v = 0; v < this.video_entities.length; v++) {
+            this.load_video_wall(this.video_entities[v]);
+        }
     };
 
     this.add_video_to_css_group = function(video) {
-
-        this.container.removeChild(MANAGER_RENDERER.css_renderer.domElement);
-        this.container.appendChild(MANAGER_RENDERER.css_renderer.domElement);
-
-        this.css_scene.remove(this.video_group);
-
         this.video_group.add(video);
+        this.number_of_videos_loaded += 1;
 
-        this.css_scene.add(this.video_group);
-
-        /*
-        // THIS CODE WORKS
-        video_id += 1;
-        videos.push(video);
-
-        if (video_id === 3) {
-            this.video_group = new THREE.Group();
-            for (var v = 0; v < videos.length; v++) {
-                this.video_group.add(videos[v]);
-            }
+        if (this.number_of_videos_loaded === this.number_of_videos_to_load) {
             this.css_scene.add(this.video_group);
         }
-        */
-
     };
 
     this.create_new_video = function(this_context) {
-        if (!this.css_scene_added) {
-            this.add_css_scene();
-        }
-
+        // TODO : Add needed functionality to this. (Such as temporarily displaying a refresh-warning).
         var video_wall = new FloatingVideo(this_context);
         this_context.videos.push(video_wall);
     };
 
     this.load_video_wall = function(video_entity) {
-        if (!this.css_scene_added) {
-            this.add_css_scene();
-        }
-
-        //this.add_video_container();
-
         var video_wall = new FloatingVideo(this, video_entity);
         this.videos.push(video_wall);
     };
