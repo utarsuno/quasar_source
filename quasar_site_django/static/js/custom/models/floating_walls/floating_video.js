d16e6dda857fc3512568bd1ab249f5b758e09ceb
utarsuno <utarsuno@gmail.com>
2018-03-04 19:42:25 -0600

    video click events 12

--- quasar_site_django/static/js/custom/models/floating_walls/floating_video.js
+++ quasar_site_django/static/js/custom/models/floating_walls/floating_video.js
@@ -64,6 +64,61 @@ VidoeCSSElement.prototype = {
         base_wall.world.css_scene.add(this.object);
     },
 
+    // Base code from : https://stackoverflow.com/questions/6157929/how-to-simulate-a-mouse-click-using-javascript
+    simulatedClick: function(target, options) {
+
+        var event = target.ownerDocument.createEvent('MouseEvents'),
+            options = options || {},
+            opts = { // These are the default values, set up for un-modified left clicks
+                type: 'click',
+                canBubble: true,
+                cancelable: true,
+                view: target.ownerDocument.defaultView,
+                detail: 1,
+                screenX: 0, //The coordinates within the entire page
+                screenY: 0,
+                clientX: 0, //The coordinates within the viewport
+                clientY: 0,
+                ctrlKey: false,
+                altKey: false,
+                shiftKey: false,
+                metaKey: false, //I *think* 'meta' is 'Cmd/Apple' on Mac, and 'Windows key' on Win. Not sure, though!
+                button: 0, //0 = left, 1 = middle, 2 = right
+                relatedTarget: null,
+            };
+
+        //Merge the options with the defaults
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                opts[key] = options[key];
+            }
+        }
+
+        //Pass in the options
+        event.initMouseEvent(
+            opts.type,
+            opts.canBubble,
+            opts.cancelable,
+            opts.view,
+            opts.detail,
+            opts.screenX,
+            opts.screenY,
+            opts.clientX,
+            opts.clientY,
+            opts.ctrlKey,
+            opts.altKey,
+            opts.shiftKey,
+            opts.metaKey,
+            opts.button,
+            opts.relatedTarget
+        );
+
+        //Fire the event
+        target.dispatchEvent(event);
+    },
+
+    /////
+
     set_position_and_normal: function(position, normal) {
         var p = position;
         var nn = normal;
@@ -85,6 +140,7 @@ VidoeCSSElement.prototype = {
         });
 
         this.iframe.dispatchEvent(mouse_event);
+        this.simulatedClick(this.iframe);
 
         var event = JSON.parse(JSON.stringify(mouse_event));
 
