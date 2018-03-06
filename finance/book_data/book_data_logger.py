6d80c90070b690df90f93996e1b73661256367ed
utarsuno <utarsuno@gmail.com>
2018-03-05 23:17:13 -0600

    data server updates

--- finance/book_data/book_data_logger.py
+++ finance/book_data/book_data_logger.py
@@ -12,15 +12,43 @@ KEY_PRICE       = 'Price'
 KEY_INDEX       = 'Index'
 
 
+# Function from : https://stackoverflow.com/questions/38847690/convert-float-to-string-without-scientific-notation-and-false-precision/38847691
+def float_to_str(f):
+	"""Returns a pure string representation of the float provided."""
+	float_string = repr(f)
+	if 'e' in float_string:  # detect scientific notation
+		digits, exp = float_string.split('e')
+		digits = digits.replace('.', '').replace('-', '')
+		exp = int(exp)
+		zero_padding = '0' * (abs(int(exp)) - 1)  # minus 1 for decimal point in the sci notation
+		sign = '-' if f < 0 else ''
+		if exp > 0:
+			float_string = '{}{}{}.0'.format(sign, digits, zero_padding)
+		else:
+			float_string = '{}0.{}{}'.format(sign, zero_padding, digits)
+	return float_string
+
+
+class BookOrders(object):
+	"""Utility objects to parse data raw data into C data."""
+
+	def __init__(self, type, data):
+		self.type = type
+		for row in data:
+			amount = float_to_str(row[KEY_AMOUNT])
+			price = row[KEY_PRICE]
+			print(str(amount) + ' {' + str(type(amount)) + '}\t' + str(price) + ' {' + str(type(price)) + '}')
+
+
 result = r.get('https://www.southxchange.com/api/book/MSR/BTC')
 
 if result.status_code == 200:
 	data = eval(result.content.decode("utf-8"))
 
-	for row in data[ORDER_TYPE_BUY]:
-		amount = row[KEY_AMOUNT]
-		price  = row[KEY_PRICE]
-		print(str(amount) + ' {' + str(type(amount)) + '}\t' + str(price) + ' {' + str(type(price)) + '}')
+	buy_orders  = BookOrders(ORDER_TYPE_BUY,  data[ORDER_TYPE_BUY])
+	#sell_orders = BookOrders(ORDER_TYPE_SELL, data[ORDER_TYPE_SELL])
+
 
 else:
 	print('TODO : Log an error!')
+
