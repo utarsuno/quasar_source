bfe43882948fc86f06061887e174ca0eb8e121b5
utarsuno <utarsuno@gmail.com>
2018-03-05 23:06:13 -0600

    data server updates

--- finance/book_data/book_data_logger.py
+++ finance/book_data/book_data_logger.py
@@ -7,6 +7,10 @@ import requests as r
 ORDER_TYPE_SELL = 'SellOrders'
 ORDER_TYPE_BUY  = 'BuyOrders'
 
+KEY_AMOUNT      = 'Amount'
+KEY_PRICE       = 'Price'
+KEY_INDEX       = 'Index'
+
 
 result = r.get('https://www.southxchange.com/api/book/MSR/BTC')
 
@@ -15,6 +19,10 @@ if result.status_code == 200:
 
 	for i in data[ORDER_TYPE_BUY]:
 		print(i)
+		amount = data[ORDER_TYPE_BUY][KEY_AMOUNT]
+		print(str(amount) + '\t' + type(amount))
+		price = data[ORDER_TYPE_BUY][KEY_AMOUNT]
+		print(str(price) + '\t' + type(price))
 
 else:
 	print('TODO : Log an error!')
