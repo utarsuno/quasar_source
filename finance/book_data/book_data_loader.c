584ec95d795da425f2d96f45663905446bc22b12
utarsuno <utarsuno@gmail.com>
2018-03-06 21:17:38 -0600

    book orders logging updates 13

--- finance/book_data/book_data_loader.c
+++ finance/book_data/book_data_loader.c
@@ -38,87 +38,24 @@ int main(int argc, char * argv[]) {
     fclose(fileptr);
 
 
-    
+    unsigned short last_price = 0;
+    float price_variation = 0;
+    float volume = 0;
+    int number_of_buy_orders = 0;
+    int number_of_sell_orders = 0;
+
     // Read in the last price.
+    last_price = (* (file_buffer + 0) << 8 + * (file_buffer + 1));
 
+    2, 3, 4, 5
 
-    /*__  ___  __             __       ___               __        __          __
-     /__`  |  |  \ | |\ |    |  \  /\   |   /\     |    /  \  /\  |  \ | |\ | / _`
-     .__/  |  |__/ | | \|    |__/ /~~\  |  /~~\    |___ \__/ /~~\ |__/ | | \| \__> */
-    int index;
-    float current_amount;
-    unsigned short current_price;
+    // Read in the price variation.
+    price_variation = (* (file_buffer + 5) << 24 + * (file_buffer + 4) << 16 + * (file_buffer + 3) << 8 + * (file_buffer + 2));
 
-    float * buy_prices            = (float *) malloc(sizeof(float) * number_of_buy_orders);
-    unsigned short * buy_amounts  = (unsigned short *) malloc(sizeof(unsigned short) * number_of_buy_orders);
-    float * sell_prices           = (float *) malloc(sizeof(float) * number_of_sell_orders);
-    unsigned short * sell_amounts = (unsigned short *) malloc(sizeof(unsigned short) * number_of_sell_orders);
+    // TESTING.
+    printf("The last price was {%hu}\n", last_price);
+    printf("The last price variation was {%f}\n", price_variation);
 
-    // Buy amounts.
-    for (index = 0; index < number_of_buy_orders; index++) {
-        scanf("%f", & current_amount);
-        * (buy_amounts + index) = current_amount;
-    }
-
-    // Buy prices.
-    for (index = 0; index < number_of_buy_orders; index++) {
-        scanf("%hu", & current_price);
-        * (buy_prices + index) = current_price;
-    }
-
-    // Sell amounts.
-    for (index = 0; index < number_of_sell_orders; index++) {
-        scanf("%f", & current_amount);
-        * (sell_amounts + index) = current_amount;
-    }
-
-    // Sell prices.
-    for (index = 0; index < number_of_sell_orders; index++) {
-        scanf("%hu", & current_price);
-        * (sell_prices + index) = current_price;
-    }
-
-    /*___         ___            __       ___   /  __       ___  __       ___
-     |__  | |    |__     | |\ | |__) |  |  |   /  /  \ |  |  |  |__) |  |  |
-     |    | |___ |___    | | \| |    \__/  |  /   \__/ \__/  |  |    \__/  |  */
-    FILE * file_pointer = fopen(file_load_path, "rb");
-
-
-
-    // First 4 bytes, last price.
-    fwrite(& last_price, sizeof(last_price), 1, file_pointer);
-
-    // Next 8 bytes, price variation.
-    fwrite(& price_variation, sizeof(price_variation), 1, file_pointer);
-
-    // Next 8 bytes, volume.
-    fwrite(& volume, sizeof(volume), 1, file_pointer);
-
-    // Next 4 bytes, number of buy orders.
-    fwrite(& number_of_buy_orders, 1, sizeof(number_of_buy_orders), file_pointer);
-
-    // Next 4 bytes, number of sell orders.
-    fwrite(& number_of_sell_orders, 1, sizeof(number_of_sell_orders), file_pointer);
-
-    // Next n0 bytes, buy prices.
-    fwrite(buy_prices, sizeof(float), number_of_buy_orders, file_pointer);
-
-    // Next n1 bytes, buy amounts.
-    fwrite(buy_amounts, sizeof(unsigned short), number_of_buy_orders, file_pointer);
-
-    // Next n2 bytes, sell prices.
-    fwrite(sell_prices, sizeof(float), number_of_sell_orders, file_pointer);
-
-    // Next n3 bytes, sell amounts.
-    fwrite(sell_amounts, sizeof(unsigned short), number_of_sell_orders, file_pointer);
-
-    /*___  __   ___  ___           ___        __   __      
-     |__  |__) |__  |__      |\/| |__   |\/| /  \ |__) \ / 
-     |    |  \ |___ |___     |  | |___  |  | \__/ |  \  |  */
-    free(buy_prices);
-    free(buy_amounts);
-    free(sell_prices);
-    free(sell_amounts);
-    fclose(file_pointer);
+    
     return SUCCESS;
 }
