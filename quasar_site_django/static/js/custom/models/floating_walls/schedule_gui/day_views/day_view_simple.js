acbe12fd2222398efdcca10a2d94b55a0727a931
utarsuno <utarsuno@gmail.com>
2018-03-10 18:05:36 -0600

    adding month views 32

--- quasar_site_django/static/js/custom/models/floating_walls/schedule_gui/day_views/day_view_simple.js
+++ quasar_site_django/static/js/custom/models/floating_walls/schedule_gui/day_views/day_view_simple.js
@@ -10,10 +10,8 @@ DayViewSimple.prototype = {
         this.day_instance = day_instance;
         this.base_wall    = base_wall;
 
-        l(this.base_wall);
-
         var position_width = this.base_wall.width / 7;
-        var position_height = this.base_wall.height / 5;
+        var position_height = this.base_wall.height / 6;
 
         var width = (this.base_wall.width - 100) / 7;
         var height = (this.base_wall.height - 100) / 5;
@@ -22,9 +20,9 @@ DayViewSimple.prototype = {
         this.day_view = new FloatingWall(width, height, null, null, this.base_wall.world, false);
         this.day_view.set_attachment_depth_offset(5);
         this.day_view.set_attachment_horizontal_offset(position_width * this.day_instance.day_of_the_week + width / 2, -HALF);
-        this.day_view.set_attachment_vertical_offset(-(position_height * this.day_instance.week_number + height / 2), HALF);
+        this.day_view.set_attachment_vertical_offset(-(position_height * this.day_instance.week_number - height / 2), HALF);
 
-        this.day_view.add_row(-1).add_3D_element(this.day_instance.day_number, TYPE_TITLE, null);
+        this.day_view.add_row(0).add_3D_element(this.day_instance.day_number, TYPE_TITLE, null);
 
         this.day_view.attach_to(this.base_wall);
     }
