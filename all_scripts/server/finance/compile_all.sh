861bddc8e176d559583a791d144c47eefc3a66e6
utarsuno <utarsuno@gmail.com>
2018-03-14 12:14:28 -0500

    general update 08

--- all_scripts/server/finance/compile_all.sh
+++ all_scripts/server/finance/compile_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -O2 /home/git_repos/quasar_source/finance/book_data/book_data_saver.c -o data_saver
-gcc -O2 /home/git_repos/quasar_source/finance/book_data/book_data_loader.c -o data_loader
+gcc -O2 /home/git_repos/quasar_source/finance/book_data/book_data_saver.c -o /home/databoi/finance/data_saver
+gcc -O2 /home/git_repos/quasar_source/finance/book_data/book_data_loader.c -o /home/databoi/finance/data_loader
