127b498e6e04198a3c466f5bc8e484e942537ee8
utarsuno <utarsuno@gmail.com>
2018-03-14 21:55:27 -0500

    client side dynamic content management work 38

--- quasar_site_django/static/js/custom/models/entity_gui/entity_editor.js
+++ quasar_site_django/static/js/custom/models/entity_gui/entity_editor.js
@@ -27,6 +27,7 @@ EntityEditor.prototype = {
     },
 
     cancel_or_delete_button_pressed: function() {
+        this.wall_entity_editor.force_hide_self_and_all_child_attachments_recursively();
         if (this.current_mode === EDITOR_MODE_EDIT) {
             MANAGER_ENTITY.delete_entity_by_id(this.entity_id_being_edited);
         }
