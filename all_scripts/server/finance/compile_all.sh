b6b45ab9623d50fd7c9c14aee4f1f4a800e51256
utarsuno <utarsuno@gmail.com>
2018-03-17 18:58:19 -0500

    finance updating 02

--- all_scripts/server/finance/compile_all.sh
+++ all_scripts/server/finance/compile_all.sh
@@ -3,5 +3,5 @@
 # Compile the object files.
 gcc -c /home/git_repos/quasar_source/finance/book_data/c/abstract/book_data.c
 
-gcc -O2 /home/git_repos/quasar_source/finance/book_data/c/book_data_saver.c -o /home/databoi/finance/data_saver
-gcc -O2 /home/git_repos/quasar_source/finance/book_data/c/book_data_loader.c -o /home/databoi/finance/data_loader
+gcc -O2 /home/git_repos/quasar_source/finance/book_data/c/book_data_saver.c book_data.o -o /home/databoi/finance/data_saver
+gcc -O2 /home/git_repos/quasar_source/finance/book_data/c/book_data_loader.c book_data.o -o /home/databoi/finance/data_loader
