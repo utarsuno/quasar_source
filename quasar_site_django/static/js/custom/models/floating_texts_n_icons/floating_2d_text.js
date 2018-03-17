b61ce532b516983bd44eee7f3763cfe872978932
utarsuno <utarsuno@gmail.com>
2018-03-17 13:13:06 -0500

    general updating 11

--- quasar_site_django/static/js/custom/models/floating_texts_n_icons/floating_2d_text.js
+++ quasar_site_django/static/js/custom/models/floating_texts_n_icons/floating_2d_text.js
@@ -26,12 +26,16 @@ Floating2DText.prototype = {
             if (this.type === TYPE_BUTTON || this.type === TYPE_CHECK_BOX || this.type === TYPE_TITLE || this.type === TYPE_SUPER_TITLE) {
                 x_offset = this.texture_width / 2 - this._get_text_length() / 2;
             }
+
+            this.dynamic_texture.clear(this.current_background_color).drawText(this.text, x_offset, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color);
+            /*
             if (this.current_background_color !== COLOR_TRANSPARENT) {
                 this.dynamic_texture.clear(this.current_background_color).drawText(this.text, x_offset, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.current_background_color);
             } else {
                 //this.dynamic_texture.clear().drawText(this.text, x_offset, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.current_background_color);
                 this.dynamic_texture.clear(this.current_background_color).drawText(this.text, x_offset, this.font_size * TEMP_SMUDGE_FACTOR, this.current_color, this.current_background_color);
             }
+            */
             this.dynamic_texture.needsUpdate = true;
         }
     },
