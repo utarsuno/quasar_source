77a823786460cfdb4fb8137008d82963165be407
utarsuno <utarsuno@gmail.com>
2018-03-18 02:22:47 -0500

    finance updating 56

--- c_processes/c_file_abstraction/c_library.py
+++ c_processes/c_file_abstraction/c_library.py
@@ -2,7 +2,7 @@
 
 """This module, c_library.py, is an abstraction to a custom made C library."""
 
-
+from subprocess import Popen, PIPE
 from universal_code import terminal_api
 
 
@@ -16,5 +16,48 @@ class CLibrary(object):
 	def compile_library(self):
 		"""Compiles the library given the input source code directory and the output directory."""
 		print('Compile file test!')
-		print(terminal_api.run_terminal_command(['gcc', '-c', self._c_source_file.full_file_path, '-o', self._output_directory + self._c_source_file.raw_file_name]))
+		#print(terminal_api.run_terminal_command(['gcc', '-c', self._c_source_file.full_file_path, '-o', self._output_directory + self._c_source_file.raw_file_name]))
+		#print(terminal_api.run_terminal_command('gcc' + ' ' + '-c', self._c_source_file.full_file_path, '-o', self._output_directory + self._c_source_file.raw_file_name]))
+		self.c_process = Popen('gcc -c ' + self._c_source_file.full_file_path + ' -o ' + self._output_directory + self._c_source_file.raw_file_name, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		self.c_process.wait()
+		self.output_stdout, self.output_stderr = self.c_process.communicate()
+		print('OUTPUT!')
+		print(self.output_stdout)
+		print(self.output_stderr)
 
+
+'''
+
+
+# Needed for running a C process and sending data to it.
+from subprocess import Popen, PIPE
+
+
+class CProcess(object):
+	"""Represents a single running C process."""
+
+	def __init__(self, flags):
+		self._path_source_file = path_source_file
+		self._path_output_file = path_output_file
+		self._output_file_name = output_file_name
+		self._flags            = flags
+
+		self._run_string = output_file_name
+		if len(flags) > 0:
+			for f in flags:
+				self._run_string += ' ' + f
+
+		self.c_process     = None
+		self.output_stdout = None
+		self.output_stderr = None
+
+	def run_process(self):
+		"""Runs the c_process."""
+		self.c_process = Popen(self._run_string, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		self.c_process.wait()
+		self.output_stdout, self.output_stderr = self.c_process.communicate()
+
+	def compile_process(self):
+		"""Compiles this c program."""
+
+'''
