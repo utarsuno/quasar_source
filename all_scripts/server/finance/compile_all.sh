effbb012fb15ba85f84f9a5167f9d3dbcf1af519
utarsuno <utarsuno@gmail.com>
2018-03-17 19:50:12 -0500

    finance updating 17

--- all_scripts/server/finance/compile_all.sh
+++ all_scripts/server/finance/compile_all.sh
@@ -3,25 +3,39 @@
 path_source="/home/git_repos/quasar_source/finance/book_data/c/"
 path_output="/home/databoi/finance/"
 
-path_source_book_data_h="abstract/book_data.h"
-path_output_book_data_h="book_data.h"
+book_data="book_data"
+data_loader="data_loader"
+data_saver="data_saver"
 
-path_source_book_data="abstract/book_data.c"
-path_output_book_data="book_data.o"
+path_source_book_data_h=${path_source}"abstract/"${book_data}".h"
+path_output_book_data_h=${path_output}${book_data}".h"
 
-path_source_data_saver="book_data_saver.c"
-path_source_data_loader="book_data_loader.c"
-path_output_data_saver="data_saver"
-path_output_data_loader="data_loader"
+path_source_book_data=${path_source}"abstract/"${book_data}".c"
+path_output_book_data=${path_output}${book_data}".c"
+path_output_book_data_object_file=${path_output}${book_data}".o"
 
-# Copy the needed .h file over.
-cp ${path_source}${path_source_book_data_h} ${path_output}${path_output_book_data_h}
-# Compile the object files.
-gcc -c ${path_source}${path_source_book_data} -o ${path_output}${path_output_book_data}
+path_source_loader=${path_source}${data_loader}".c"
+path_output_loader=${path_output}${data_loader}".c"
+
+path_source_saver=${path_source}${data_saver}".c"
+path_output_saver=${path_output}${data_saver}".c"
+
+#  __   __   __               ___  ___  __   ___  __      __      ___         ___  __
+# /  ` /  \ |__) \ /    |\ | |__  |__  |  \ |__  |  \    /  `    |__  | |    |__  /__`
+# \__, \__/ |     |     | \| |___ |___ |__/ |___ |__/    \__,    |    | |___ |___ .__/
+cp ${path_source_book_data_h} ${path_output_book_data_h}
+cp ${path_source_book_data} ${path_output_book_data}
+cp ${path_source_loader} ${path_output_loader}
+cp ${path_source_saver} ${path_output_saver}
+
+
+#  __   __         __          ___     __   __        ___  __  ___     ___         ___  __
+# /  ` /  \  |\/| |__) | |    |__     /  \ |__)    | |__  /  `  |     |__  | |    |__  /__`
+# \__, \__/  |  | |    | |___ |___    \__/ |__) \__/ |___ \__,  |     |    | |___ |___ .__/
+gcc -c ${path_output_book_data} -o ${path_output_book_data_object_file}
 
 
 
-gcc -O2 ${path_source}${path_source_data_saver} ${path_output}${path_output_book_data} -o ${path_output}${path_output_data_saver}
 
-
-gcc -O2 ${path_output}${path_output_book_data} ${path_source}${path_source_data_loader} -o ${path_output}${path_output_data_loader}
+#gcc -O2 ${path_source}${path_source_data_saver} ${path_output}${path_output_book_data} -o ${path_output}${path_output_data_saver}
+#gcc -O2 ${path_output}${path_output_book_data} ${path_source}${path_source_data_loader} -o ${path_output}${path_output_data_loader}
