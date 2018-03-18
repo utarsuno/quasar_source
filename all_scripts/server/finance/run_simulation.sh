ab1a330f755dab035f5b34b1cacaab7d6ea8ccf8
utarsuno <utarsuno@gmail.com>
2018-03-18 12:07:23 -0500

    finance updating 181

--- all_scripts/server/finance/run_simulation.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=/home/git_repos/quasar_source/
-python3 /home/git_repos/quasar_source/finance/finance_simulations/basic_simulation.py
---
+++ all_scripts/server/finance/run_testing_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gcc m0_net_resistance_testing.c
+./a.out 1 2 3 4
