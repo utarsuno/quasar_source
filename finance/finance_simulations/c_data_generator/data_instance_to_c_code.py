c1d7956092222bede1174acab0fedfdc50582cf9
utarsuno <utarsuno@gmail.com>
2018-03-18 06:32:13 -0500

    finance updating 127

--- finance/finance_simulations/c_data_generator/data_instance_to_c_code.py
+++ finance/finance_simulations/c_data_generator/data_instance_to_c_code.py
@@ -26,6 +26,16 @@ class CCode(object):
 		numbers = numbers[:-1]
 		self.add_line(list_declaration.replace('ARG', numbers))
 
+	def add_defined_2d_list_of_numbers(self, list_declaration, list_of_list_of_numbers):
+		"""Utility function."""
+		code_text = ''
+		for row in list_of_list_of_numbers:
+			single_line = '{'
+			for e in row:
+				single_line += str(e) + ','
+				code_text += single_line[:-1] + '},\n'
+		return list_declaration.replace('ARG', code_text[:-2])
+
 	def __str__(self):
 		s = ''
 		for l in self._lines:
@@ -43,24 +53,38 @@ def get_c_code_from_list_of_data_instances(data_instances, finance_model):
 		number_of_buy_orders_list = []
 		number_of_sell_orders_list = []
 
-
-		# Things to keep track of :
-		# 0x0 - Number of time instances.                  {{int number_of_time_segments}}
-		# 0x1 - Number of buy orders per time instance.    {{int number_of_buy_orders[number_of_time_segments]}}
-		# 0x2 - Number of sell orders per time instance.   {{int number_of_sell_orders[number_of_time_segments]}}
-		# 0x3 - Buy prices per time instance.              {{unsigned short buy_prices[number_of_time_segments][number_of_buy_orders[i]]}}
-		# 0x3 - Buy amounts per time instance.             {{unsigned short buy_amounts[number_of_time_segments][number_of_buy_orders[i]]}}
-		# 0x3 - Sell prices per time instance.             {{unsigned short sell_prices[number_of_time_segments][number_of_sell_orders[i]]}}
-		# 0x3 - Sell amounts per time instance.            {{unsigned short buy_amounts[number_of_time_segments][number_of_sell_orders[i]]}}
+		list_of_list_of_buy_prices = []
+		list_of_list_of_buy_amounts = []
+		list_of_list_of_sell_prices = []
+		list_of_list_of_sell_amounts = []
 
 		for i in data_instances:
 			number_of_buy_orders_list.append(i.data[DATA_KEY_NUMBER_OF_BUY_ORDERS])
 			number_of_sell_orders_list.append(i.data[DATA_KEY_NUMBER_OF_BUY_ORDERS])
-			#print(i)
+
+			list_of_list_of_buy_prices.append([])
+			for e in i.data[DATA_KEY_BUY_PRICES]:
+				list_of_list_of_buy_prices[-1].append(e)
+
+				list_of_list_of_buy_amounts.append([])
+			for e in i.data[DATA_KEY_BUY_AMOUNTS]:
+				list_of_list_of_buy_amounts[-1].append(e)
+
+				list_of_list_of_sell_prices.append([])
+			for e in i.data[DATA_KEY_SELL_PRICES]:
+				list_of_list_of_sell_prices[-1].append(e)
+
+				list_of_list_of_sell_amounts.append([])
+			for e in i.data[DATA_KEY_SELL_AMOUNTS]:
+				list_of_list_of_sell_amounts[-1].append(e)
 
 		c_code.add_defined_1d_list_of_numbers('const int number_of_buy_orders[NUMBER_OF_TIME_INSTANCES] = {ARG};\n', number_of_buy_orders_list)
 		c_code.add_defined_1d_list_of_numbers('const int number_of_sell_orders[NUMBER_OF_TIME_INSTANCES] = {ARG};\n', number_of_sell_orders_list)
 
+		c_code.add_defined_2d_list_of_numbers('const unsigned short all_buy_prices[NUMBER_OF_TIME_INSTANCES][50]   = {ARG};\n', list_of_list_of_buy_prices)
+		c_code.add_defined_2d_list_of_numbers('const float          all_buy_amounts[NUMBER_OF_TIME_INSTANCES][50]  = {ARG};\n', list_of_list_of_buy_amounts)
+		c_code.add_defined_2d_list_of_numbers('const unsigned short all_sell_prices[NUMBER_OF_TIME_INSTANCES][50]  = {ARG};\n', list_of_list_of_sell_prices)
+		c_code.add_defined_2d_list_of_numbers('const float          all_sell_amounts[NUMBER_OF_TIME_INSTANCES][50] = {ARG};\n', list_of_list_of_sell_amounts)
 
 		return str(c_code)
 
