5402069e2b276b5436f67b6f1f653c31a273cf2b
utarsuno <utarsuno@gmail.com>
2018-03-18 16:41:54 -0500

    finance updating 198

--- all_scripts/server/finance/run_testing_test.sh
+++ all_scripts/server/finance/run_testing_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/home/databoi/c_code/m0_net_resistance_testing 1 2 3 4
+#/home/databoi/c_code/m0_net_resistance_testing 1 2 3 4
+export PYTHONPATH=/home/git_repos/quasar_source/
+python3 /home/git_repos/quasar_source/finance/finance_simulations/massive_simulation_runner.py
--- finance/finance_simulations/massive_simulation_runner.py
+++ finance/finance_simulations/massive_simulation_runner.py
@@ -6,6 +6,9 @@ from c_processes.c_process import CProcess
 from c_processes.c_process_manager import ProcessManager
 
 
+PATH_TO_TRAINER = '/home/databoi/c_code/m0_net_resistance_testing'
+
+
 class MassiveSimulationRunner(object):
 	"""Runs many simulations in parallel."""
 
@@ -13,13 +16,48 @@ class MassiveSimulationRunner(object):
 		self.best_weights = None
 		self.best_score   = None
 
-		self.training_flags = [.0001, -.0007, .1, -.4]
-		masari_training_process = CProcess('/home/databoi/c_code/m0_net_resistance_testing')
+		self.delta = .00001
+
+		self.base_training_flags = [.0001, -.0007, .1, -.4]
+		#masari_training_process = CProcess('/home/databoi/c_code/m0_net_resistance_testing')
 
 	def one_training_session(self):
 		"""Runs a single training session."""
 		# A single training session will train each weight 3000 times with varying values.
 
+		number_of_weights = len(self.base_training_flags)
+		#for w in range(number_of_weights):
+		self._train_one_weight(0)
+
+
+	def _train_one_weight(self, weight_index):
+		"""Trains a single weight."""
+		# Make 3000 processes.
+		processes_to_run = []
+		for p in range(3000):
+
+			# Calculate the weight delta.
+			if p < 1000:
+				weight_delta = self.delta * p * -1
+			elif p < 2000:
+				weight_delta = self.delta * p
+			else:
+				weight_delta = self.delta * p * 2
+
+			weights_to_use = []
+			for i, w in enumerate(self.base_training_flags):
+				if i == weight_index:
+					weights_to_use.append(w + weight_delta)
+				else:
+					weights_to_use.append(w)
+			processes_to_run.append(CProcess(PATH_TO_TRAINER, weights_to_use))
+
+		simulation_runner = ProcessManager(processes_to_run)
+		results = simulation_runner.run_all_c_processes()
+
+		print('GOT THE FOLLOWING RESULTS')
+		for r in results:
+			print(r)
 
 
 '''
