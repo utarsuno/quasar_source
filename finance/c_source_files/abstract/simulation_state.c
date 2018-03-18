b0fcb4acf2c8ffaac731bd20450e1833129a84d7
utarsuno <utarsuno@gmail.com>
2018-03-18 12:51:51 -0500

    finance updating 189

--- finance/c_source_files/abstract/simulation_state.c
+++ finance/c_source_files/abstract/simulation_state.c
@@ -34,10 +34,10 @@ inline int can_place_sell_order(SimulationState * simulation_state) {
 inline void place_buy_order(const float order_amount, const float order_price, SimulationState * simulation_state, BookOrder * buy_orders) {
     int i;
     for (i = 0; i < MAXIMUM_NUMBER_OF_BUY_ORDERS; i++) {
-        if (buy_orders[i]->currently_active == FALSE) {
-            buy_orders[i]->currently_active = TRUE;
-            buy_orders[i]->order_amount = order_amount;
-            buy_orders[i]->order_price = order_price;
+        if (buy_orders[i].currently_active == FALSE) {
+            buy_orders[i].currently_active = TRUE;
+            buy_orders[i].order_amount = order_amount;
+            buy_orders[i].order_price = order_price;
             simulation_state->number_of_current_buy_orders += 1;
             return;
         }
@@ -47,10 +47,10 @@ inline void place_buy_order(const float order_amount, const float order_price, S
 inline void place_sell_order(const float order_amount, const float order_price, SimulationState * simulation_state, BookOrder * sell_orders) {
     int i;
     for (i = 0; i < MAXIMUM_NUMBER_OF_SELL_ORDERS; i++) {
-        if (sell_orders[i]->currently_active == FALSE) {
-            sell_orders[i]->currently_active = TRUE;
-            sell_orders[i]->order_amount = order_amount;
-            sell_orders[i]->order_price = order_price;
+        if (sell_orders[i].currently_active == FALSE) {
+            sell_orders[i].currently_active = TRUE;
+            sell_orders[i].order_amount = order_amount;
+            sell_orders[i].order_price = order_price;
             simulation_state->number_of_current_sell_orders += 1;
             return;
         }
@@ -61,18 +61,18 @@ inline void check_if_any_buy_or_sell_orders_should_be_filled(const float current
     int i;
 
     for (i = 0; i < MAXIMUM_NUMBER_OF_BUY_ORDERS; i++) {
-        if (buy_orders[i]->currently_active == TRUE && buy_orders[i]->order_price < current_price) {
-            buy_orders[i]->currently_active = FALSE;
-            simulation_state->masari_capital -= buy_orders[i]->order_amount;
-            simulation_state->btc_capital += buy_orders[i]->order_price * buy_orders[i]->order_amount;
+        if (buy_orders[i].currently_active == TRUE && buy_orders[i].order_price < current_price) {
+            buy_orders[i].currently_active = FALSE;
+            simulation_state->masari_capital -= buy_orders[i].order_amount;
+            simulation_state->btc_capital += buy_orders[i].order_price * buy_orders[i].order_amount;
         }
     }
 
     for (i = 0; i < MAXIMUM_NUMBER_OF_SELL_ORDERS; i++) {
-        if (sell_orders[i]->currently_active == TRUE && sell_orders[i]->order_price > current_price) {
-            sell_orders[i]->currently_active = FALSE;
-            simulation_state->masari_capital += sell_orders[i]->order_amount;
-            simulation_state->btc_capital -= sell_orders[i]->order_price * sell_orders[i]->order_amount;
+        if (sell_orders[i].currently_active == TRUE && sell_orders[i].order_price > current_price) {
+            sell_orders[i].currently_active = FALSE;
+            simulation_state->masari_capital += sell_orders[i].order_amount;
+            simulation_state->btc_capital -= sell_orders[i].order_price * sell_orders[i].order_amount;
         }
     }
 }
