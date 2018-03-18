035cad73813b02a5b1a6031addb5ca41f3d3b1c8
utarsuno <utarsuno@gmail.com>
2018-03-18 00:22:43 -0500

    finance updating 45

--- finance/book_data/c/quick_test.c
+++ finance/book_data/c/quick_test.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 
-float bytes_to_float(const unsigned char b0, const, unsigned char b1, const unsigned char b2, const unsigned char b3) {
+float bytes_to_float(const unsigned char b0, const unsigned char b1, const unsigned char b2, const unsigned char b3) {
     float output;
     * ((unsigned char *)(& output) + 3) = b0;
     * ((unsigned char *)(& output) + 2) = b1;
