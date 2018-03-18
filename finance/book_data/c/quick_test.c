6dd16e5fd28224e3570c728f36bbb4a38edcdc2b
utarsuno <utarsuno@gmail.com>
2018-03-18 00:19:17 -0500

    finance updating 41

--- finance/book_data/c/quick_test.c
+++ finance/book_data/c/quick_test.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 
-final float bytes_to_float(final unsigned char b0, final, unsigned char b1, final unsigned char b2, final unsigned char b3) {
+static inline const float bytes_to_float(const unsigned char b0, const, unsigned char b1, const unsigned char b2, const unsigned char b3) {
     float output;
     * ((unsigned char *)(& output) + 3) = b0;
     * ((unsigned char *)(& output) + 2) = b1;
@@ -12,7 +12,7 @@ final float bytes_to_float(final unsigned char b0, final, unsigned char b1, fina
     return output;
 }
 
-int main(final int argc, final char * argv[]) {
+int main(const int argc, const char * argv[]) {
 
     sleep(5);
     printf("This is the output from program{%s}!\n", argv[1]);
