6238d191442f14e7804a0d7013befab536f9cd83
utarsuno <utarsuno@gmail.com>
2018-03-18 05:53:38 -0500

    finance updating 118

--- finance/finance_simulations/c_data_generator/data_instance_to_c_code.py
+++ finance/finance_simulations/c_data_generator/data_instance_to_c_code.py
@@ -8,6 +8,10 @@ from finance.finance_simulations.data_instance import DataInstance
 def get_c_code_from_list_of_data_instances(data_instances, finance_model):
 	"""Returns a text containing c_code that represents the data instances."""
 
+	print(finance_model.type_of_data_needed)
+	print(DataInstance.DATA_REQUIREMENT_FULL_BOOK_ORDER)
+	print(finance_model.type_of_data_needed == DataInstance.DATA_REQUIREMENT_FULL_BOOK_ORDER)
+
 	if finance_model.type_of_data_needed == DataInstance.DATA_REQUIREMENT_FULL_BOOK_ORDER:
 		for i in data_instances:
 			print(i)
