3f054eb7ad757656b2bbebda80a586e6f848a4f2
utarsuno <utarsuno@gmail.com>
2018-03-18 02:23:57 -0500

    finance updating 58

--- finance/c_source_files/abstract/book_data.c
+++ finance/c_source_files/abstract/book_data.c
@@ -1,7 +1,7 @@
 #include "book_data.h"
 
 // Base code from : https://stackoverflow.com/questions/3991478/building-a-32bit-float-out-of-its-4-composite-bytes-c
-final static inline float bytes_to_float(final unsigned char b0, unsigned char b1, unsigned char b2, unsigned char b3) {
+static inline const float bytes_to_float(const unsigned char b0, const unsigned char b1, const unsigned char b2, const unsigned char b3) {
     float output;
     * ((unsigned char *)(& output) + 3) = b0;
     * ((unsigned char *)(& output) + 2) = b1;
