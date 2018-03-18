7bf363c726ac018a5f72e63cba3bf159e87dcf1e
utarsuno <utarsuno@gmail.com>
2018-03-18 04:05:55 -0500

    finance updating 74

--- finance/project_management/finance_projects_builder.py
+++ finance/project_management/finance_projects_builder.py
@@ -47,7 +47,7 @@ C_LIBRARY_BOOK_DATA = c_libraries['book_data']
 ''' __                         ___    __        __
    /__` |  |\/| |  | |     /\   |  | /  \ |\ | /__`
    .__/ |  |  | \__/ |___ /~~\  |  | \__/ | \| .__/ '''
-c_file_raw_data_parser = CFile(c.FILE_PATH_RAW_DATA_PARSER)
+c_file_raw_data_parser = CFile(FILE_PATH_RAW_DATA_PARSER)
 PROJECT_SIMULATION_DATA_FETCHER = CProject(c_file_raw_data_parser, [C_LIBRARY_BOOK_DATA], DIRECTORY_FINANCE_C_CODE_OUTPUT)
 PROJECT_SIMULATION_DATA_FETCHER.build_project()
 
