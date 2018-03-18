99274dfdb2a3b43c427827b9cd61b36cf8e13f4e
utarsuno <utarsuno@gmail.com>
2018-03-17 19:13:53 -0500

    finance updating 07

--- all_scripts/server/finance/compile_all.sh
+++ all_scripts/server/finance/compile_all.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Compile the object files.
-gcc -c /home/git_repos/quasar_source/finance/book_data/c/abstract/book_data.c
+path_source="/home/git_repos/quasar_source/finance/book_data/c/"
+path_output="/home/databoi/finance/"
 
-gcc -O2 /home/git_repos/quasar_source/finance/book_data/c/book_data_saver.c book_data.o -o /home/databoi/finance/data_saver
-gcc -O2 /home/git_repos/quasar_source/finance/book_data/c/book_data_loader.c book_data.o -o /home/databoi/finance/data_loader
+path_source_book_data="abstract/book_data.c"
+path_output_book_data="data_saver/book_data.o"
+
+path_source_data_saver="data_saver.c"
+path_source_data_loader="data_loader.c"
+path_output_data_saver="data_saver"
+path_output_data_loader="data_loader"
+
+# Compile the object files.
+gcc -c ${path_source}${path_source_book_data} -o ${path_output}${path_output_book_data}
+
+gcc -O2 ${path_source}${path_source_data_saver} ${path_output}${path_source_data_saver} -o ${path_output}${path_output_data_saver}
+gcc -O2 ${path_source}${path_source_data_loader} ${path_output}${path_source_data_loader} -o ${path_output}${path_output_data_loader}
