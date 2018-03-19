e039406325a4a2dfd3ff37b151b4e7181c7d8352
utarsuno <utarsuno@gmail.com>
2018-03-19 15:07:30 -0500

    finance model updating 01

--- all_scripts/server/finance/simulations/run_testing_test.sh
+++ all_scripts/server/finance/simulations/run_testing_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 #/home/databoi/c_code/m0_net_resistance_testing 1 2 3 4
-export PYTHONPATH=/home/git_repos/quasar_source/
-python3 /home/git_repos/quasar_source/finance/finance_simulations/massive_simulation_runner.py
+#export PYTHONPATH=/home/git_repos/quasar_source/
+#python3 /home/git_repos/quasar_source/finance/finance_simulations/massive_simulation_runner.py
