137290247211a60152cddac4733614db8dd7bb87
utarsuno <utarsuno@gmail.com>
2018-03-22 07:28:14 -0500

    full project cleanup 35

--- code_api/discrete_projects/quasar_source/all_scripts/server_scripts.py
+++ code_api/discrete_projects/quasar_source/all_scripts/server_scripts.py
@@ -156,6 +156,7 @@ def load_quasar_directory(directory_server, code_file_script_utilities, code_fil
 	# Live run.
 	code_file_live_run = ShellFile('live_run')
 	code_file_live_run.treat_paths_as_server_paths()
+	code_file_live_run.add_required_safety_check(SHELL_SAFETY_CHECK_TERMINATE_IF_NOT_SUDO)
 	code_file_live_run.add_required_variable_setters(CodeChunk(['IS_QUASAR_SERVER_RUNNING=`python3 ${PATH_TO_IS_PROGRAM_RUNNING} ${PYTHON_QUASAR_MANAGE_PATH}`']))
 	code_file_live_run.set_main_code(CodeChunk('''
 if [ "${IS_QUASAR_SERVER_RUNNING}" == "true" ]; then
@@ -170,7 +171,7 @@ fi
 	# Restart.
 	code_file_restart = ShellFile('restart')
 	code_file_restart.treat_paths_as_server_paths()
-	#code_file_restart.add_required_safety_check(SHELL_SAFETY_CHECK_TERMINATE_IF_NOT_SUDO)
+	code_file_restart.add_required_safety_check(SHELL_SAFETY_CHECK_TERMINATE_IF_NOT_SUDO)
 	code_file_restart.set_main_code(CodeChunk('''
 sudo bash ${PYTHON_QUASAR_SCRIPT_TERMINATE}
 sudo bash ${PYTHON_QUASAR_SCRIPT_RUN_IN_BACKGROUND}
@@ -179,7 +180,7 @@ sudo bash ${PYTHON_QUASAR_SCRIPT_RUN_IN_BACKGROUND}
 	# Run in background.
 	code_file_run_in_background = ShellFile('run_in_background')
 	code_file_run_in_background.treat_paths_as_server_paths()
-	#code_file_run_in_background.add_required_safety_check(SHELL_SAFETY_CHECK_TERMINATE_IF_NOT_SUDO)
+	code_file_run_in_background.add_required_safety_check(SHELL_SAFETY_CHECK_TERMINATE_IF_NOT_SUDO)
 	code_file_run_in_background.add_required_variable_setters(CodeChunk(['IS_QUASAR_SERVER_RUNNING=`python3 ${PATH_TO_IS_PROGRAM_RUNNING} ${PYTHON_QUASAR_MANAGE_PATH}`']))
 	code_file_run_in_background.set_main_code(CodeChunk('''
 if [ "${IS_QUASAR_SERVER_RUNNING}" == "true" ]; then
@@ -206,7 +207,7 @@ fi
 	# Terminate.
 	code_file_terminate = ShellFile('terminate')
 	code_file_terminate.treat_paths_as_server_paths()
-	#code_file_terminate.add_required_safety_check(SHELL_SAFETY_CHECK_TERMINATE_IF_NOT_SUDO)
+	code_file_terminate.add_required_safety_check(SHELL_SAFETY_CHECK_TERMINATE_IF_NOT_SUDO)
 	code_file_terminate.set_main_code(CodeChunk('''
 sudo pkill -f "${PYTHON_QUASAR_MANAGE_PATH}"
 '''))
