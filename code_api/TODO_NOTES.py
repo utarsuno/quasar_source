2aad9288fc08824069c0d71b97a6fbe26e6ed558
utarsuno <utarsuno@gmail.com>
2018-03-21 23:22:17 -0500

    full project cleanup 09

--- code_api/TODO_NOTES.py
+++ code_api/TODO_NOTES.py
@@ -3,7 +3,33 @@
 
 # Utilize design concepts from code_api and z_archive version of the code_api
 
-# Once refactorings are fully complete rename this directory to code_api
-
 # START ABSTRACT, make sure all features added comply for needs of C, Python, and Javascript with minimal language-specific logic.
 
+
+
+
+# FEATURES THAT NEED TO BE ADDED
+# get_all_string_literals
+
+
+# VERY IMPORTANT FEATURE TO BE ADDED : UniversalConstants
+
+
+# VERY IMPORTANT FEATURE :
+'''
+from jsmin import jsmin
+jsmin(text)
+
+RELATING TO THIS :
+deal with marks such as '// FOR_DEV_START'
+ALSO : Ensure they are all consistent, example : not a mix of FOR_DEV_START and DEV_START
+
+'''
+
+
+
+# Feature to add after refactoring :
+'''
+from universal_code import string_utilities as su
+sort_lines_by_delimiter
+'''
