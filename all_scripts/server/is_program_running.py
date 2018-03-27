2d691f8a157c9f1bde93136200779c1baaf7e8b5
utarsuno <utarsuno@gmail.com>
2018-03-26 23:00:07 -0500

    fixing scripts 11

--- all_scripts/server/is_program_running.py
+++ all_scripts/server/is_program_running.py
@@ -6,38 +6,8 @@
 import subprocess
 # Needed for parsing arguments.
 import argparse
-
-
-'''
-def run_bash_command_and_get_output(bash_command, shell=False, cwd=None):
-	"""Runs the provided bash command and returns the output.
-	:param bash_command : The bash command to run, as a list of String parameters.
-	:param shell        : Don't set to True unless things like environment variables are needed. More information here : https://stackoverflow.com/questions/3172470/actual-meaning-of-shell-true-in-subprocess
-	:param cwd          : The current working directory that this command should be ran from.
-	:return: The output of the bash command, as a string."""
-	bad_argument = False
-	if type(bash_command) != list and shell is False:
-		bad_argument = True
-	else:
-		if shell is False:
-			for c in bash_command:
-				if type(c) != str:
-					bad_argument = True
-	if bad_argument:
-		print('Bad argument! {' + str(bash_command) + '}')
-		exit()
-	# Safety checks are over.
-	if cwd is not None:
-		result = subprocess.run(bash_command, stdout=subprocess.PIPE, shell=shell, cwd=cwd)
-	else:
-		result = subprocess.run(bash_command, stdout=subprocess.PIPE, shell=shell)
-	return result.stdout.decode('utf-8')
-'''
-
-
-def run_process_finder():
-	"""Utility function."""
-	return subprocess.check_output(['ps', '-edaf']).decode('utf-8').split('\n')
+# Needed for perform regax expressions.
+import re
 
 
 if __name__ == '__main__':
@@ -46,12 +16,26 @@ if __name__ == '__main__':
 	args         = parser.parse_args()
 	service_name = args.service_name
 
-	#output = run_bash_command_and_get_output(['ps', '-edaf', '|', 'grep', '"' + service_name + '"', '|', 'grep', '-v', 'grep'], shell=True).split('\n')
 
-	output = run_process_finder()
+	output = subprocess.check_output(['ps', '-edaf']).decode('utf-8').split('\n')
 	for l in output:
-		print(l)
-	
+		if 'is_program_running.py' not in l:
+			content = re.findall(r'\S+', l)
+
+			uid   = content[0]
+			pid   = content[1]
+			ppid  = content[2]
+			c     = content[3]
+			stime = content[4]
+			tty   = content[5]
+			time  = content[6]
+			cmd   = content[6:]
+
+			print(pid)
+			for command in cmd:
+				print(command)
+			print('-----')
+
 
 	#output       = run_bash_command_and_get_output(['top', '-c', '-n', '1', '-b']).split('\n')
 
