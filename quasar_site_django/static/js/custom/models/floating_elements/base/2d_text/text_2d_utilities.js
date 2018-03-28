25ff3b7a4b3c969c8921c72f7605cdeb2294b967
utarsuno <utarsuno@gmail.com>
2018-03-28 17:56:10 -0500

    floating elements refactoring 47

--- quasar_site_django/static/js/custom/models/floating_elements/base/2d_text/text_2d_utilities.js
+++ quasar_site_django/static/js/custom/models/floating_elements/base/2d_text/text_2d_utilities.js
@@ -10,7 +10,8 @@ Text2DUtilities.prototype = {
     },
 
     get_width_of_text_given_bounding_height: function(text, height) {
-        var h = get_nearest_power_of_two_for_number(height * 2) * 0.75;
+        //var h = get_nearest_power_of_two_for_number(height * 2) * 0.75;
+        var h = get_nearest_power_of_two_for_number(height * 2) * 0.85;
         var ratio = parseFloat(h) / parseFloat(height);
         this.canvas.set_height(height);
 
