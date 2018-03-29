80eaf1a2ea48f625c35d465f06f96a0ac37947db
utarsuno <utarsuno@gmail.com>
2018-03-29 17:46:16 -0500

    floating elements refactoring 169

--- quasar_site_django/static/js/custom/models/floating_elements/base/2d_text/text_2d_utilities.js
+++ quasar_site_django/static/js/custom/models/floating_elements/base/2d_text/text_2d_utilities.js
@@ -23,27 +23,27 @@ Text2DUtilities.prototype = {
         this.canvas_regular.set_height(height);
         this.canvas_regular.update_font();
         var w = this.canvas_regular.get_text_width_for_texture(text);
-        return get_nearest_power_of_two_for_number(w / 2);
+        return get_nearest_power_of_two_for_number(w);
     },
 
     get_width_of_bold_text_given_bounding_height: function(text, height) {
         this.canvas_bold.set_height(height);
         this.canvas_bold.update_font();
         var w = this.canvas_bold.get_text_width_for_texture(text);
-        return get_nearest_power_of_two_for_number(w / 2);
+        return get_nearest_power_of_two_for_number(w);
     },
 
     get_width_of_italic_text_given_bounding_height: function(text, height) {
         this.canvas_italic.set_height(height);
         this.canvas_italic.update_font();
         var w = this.canvas_italic.get_text_width_for_texture(text);
-        return get_nearest_power_of_two_for_number(w / 2);
+        return get_nearest_power_of_two_for_number(w);
     },
 
     get_width_of_italic_and_bold_text_given_bounding_height: function(text, height) {
         this.canvas_italic_and_bold.set_height(height);
         this.canvas_italic_and_bold.update_font();
         var w = this.canvas_italic_and_bold.get_text_width_for_texture(text);
-        return get_nearest_power_of_two_for_number(w / 2);
+        return get_nearest_power_of_two_for_number(w);
     }
 };
