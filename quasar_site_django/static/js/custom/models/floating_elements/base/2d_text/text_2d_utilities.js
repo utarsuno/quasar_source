454481d30a4ebe0614689be964c92300396822a2
utarsuno <utarsuno@gmail.com>
2018-03-29 17:39:12 -0500

    floating elements refactoring 167

--- quasar_site_django/static/js/custom/models/floating_elements/base/2d_text/text_2d_utilities.js
+++ quasar_site_django/static/js/custom/models/floating_elements/base/2d_text/text_2d_utilities.js
@@ -27,7 +27,7 @@ Text2DUtilities.prototype = {
         var w = this.canvas_regular.get_text_width_for_texture(text);
 
         //return w * ratio;
-        return get_nearest_power_of_two_for_number(w);
+        return get_nearest_power_of_two_for_number(w / 2);
     },
 
     get_width_of_bold_text_given_bounding_height: function(text, height) {
@@ -52,7 +52,7 @@ Text2DUtilities.prototype = {
         l('w{' + w + '}');
         l('w * ratio{' + (w * ratio) + '}');
 
-        return get_nearest_power_of_two_for_number(w);
+        return get_nearest_power_of_two_for_number(w / 2);
         //return get_nearest_power_of_two_for_number(w / 2);
         //return get_nearest_power_of_two_for_number(w);
         //return w;
