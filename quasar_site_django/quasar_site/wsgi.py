d9f0a34ddb9e24640d7f8f621397143d573d1c8e
utarsuno <utarsuno@gmail.com>
2018-04-02 20:12:37 -0500

    websockets 01

--- quasar_site_django/quasar_site/wsgi.py
+++ quasar_site_django/quasar_site/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quasar_site.settings")
-
 application = get_wsgi_application()
--- quasar_site_django/static/js/custom/web_sockets/web_socket_manager.js
+++ quasar_site_django/static/js/custom/web_sockets/web_socket_manager.js
@@ -7,7 +7,7 @@ function WebSocketManager() {
 WebSocketManager.prototype = {
 
     __init__: function() {
-        this._connection_string = 'ws://' + window.location.host + '/users/';
+        this._connection_string = 'ws://' + window.location.host + '/';
         this._connected = false;
     },
 
