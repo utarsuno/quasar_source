8ea9a45b0b41df6a2365afb58fffd07c1b2ecec5
utarsuno <utarsuno@gmail.com>
2018-04-08 22:23:55 -0500

    transition shader work 27

--- quasar_site_django/static/js/custom/rendering/world_transition.js
+++ quasar_site_django/static/js/custom/rendering/world_transition.js
@@ -95,8 +95,12 @@ TransitionPair.prototype = {
         if (this.transition == 0 ) {
             //this.scene_b.render(delta, false);
             //this.renderer_manager.renderer.render()
+            this.renderer_manager.renderer.setClearColor(0x111111);
+            this.renderer_manager.renderer.render(this.scene_new, this.current_camera, this.scene_new.fbo, true);
         } else if (this.transition >= 1) {
             //this.scene_a.render(delta, false);
+            this.renderer_manager.renderer.setClearColor(0xffffff);
+            this.renderer_manager.renderer.render(this.scene_old, this.previous_camera, this.scene_old.fbo, true)
             this.renderer_manager.in_transition = false;
             this.renderer_manager.set_current_scene(this.scene_new);
         } else {
