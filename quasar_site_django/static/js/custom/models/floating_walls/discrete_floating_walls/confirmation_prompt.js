7300a9b1b2db139df39dc77e6bb158564d3aae95
utarsuno <utarsuno@gmail.com>
2018-04-13 19:36:33 -0500

    general updates 55

--- quasar_site_django/static/js/custom/models/floating_walls/discrete_floating_walls/confirmation_prompt.js
+++ quasar_site_django/static/js/custom/models/floating_walls/discrete_floating_walls/confirmation_prompt.js
@@ -9,11 +9,11 @@ ConfirmationPrompt.prototype = {
     __init__: function(world) {
         this.wall = new FloatingWall(300, 50, null, null, world, false);
         var row = this.wall.add_row();
-        this.title = row.add_text_2D([0, 1, false], 16, '');
+        //this.title = row.add_text_2D([0, 1, false], 16, '');
         row = this.wall.add_row();
-        this.button_yes = row.add_button([0, HALF, false], 16, 'yes', this.button_yes_clicked.bind(this));
+        this.button_yes = row.add_button([0, HALF, true], 16, 'yes', this.button_yes_clicked.bind(this));
         this.button_yes.set_foreground_color(COLOR_RED);
-        this.button_no  = row.add_button([HALF, 1, false], 16, 'no', this.button_no_clicked.bind(this));
+        this.button_no  = row.add_button([HALF, 1, true], 16, 'no', this.button_no_clicked.bind(this));
         this.button_no.set_foreground_color(COLOR_GREEN);
         this.wall.hide_self_and_all_child_attachments_recursively();
 
@@ -55,6 +55,11 @@ ConfirmationPrompt.prototype = {
     },
 
     prompt: function() {
+        this.wall.delete_row_by_index(0);
+        var row = this.wall.add_row(0);
+        this.title = row.add_text_2D([0, 1, false], 16, this._get_confirmation_prompt());
+        l('Setting text to :');
+        l(this._get_confirmation_prompt());
         this.title.update_text(this._get_confirmation_prompt());
         this.wall.display_self_and_all_child_attachments_recursively();
     }
