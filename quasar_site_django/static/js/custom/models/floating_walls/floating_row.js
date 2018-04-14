21e7b6a94089e212131fd15441c2ea21bc35b7d3
utarsuno <utarsuno@gmail.com>
2018-04-13 23:16:02 -0500

    general updates 81

--- quasar_site_django/static/js/custom/models/floating_walls/floating_row.js
+++ quasar_site_django/static/js/custom/models/floating_walls/floating_row.js
@@ -123,9 +123,10 @@ FloatingRow.prototype = {
             floating_element.set_attachment_horizontal_offset(0, (x_params[0] + x_params[1]) / 2 - HALF);
             break;
         case CENTER_LEFT:
-            var width_percentage = (floating_element.width / this.parent_wall.width) / 2;
-            var alpha = -.5 / width_percentage;
-            floating_element.set_attachment_horizontal_offset(0, width_percentage + alpha);
+            var width_percentage = (floating_element.width / this.parent_wall.width);
+            var alpha = (-1 / width_percentage) / 2;
+            //var alpha = -.5 / width_percentage;
+            floating_element.set_attachment_horizontal_offset(0, width_percentage / 2 + alpha);
             break;
         case CENTER_MIDDLE:
             break;
