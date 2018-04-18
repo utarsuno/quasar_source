233b794fa1657db48364f3bab4a2a4166a6ff340
utarsuno <utarsuno@gmail.com>
2018-04-18 18:03:47 -0500

    dynamic console logs work 02

--- quasar_site_django/static/js/custom/client/message_log/message_log_manager.js
+++ quasar_site_django/static/js/custom/client/message_log/message_log_manager.js
@@ -27,7 +27,7 @@ function MessageLogManager() {
 
     this.current_max_row = 0;
 
-    this.load_log_rows();
+    //this.load_log_rows();
 
     this.add_server_message_red = function(message) {
         this._add_message(message, 'server', LOG_MESSAGE_COLOR_RED);
