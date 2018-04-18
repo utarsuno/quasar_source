d5ec29a5b59931eb493dc2e3bd6b08eb97a62021
utarsuno <utarsuno@gmail.com>
2018-04-18 09:39:17 -0500

    general updating 05

--- quasar_site_django/static/js/custom/models/discrete_models/keyboard/button_model.js
+++ quasar_site_django/static/js/custom/models/discrete_models/keyboard/button_model.js
@@ -14,26 +14,43 @@ ButtonModel.prototype = {
     create: function(key_width) {
         this.geometry = new THREE.Geometry();
 
-        // Key face.
+        // Face.
         let v0 = new THREE.Vector3(10, 10, 0);
         let v1 = new THREE.Vector3(10 + key_width, 10, 0);
         let v2 = new THREE.Vector3(10 + key_width, 10 + key_width, 0);
         let v3 = new THREE.Vector3(10, 10 + key_width, 0);
-
-        // Key top edge.
+        // Top left.
         let v4 = new THREE.Vector3(0, 10 + key_width + 10, -10);
+        // Top right.
         let v5 = new THREE.Vector3(10 + key_width + 10, 10 + key_width + 10, -10);
+        // Bottom right.
+        let v6 = new THREE.Vector3(10 + key_width + 10, 0, -10);
+        // Bottom left.
+        let v7 = new THREE.Vector3(0, 10 + key_width + 10, -10);
 
+        // Face.
         this.geometry.vertices.push(v0);
         this.geometry.vertices.push(v1);
         this.geometry.vertices.push(v2);
         this.geometry.vertices.push(v3);
+        // Top left.
         this.geometry.vertices.push(v4);
+        // Top right.
         this.geometry.vertices.push(v5);
+        // Right edge.
+        this.geometry.vertices.push(v6);
+        // Bottom left.
+        this.geometry.vertices.push(v7);
 
+        // Face.
         this.geometry.faces.push(new THREE.Face3(0, 1, 2));
         this.geometry.faces.push(new THREE.Face3(2, 3, 0));
+        // Top edge.
         this.geometry.faces.push(new THREE.Face3(3, 5, 4));
+        this.geometry.faces.push(new THREE.Face3(3, 2, 5));
+        // Right edge.
+
+
         this.geometry.computeFaceNormals();
 
         this.mesh = new THREE.Mesh(this.geometry, new THREE.MeshNormalMaterial());
