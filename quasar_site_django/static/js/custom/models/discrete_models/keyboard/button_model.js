0bc7e9c5a7c7c6a4753b03a554d93d25cdc2440e
utarsuno <utarsuno@gmail.com>
2018-04-18 09:29:55 -0500

    general updating 02

--- quasar_site_django/static/js/custom/models/discrete_models/keyboard/button_model.js
+++ quasar_site_django/static/js/custom/models/discrete_models/keyboard/button_model.js
@@ -14,17 +14,26 @@ ButtonModel.prototype = {
     create: function(key_width) {
         this.geometry = new THREE.Geometry();
 
+        // Key face.
         let v0 = new THREE.Vector3(10, 10, 0);
         let v1 = new THREE.Vector3(10 + key_width, 10, 0);
         let v2 = new THREE.Vector3(10 + key_width, 10 + key_width, 0);
         let v3 = new THREE.Vector3(10, 10 + key_width, 0);
 
+        // Key top edge.
+        let v4 = new THREE.Vector3(0, 10 + key_width + 10, -10);
+        let v5 = new THREE.Vector3(10 + key_width + 10, 10 + key_width + 10, -10);
+
         this.geometry.vertices.push(v0);
         this.geometry.vertices.push(v1);
         this.geometry.vertices.push(v2);
         this.geometry.vertices.push(v3);
+        this.geometry.vertices.push(v4);
+        this.geometry.vertices.push(v5);
 
-        this.geometry.faces.push(new THREE.Face3(0, 1, 2, 3));
+        this.geometry.faces.push(new THREE.Face3(0, 1, 2));
+        this.geometry.faces.push(new THREE.Face3(2, 4, 0));
+        this.geometry.faces.push(new THREE.Face3(4, 6, 5));
         this.geometry.computeFaceNormals();
 
         this.mesh = new THREE.Mesh(this.geometry, new THREE.MeshNormalMaterial());
