ae2174f37a5ec1e5d94109e0501f0c5908a12c91
utarsuno <utarsuno@gmail.com>
2018-04-20 14:36:13 -0500

    text2D refactoring 29

--- quasar_site_django/static/js/custom/models/floating_elements/base/text_2d.js
+++ quasar_site_django/static/js/custom/models/floating_elements/base/text_2d.js
@@ -120,6 +120,7 @@ function Text2D(world, width, height, text) {
 
             //l(this.geometry.faceVertexUvs[0]);
 
+            this.geometry.faceVertexUvs[0][0][2].x = this.ratio;
             this.geometry.faceVertexUvs[0][1][1].x = this.ratio;
             this.geometry.faceVertexUvs[0][1][2].x = this.ratio;
 
