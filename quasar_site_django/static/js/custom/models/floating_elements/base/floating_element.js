466489cf7c6d483a43b712c70f963b83c2234491
utarsuno <utarsuno@gmail.com>
2018-04-20 14:39:18 -0500

    text2D refactoring 31

--- quasar_site_django/static/js/custom/models/floating_elements/base/floating_element.js
+++ quasar_site_django/static/js/custom/models/floating_elements/base/floating_element.js
@@ -28,7 +28,8 @@ function FloatingElement(world) {
         let label = new FloatingText2D(this.world, this.height, text);
         //label.set_text_property_right(true);
         label.set_current_foreground_color(COLOR_TEXT_CONSTANT, true);
-        this.add_floating_element([-label.width / 2, -HALF], null, 0, label);
+        //this.add_floating_element([-label.width / 2, -HALF], null, 0, label);
+        this.add_floating_element([0, -HALF], null, 0, label);
         return label;
     };
 
