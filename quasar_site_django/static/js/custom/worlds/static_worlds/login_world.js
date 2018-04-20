d8946f2aa72347f01613c1bbc4b83cb8ed63c5eb
utarsuno <utarsuno@gmail.com>
2018-04-20 14:42:14 -0500

    text2D refactoring 34

--- quasar_site_django/static/js/custom/worlds/static_worlds/login_world.js
+++ quasar_site_django/static/js/custom/worlds/static_worlds/login_world.js
@@ -9,46 +9,11 @@ LoginWorld.prototype = {
     __init__: function() {
         // Inherit.
         World.call(this);
-        //WorldState.call(this, new THREE.Vector3(0, 200, 0), new THREE.Vector3(400, 200, 430), this.custom_world_enter.bind(this), this.custom_world_exit.bind(this));
-        WorldState.call(this, new THREE.Vector3(0, 200, 0), new THREE.Vector3(400, 200, 430), null, null);
+        WorldState.call(this, new THREE.Vector3(0, 200, 0), new THREE.Vector3(400, 200, 430), this.custom_world_enter.bind(this), this.custom_world_exit.bind(this));
         WorldInput.call(this);
     },
 
-    create: function() {
-        let login_wall_position = new THREE.Vector3(550, 95, 300);
-        let login_wall_normal = new THREE.Vector3(-login_wall_position.x, 0, -login_wall_position.z);
-        this.wall_login = new FloatingWall(200, 95, login_wall_position, login_wall_normal, this);
-        this.wall_login.make_base_wall_invisible();
 
-        // Title.
-        let row = this.wall_login.add_row(-1);
-        row.add_text_3D([HALF, null, true], 32, 'Login', true);
-
-        // Username.
-        row = this.wall_login.add_row();
-        this.input_username_login = row.add_input_2D([0, 1], 16);
-        this.input_username_login.add_syntax(TEXT_SYNTAX_USERNAME);
-        this.input_username_login.add_label_left('username:');
-
-        // Password.
-        row = this.wall_login.add_row();
-        this.input_password_login = row.add_input_2D([0, 1], 16);
-        this.input_password_login.add_syntax(TEXT_SYNTAX_PASSWORD);
-        this.input_password_login.add_label_left('password:');
-
-        // Remember username.
-        //row = this.wall_login.add_row();
-        //this.remember_username = row.add_checkbox([0], 16, false, this.remember_username_pressed.bind(this));
-        //this.remember_username.add_label_left('remember username:');
-
-        // Login button.
-        row = this.wall_login.add_row();
-        this.button_login = row.add_button([ONE_FOURTH, THREE_FOURTHS, true], 16, 'login', null);
-
-        this.wall_login.refresh_position_and_look_at();
-    },
-
-    /*
     custom_world_enter: function() {
         if (CURRENT_CLIENT.get_cookie(COOKIE_SHOULD_REMEMBER_USERNAME)) {
             this.remember_username.set_checked_state(true);
@@ -69,7 +34,7 @@ LoginWorld.prototype = {
 
     /*     __   __                  __  ___    __
      |    /  \ / _` | |\ |     /\  /  `  |  | /  \ |\ |
-     |___ \__/ \__> | | \|    /~~\ \__,  |  | \__/ | \| *//*
+     |___ \__/ \__> | | \|    /~~\ \__,  |  | \__/ | \| */
     login_button_pressed: function() {
         this.server_request_login.set_username(this.input_username_login.get_text());
         this.server_request_login.set_password(this.input_password_login.get_text());
@@ -84,7 +49,7 @@ LoginWorld.prototype = {
 
     /*__   __   ___      ___  ___          __   __   __            ___          __  ___    __
      /  ` |__) |__   /\   |  |__      /\  /  ` /  ` /  \ |  | |\ |  |      /\  /  `  |  | /  \ |\ |
-     \__, |  \ |___ /~~\  |  |___    /~~\ \__, \__, \__/ \__/ | \|  |     /~~\ \__,  |  | \__/ | \| *//*
+     \__, |  \ |___ /~~\  |  |___    /~~\ \__, \__, \__/ \__/ | \|  |     /~~\ \__,  |  | \__/ | \| */
     create_account_button_pressed: function() {
         this.server_request_create_account.set_username(this.input_username_create.get_text());
         this.server_request_create_account.set_password(this.input_password_create.get_text());
@@ -100,14 +65,14 @@ LoginWorld.prototype = {
 
     /*__   ___        ___        __   ___  __           __   ___  __                   ___     __   __   ___  __   __   ___  __
      |__) |__   |\/| |__   |\/| |__) |__  |__)    |  | /__` |__  |__) |\ |  /\   |\/| |__     |__) |__) |__  /__` /__` |__  |  \
-     |  \ |___  |  | |___  |  | |__) |___ |  \    \__/ .__/ |___ |  \ | \| /~~\  |  | |___    |    |  \ |___ .__/ .__/ |___ |__/ *//*
+     |  \ |___  |  | |___  |  | |__) |___ |  \    \__/ .__/ |___ |  \ | \| /~~\  |  | |___    |    |  \ |___ .__/ .__/ |___ |__/ */
     remember_username_pressed: function(is_checked) {
         CURRENT_CLIENT.set_cookie(COOKIE_SHOULD_REMEMBER_USERNAME, is_checked);
     },
 
     /*        ___                      __        __
      | |\ | |  |  |  /\  |       |    /  \  /\  |  \
-     | | \| |  |  | /~~\ |___    |___ \__/ /~~\ |__/ *//*
+     | | \| |  |  | /~~\ |___    |___ \__/ /~~\ |__/ */
     create: function() {
         this._create_quasar_title();
         this._create_login_wall();
@@ -213,5 +178,5 @@ LoginWorld.prototype = {
         // Connect the elements together as a form.
         this.form_login = new FormManager([this.input_username_login, this.input_password_login], this.button_login);
     }
-    */
+    
 };
