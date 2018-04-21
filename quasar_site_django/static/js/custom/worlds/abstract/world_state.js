673f9f97c688491b7deb56bea8c9154a9183238f
utarsuno <utarsuno@gmail.com>
2018-04-20 23:40:23 -0500

    transition shader optimization 97

--- quasar_site_django/static/js/custom/worlds/abstract/world_state.js
+++ quasar_site_django/static/js/custom/worlds/abstract/world_state.js
@@ -72,7 +72,7 @@ function WorldState(default_world_enter_position, default_world_enter_look_at, c
         let p = CURRENT_PLAYER.get_position();
         let l = CURRENT_PLAYER.get_direction();
         this.player_exit_position = new THREE.Vector3(p.x, p.y, p.z);
-        this.player_exit_look_at  = new THREE.Vector3(p.x + l.x, p.y + l.y, p.z + lz.);
+        this.player_exit_look_at  = new THREE.Vector3(p.x + l.x, p.y + l.y, p.z + l.z);
     };
 
 }
