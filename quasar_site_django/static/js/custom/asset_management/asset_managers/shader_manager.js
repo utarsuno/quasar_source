26de6051972d65969cf10fd209cb20b86b34da5f
utarsuno <utarsuno@gmail.com>
2018-04-21 13:01:04 -0500

    transition shader optimization 123

--- quasar_site_django/static/js/custom/asset_management/asset_managers/shader_manager.js
+++ quasar_site_django/static/js/custom/asset_management/asset_managers/shader_manager.js
@@ -39,11 +39,19 @@ ShaderManager.prototype = {
 function ShaderMaterialAbstraction(vertex_shader_name, fragment_shader_name) {
     this.shader_vertex   = MANAGER_SHADER.get_shader(vertex_shader_name);
     this.shader_fragment = MANAGER_SHADER.get_shader(fragment_shader_name);
-    //this.uniforms        = [];
+    this.uniforms        = {};
     this.set_uniform = function(k, v) {
-        //this.uniforms[k] = v;
-        //this.shader_material.uniforms[k] = v;
+        this.uniforms[k] = {'value': v};
+    };
+    this.set_uniform_value = function(k, v) {
         this.shader_material.uniforms[k].value = v;
+    };
+    this._get_shader_material = function() {
+        this.shader_material = new THREE.ShaderMaterial({
+            uniforms: this.uniforms,
+            vertexShader: this.shader_vertex,
+            fragmentShader: this.shader_fragment
+        });
     };
 }
 
@@ -55,8 +63,8 @@ ShaderMaterialTransition.prototype = {
     __init__: function() {
         ShaderMaterialAbstraction.call(this, SHADER_TRANSITION_VERTEX, SHADER_TRANSITION_FRAGEMENT);
         this.texture = MANAGER_TEXTURE.get_texture(TEXTURE_GROUP_TRANSITION, TRANSITION_GRID);
+        /*
         this.shader_material = new THREE.ShaderMaterial({
-            /*
             uniforms: {
                 texture_diffuse_new_scene: {
                     value: null
@@ -74,10 +82,10 @@ ShaderMaterialTransition.prototype = {
                     value: this.texture
                 }
             },
-            */
             vertexShader: this.shader_vertex,
             fragmentShader: this.shader_fragment
         });
+        */
 
         //l(this.shader_vertex);
         //l(this.shader_fragment);
@@ -89,28 +97,35 @@ ShaderMaterialTransition.prototype = {
         this._uniform_key_threshold                 = 'threshold';
         this._uniform_key_texture_transition_affect = 'texture_mix';
 
+        this.set_uniform(this._uniform_key_texture_diffuse_new_scene, null);
+        this.set_uniform(this._uniform_key_texture_diffuse_old_scene, null);
+        this.set_uniform(this._uniform_key_mix_ratio, 0.0);
+        this.set_uniform(this._uniform_key_threshold, 0.1);
+        this.set_uniform(this._uniform_key_texture_transition_affect, this.texture);
+        this.shader_material = this._get_shader_material();
+
         this._set_texture_for_transition_affect(this.texture);
         this.set_threshold(0.1);
     },
 
     set_texture_for_old_scene: function(t) {
-        this.set_uniform(this._uniform_key_texture_diffuse_old_scene, t);
+        this.set_uniform_value(this._uniform_key_texture_diffuse_old_scene, t);
     },
 
     set_texture_for_new_scene: function(t) {
-        this.set_uniform(this._uniform_key_texture_diffuse_new_scene, t);
+        this.set_uniform_value(this._uniform_key_texture_diffuse_new_scene, t);
     },
 
     set_mix_ratio: function(r) {
-        this.set_uniform(this._uniform_key_mix_ratio, r);
+        this.set_uniform_value(this._uniform_key_mix_ratio, r);
     },
 
     set_threshold: function(t) {
-        this.set_uniform(this._uniform_key_threshold, t);
+        this.set_uniform_value(this._uniform_key_threshold, t);
     },
 
     _set_texture_for_transition_affect: function(t) {
-        this.set_uniform(this._uniform_key_texture_transition_affect, t);
+        this.set_uniform_value(this._uniform_key_texture_transition_affect, t);
     },
 
     get_shader_material: function() {
