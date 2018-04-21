f38cc1c0be59ab924d0b566c7ef622f1e803edcb
utarsuno <utarsuno@gmail.com>
2018-04-20 22:24:40 -0500

    transition shader optimization 64

--- quasar_site_django/static/js/custom/rendering/world_transition.js
+++ quasar_site_django/static/js/custom/rendering/world_transition.js
@@ -83,9 +83,13 @@ TransitionAffect.prototype = {
 
             //this.renderer_manager.renderer.setClearColor(0x111111);
             //this.renderer_manager.renderer.render(this.new_world.scene, this.renderer_manager.camera, this.fbo_current, true);
-            this.renderer_manager.renderer.render(this.new_world.scene, this.renderer_manager._camera_transition, this.fbo_current, true);
+            if (is_defined(this.old_world)) {
+                this.renderer_manager.renderer.render(this.old_world.scene, this.renderer_manager._camera_transition, this.fbo_current, true);
+            } else {
+                this.renderer_manager.renderer.render(this.new_world.scene, this.renderer_manager._camera_transition, this.fbo_current, true);
+            }
 
-            this.renderer_manager.renderer.render(this.scene, this.camera_ortho, null, true);
+            //this.renderer_manager.renderer.render(this.scene, this.camera_ortho, null, true);
             //this.renderer_manager.renderer.render(this.scene, this.camera_ortho);
         }
 
