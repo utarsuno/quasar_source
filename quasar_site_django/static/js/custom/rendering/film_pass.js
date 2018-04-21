85b970c1464825e45e681c85d85bccda5979a24c
utarsuno <utarsuno@gmail.com>
2018-04-21 13:52:07 -0500

    transition shader optimization 130

--- quasar_site_django/static/js/custom/rendering/film_pass.js
+++ quasar_site_django/static/js/custom/rendering/film_pass.js
@@ -8,22 +8,20 @@
  */
 
 function FilmNoise() {
-    this.__init__();
+    THREE.Pass.call(this);
+    this.shader_material = MANAGER_SHADER.get_shader_material_abstraction(SHADER_MATERIAL_NOISE);
+
+    this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+    this.scene  = new THREE.Scene();
+
+    this.quad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), null);
+    this.quad.frustumCulled = false; // Avoid getting clipped
+    this.scene.add(this.quad);
 }
 
 FilmNoise.prototype = Object.assign(Object.create(THREE.Pass.prototype), {
 
-    __init__: function() {
-        THREE.Pass.call(this);
-        this.shader_material = MANAGER_SHADER.get_shader_material_abstraction(SHADER_MATERIAL_NOISE);
-
-        this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
-        this.scene  = new THREE.Scene();
-
-        this.quad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), null);
-        this.quad.frustumCulled = false; // Avoid getting clipped
-        this.scene.add(this.quad);
-    },
+    constructor: THREE.FilmNoise,
 
     render: function(renderer, writeBuffer, readBuffer, delta, maskActive) {
     	this.shader_material.set_t_diffuse(readBuffer.texture);
