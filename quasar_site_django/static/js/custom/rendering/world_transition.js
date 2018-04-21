b7217ef23410e5ac8a1613dcd5438a5dcb575109
utarsuno <utarsuno@gmail.com>
2018-04-21 12:02:39 -0500

    transition shader optimization 113

--- quasar_site_django/static/js/custom/rendering/world_transition.js
+++ quasar_site_django/static/js/custom/rendering/world_transition.js
@@ -44,12 +44,6 @@ TransitionAffect.prototype = {
 
         this.fake_camera = new THREE.Camera();
         this.fake_scene = new THREE.Scene();
-        //this.renderer_manager.renderer.autoClear = false;
-
-        //l('CAMERA POSITIONS!!');
-        //l(this.renderer_manager._camera_transition.position);
-        //l(this.renderer_manager.camera.position);
-        //l('-------');
 
         this.renderer_manager.renderer.autoClear = false;
         //this.renderer_manager.renderer.render(this.old_world.scene, this.renderer_manager._camera_transition, this.fbo_previous, true);
@@ -62,47 +56,17 @@ TransitionAffect.prototype = {
         //this.transition = THREE.Math.smoothstep(t, 0.4, 0.6);
         //this.transition = t;
         //this.transition = this.elapsed_delta;
-
         this.renderer_manager._transition_shader_material.set_mix_ratio(this.elapsed_delta);
-
-        //this.renderer_manager._transition_shader_material.set_texture_for_old_scene(this.fbo_previous.texture);
-        //this.renderer_manager._transition_shader_material.set_texture_for_new_scene(this.fbo_current.texture);
-
-        //l(this.elapsed_delta);
-        //l(this.renderer_manager._transition_shader_material.shader_material.uniforms);
-        //l(this.renderer_manager._transition_shader_material.shader_material.uniforms['mix_ratio']);
 
         // Prevent render both scenes when it's not necessary
         if (this.elapsed_delta == 0 ) {
             this.renderer_manager.renderer.render(this.fake_scene, this.fake_camera);
         } else if (this.elapsed_delta >= 1) {
-            //this.renderer_manager.in_transition = false;
             this.renderer_manager.renderer.render(this.new_world.scene, this.renderer_manager.camera);
             this.renderer_manager.set_current_scene(this.new_world.scene, this.transition_finished_callback);
         } else {
-            //this.renderer_manager.renderer.render(this.fake_scene, this.fake_camera);
-
-            //this.renderer_manager.renderer.setClearColor(0x111111);
-            //this.renderer_manager.renderer.render(this.new_world.scene, this.renderer_manager.camera, this.fbo_current, true);
-            if (is_defined(this.old_world)) {
-                //this.renderer_manager.renderer.render(this.old_world.scene, this.renderer_manager._camera_transition, this.fbo_current, true);
-                //this.renderer_manager.renderer.render(this.old_world.scene, this.renderer_manager._camera_transition, this.fbo_previous, true);
-            } else {
-                //this.renderer_manager.renderer.render(this.new_world.scene, this.renderer_manager._camera_transition, this.fbo_current, true);
-            }
-
-            //this.renderer_manager.renderer.render(this.new_world.scene, this.renderer_manager._camera_transition, this.fbo_current, true);
-
-
-            //this.renderer_manager.renderer.render(this.fake_scene, this.fake_camera);
-
             this.renderer_manager.renderer.render(this.new_world.scene, this.renderer_manager.camera, this.fbo_current, true);
-            //this.renderer_manager.renderer.render(this.new_world.scene, this.renderer_manager._camera_transition, this.fbo_current, true);
-
             this.renderer_manager.renderer.render(this.scene, this.camera_ortho, null, true);
-
-
-            //this.renderer_manager.renderer.render(this.scene, this.camera_ortho);
         }
 
         //this.elapsed_delta += delta / 2.5;
@@ -153,24 +117,6 @@ function WorldTransition() {
         this._current_transition = new TransitionAffect(previous_world, current_world, this);
         this._current_transition.start(transition_finished_callback);
     };
-
-    /*
-    this.set_current_worldOLD = function(current_world, previous_world, transition_finished_callback, previous_position_and_look_at) {
-        let previous_camera_position = CURRENT_PLAYER.get_position();
-        let previous_camera_look_at  = CURRENT_PLAYER.get_direction();
-
-        let previous_scene = previous_world.scene;
-        let current_scene  = current_world.scene;
-
-        this.camera_transition.position.set(previous_camera_position.x, previous_camera_position.y, previous_camera_position.z);
-        this.camera_transition.lookAt(previous_camera_position.x + previous_camera_look_at.x, previous_camera_position.y + previous_camera_look_at.y, previous_camera_position.z + previous_camera_look_at.z);
-
-        //
-        this.current_transition = this._get_transition_pair(previous_scene, current_scene);
-        this.current_transition.set_size_if_needed(this.current_resize);
-        this.current_transition.start(this.camera, this.camera_transition, transition_finished_callback);
-    };
-    */
 
     // Only used once for displaying the initial login world.
     this.set_current_scene = function(scene, transition_finished_callback) {
