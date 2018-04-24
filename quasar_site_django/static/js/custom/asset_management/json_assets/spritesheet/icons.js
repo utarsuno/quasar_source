0385478476fdfddd7a497a3848e018cc4188b473
utarsuno <utarsuno@gmail.com>
2018-04-24 04:46:51 -0500

    asset managment 47

--- quasar_site_django/static/js/custom/asset_management/json_assets/spritesheet/icons.js
+++ quasar_site_django/static/js/custom/asset_management/json_assets/spritesheet/icons.js
@@ -2,120 +2,119 @@ const JSON_SPRITESHEET = {'frames': [
 
     {
         'filename': 'admin.png',
-        'frame': {'x':1,'y':1,'w':64,'h':64},
+        'frame': {'x':0,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'arrow.png',
-        'frame': {'x':67,'y':1,'w':64,'h':64},
+        'frame': {'x':64,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'calendar.png',
-        'frame': {'x':133,'y':1,'w':64,'h':64},
+        'frame': {'x':128,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'checkmark.png',
-        'frame': {'x':199,'y':1,'w':64,'h':64},
+        'frame': {'x':192,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'click.png',
-        'frame': {'x':265,'y':1,'w':64,'h':64},
+        'frame': {'x':256,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'cross.png',
-        'frame': {'x':331,'y':1,'w':64,'h':64},
+        'frame': {'x':320,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'cursor.png',
-        'frame': {'x':397,'y':1,'w':64,'h':64},
+        'frame': {'x':384,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'delete.png',
-        'frame': {'x':463,'y':1,'w':64,'h':64},
+        'frame': {'x':448,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'disabled.png',
-        'frame': {'x':529,'y':1,'w':64,'h':64},
+        'frame': {'x':512,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'drag.png',
-        'frame': {'x':595,'y':1,'w':64,'h':64},
+        'frame': {'x':576,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'exit.png',
-        'frame': {'x':661,'y':1,'w':64,'h':64},
+        'frame': {'x':640,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'expand.png',
-        'frame': {'x':727,'y':1,'w':64,'h':64},
+        'frame': {'x':704,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'folder.png',
-        'frame': {'x':793,'y':1,'w':64,'h':64},
+        'frame': {'x':768,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'gears.png',
-        'frame': {'x':859,'y':1,'w':64,'h':64},
+        'frame': {'x':832,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'home.png',
-        'frame': {'x':925,'y':1,'w':64,'h':64},
+        'frame': {'x':896,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'horizontal.png',
-        'frame': {'x':991,'y':1,'w':64,'h':64},
+        'frame': {'x':960,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'locked.png',
-        'frame': {'x':1057,'y':1,'w':64,'h':64},
+        'frame': {'x':1024,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'picture.png',
-        'frame': {'x':1123,'y':1,'w':64,'h':64},
+        'frame': {'x':1088,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'planet.png',
-        'frame': {'x':1189,'y':1,'w':64,'h':64},
+        'frame': {'x':1152,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'teleport.png',
-        'frame': {'x':1255,'y':1,'w':64,'h':64},
+        'frame': {'x':1216,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'text.png',
-        'frame': {'x':1321,'y':1,'w':64,'h':64},
+        'frame': {'x':1280,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'unlocked.png',
-        'frame': {'x':1387,'y':1,'w':64,'h':64},
+        'frame': {'x':1344,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'vertical.png',
-        'frame': {'x':1453,'y':1,'w':64,'h':64},
+        'frame': {'x':1408,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'video.png',
-        'frame': {'x':1519,'y':1,'w':64,'h':64},
+        'frame': {'x':1472,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'warning.png',
-        'frame': {'x':1585,'y':1,'w':64,'h':64},
+        'frame': {'x':1536,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'wrench.png',
-        'frame': {'x':1651,'y':1,'w':64,'h':64},
+        'frame': {'x':1600,'y':0,'w':64,'h':64},
     },
     {
         'filename': 'writting.png',
-        'frame': {'x':1717,'y':1,'w':64,'h':64},
+        'frame': {'x':1664,'y':0,'w':64,'h':64},
     }],
 'meta': {
     'app': 'https://www.codeandweb.com/texturepacker',
     'version': '1.0',
     'image': 'icons.png',
     'format': 'RGBA8888',
-    'size': {'w':1782,'h':66},
+    'size': {'w':1728,'h':64},
     'scale': '1',
-    'smartupdate': '$TexturePacker:SmartUpdate:9e98b50437c56d518b5910a956697884:6fcfb2a1c2a146bf8290501ef346117f:b5c5bd7252d2048fecbb1bda1760f9ef$'
+    'smartupdate': '$TexturePacker:SmartUpdate:43a48b6c4e8b64e21c00c5d7dd3ad80b:6fcfb2a1c2a146bf8290501ef346117f:b5c5bd7252d2048fecbb1bda1760f9ef$'
 }
-
 };
