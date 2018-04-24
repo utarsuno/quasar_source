0e327ab591f38dd24ca1428f269b1756e25cfc36
utarsuno <utarsuno@gmail.com>
2018-04-23 20:36:16 -0500

    heap optimization 19

--- quasar_site_django/static/js/custom/worlds/dynamic_content_manager.js
+++ quasar_site_django/static/js/custom/worlds/dynamic_content_manager.js
@@ -20,16 +20,19 @@ function DynamicContentManager() {
     this.get_all_user_created_entity_ids_from_all_worlds = function() {
         let entity_ids = [];
         let home_world_entity_ids = this.get_all_user_created_entity_ids_from_world(this.world_home);
-        for (let e = 0; e < home_world_entity_ids.length; e++) {
+        let e;
+        for (e = 0; e < home_world_entity_ids.length; e++) {
             entity_ids.push(home_world_entity_ids[e]);
         }
 
         // Go through each dynamic world now.
-        for (let id in this.dynamic_worlds) {
+        let id;
+        for (id in this.dynamic_worlds) {
             if (this.dynamic_worlds.hasOwnProperty(id)) {
                 let dynamic_world_entity_ids = this.get_all_user_created_entity_ids_from_world(this.dynamic_worlds[id]);
 
-                for (let e = 0; e < dynamic_world_entity_ids.length; e++) {
+                let e;
+                for (e = 0; e < dynamic_world_entity_ids.length; e++) {
                     entity_ids.push(dynamic_world_entity_ids[e]);
                 }
             }
@@ -44,9 +47,11 @@ function DynamicContentManager() {
         let entity_groups = world.entity_groups;
         let user_created_entity_ids = [];
 
-        for (let eg = 0; eg < entity_groups.length; eg++) {
+        let eg;
+        for (eg = 0; eg < entity_groups.length; eg++) {
             let entity_group_entity_ids = entity_groups[eg].list_of_entity_ids;
-            for (let e = 0; e < entity_group_entity_ids.length; e++) {
+            let e;
+            for (e = 0; e < entity_group_entity_ids.length; e++) {
                 user_created_entity_ids.push(entity_group_entity_ids[e]);
             }
         }
@@ -58,7 +63,8 @@ function DynamicContentManager() {
         // At this point in time all entities and dynamic content has been loaded.
         this.link_entity_notifications_for_month_views_for_world(this.world_home);
 
-        for (let id in this.dynamic_worlds) {
+        let id;
+        for (id in this.dynamic_worlds) {
             if (this.dynamic_worlds.hasOwnProperty(id)) {
                 let dynamic_world = this.dynamic_worlds[id];
                 this.link_entity_notifications_for_month_views_for_world(dynamic_world);
@@ -73,7 +79,8 @@ function DynamicContentManager() {
     this.link_entity_notifications_for_month_views_for_world = function(world) {
         let month_view_walls = world.month_view_walls;
 
-        for (let mv = 0; mv < month_view_walls.length; mv++) {
+        let mv;
+        for (mv = 0; mv < month_view_walls.length; mv++) {
             month_view_walls[mv].link_all_from_entity_events();
         }
     };
