a4a5bad979bb88af711c84bafa6953b3e3a7b1d2
utarsuno <utarsuno@gmail.com>
2018-04-27 16:42:44 -0500

    mesh merging 10

--- quasar_site_django/static/js/custom/models/discrete_models/hexagon_grid/hexagon_grid.js
+++ quasar_site_django/static/js/custom/models/discrete_models/hexagon_grid/hexagon_grid.js
@@ -12,17 +12,21 @@ HexagonGrid.prototype = {
 
     create: function() {
         this.hexagon_geometry = new THREE.CircleBufferGeometry(64, 6);
-        this.single_geometry = new THREE.Mesh();
+        this.single_geometry = new THREE.Geometry();
+
+        this.materails = [];
 
         let l;
         for (l = 0; l < this.number_of_layers; l++) {
             let c = new THREE.MeshToonMaterial({color: Math.random() * 0xffffff});
             let m = new THREE.Mesh(this.hexagon_geometry, c);
+            this.materails.push(m);
             this.single_geometry.merge(m);
         }
 
         this.object3D = new THREE.Object3D();
-        this.object3D.add(this.single_geometry);
+        this.single_mesh = new THREE.Mesh(this.single_geometry, this.materails);
+        this.object3D.add(this.single_mesh);
     }
 
 };
