41ced083d8b563e9ed270e6444e6fa1620f3d9b2
utarsuno <utarsuno@gmail.com>
2018-04-28 19:53:20 -0500

    heap/object optimizations 19

--- quasar_site_django/static/js/custom/models/floating_walls/floating_wall/fake_floating_wall.js
+++ quasar_site_django/static/js/custom/models/floating_walls/floating_wall/fake_floating_wall.js
@@ -8,5 +8,7 @@ FakeFloatingWall.prototype = {
 
     __init__: function (width, height, position, normal, world) {
         FloatingWallAbstract.call(this, width, height, position, normal, world);
+        this.manual_visibility = true;
+        this.object3D.userData.manual_visibility = true;
     }
 };
