35b408b5b8fb9e9f19c2ce6571d82240cbf2db27
utarsuno <utarsuno@gmail.com>
2018-04-30 23:08:23 -0500

    initial loading optimizations 05

--- quasar_site_django/static/js/custom/models/discrete_models/keyboard/keyboard_model.js
+++ quasar_site_django/static/js/custom/models/discrete_models/keyboard/keyboard_model.js
@@ -11,85 +11,12 @@ KeyboardModel.prototype = {
         Attachmentable.call(this, world);
         this.key_depth = 10;
         this.face_size = 30;
-
-        this.first_row = [
-            ['esc', this.face_size, [-200, 100, 'Press to release mouse control.']],
-            ['1', this.face_size],
-            ['2', this.face_size],
-            ['3', this.face_size],
-            ['4', this.face_size],
-            ['5', this.face_size],
-            ['6', this.face_size],
-            ['7', this.face_size],
-            ['8', this.face_size],
-            ['9', this.face_size],
-            ['0', this.face_size],
-            ['-', this.face_size],
-            ['+', this.face_size],
-            ['backsapce', this.face_size * 2],
-        ];
-        this.second_row = [
-            ['tab', this.face_size * 1.5],
-            ['q', this.face_size],
-            ['w', this.face_size],
-            ['e', this.face_size],
-            ['r', this.face_size],
-            ['t', this.face_size],
-            ['y', this.face_size],
-            ['u', this.face_size],
-            ['i', this.face_size],
-            ['o', this.face_size],
-            ['p', this.face_size],
-            ['p', this.face_size],
-            ['[', this.face_size],
-            [']', this.face_size],
-            ['\\', this.face_size * 1.5],
-        ];
-        this.third_row = [
-            ['caps', this.face_size * 1.6],
-            ['a', this.face_size],
-            ['s', this.face_size],
-            ['d', this.face_size],
-            ['f', this.face_size],
-            ['g', this.face_size],
-            ['h', this.face_size],
-            ['j', this.face_size],
-            ['k', this.face_size],
-            ['l', this.face_size],
-            [';', this.face_size],
-            ['\'', this.face_size],
-            ['enter', this.face_size * 2]
-        ];
-        this.fourth_row = [
-            ['shift', this.face_size * 2],
-            ['z', this.face_size],
-            ['x', this.face_size],
-            ['c', this.face_size],
-            ['v', this.face_size],
-            ['b', this.face_size],
-            ['n', this.face_size],
-            ['m', this.face_size],
-            [',', this.face_size],
-            ['.', this.face_size],
-            ['/', this.face_size],
-            ['shift', this.face_size * 2]
-        ];
-        this.fifth_row = [
-            ['ctrl', this.face_size],
-            [' ', this.face_size],
-            ['alt', this.face_size],
-            ['space', this.face_size * 9],
-            ['alt', this.face_size],
-            [' ', this.face_size],
-            ['alt', this.face_size],
-            ['ctrl', this.face_size],
-        ];
-
-        this.single_geometry = new THREE.Geometry();
     },
 
-    _fast_create: function() {
+    create: function() {
         this._create_cache();
+
+        this.single_geometry = new THREE.Geometry();
 
         let row_height = this.key_depth * 2 + this.face_size;
         let y1 = -row_height;
@@ -168,52 +95,25 @@ KeyboardModel.prototype = {
         this._clear_cache();
     },
 
-    create: function() {
-        this._fast_create();
-    },
-
-    createOLD: function() {
-        this._create_cache();
-
-        let row_height = this.key_depth * 2 + this.face_size;
-        this._create_row(this.first_row, 0);
-        this._create_row(this.second_row, -row_height);
-        this._create_row(this.third_row, -row_height * 2);
-        this._create_row(this.fourth_row, -row_height * 3);
-        this._create_row(this.fifth_row, -row_height * 4);
-
-        //this.mesh = new THREE.Mesh(this.geometry, new THREE.MeshNormalMaterial());
-        //this.mesh = new THREE.Mesh(this.geometry);
-
-        this.single_mesh = new THREE.Mesh(this.single_geometry, new THREE.MeshNormalMaterial());
-        this.object3D.add(this.single_mesh);
-
-        this._clear_cache();
-    },
-
-    _create_row: function(row, y_offset) {
-        let total_x_offset = 0;
-        let x;
-        for (x = 0; x < row.length; x++) {
-            let k = row[x];
-            this._create_key(k[0], k[1], total_x_offset, y_offset, k[2]);
-            total_x_offset += k[1] + this.key_depth * 2;
-        }
-    },
-
     _create_cache: function() {
-        // Cache the standard key size.
-        this.cached_key_geometry = this._get_key_geometry(this.face_size);
+        // Standard key sizes cache.
+        this.cached_key_geometry           = this._get_key_geometry(this.face_size);
+        this.cahced_key_geometry_plus_half = this._get_key_geometry(this.face_size * 2);
+        this.cahced_key_geometry_double    = this._get_key_geometry(this.face_size * 2);
     },
 
     _clear_cache: function() {
         this.cached_key_geometry.dispose();
+        this.cahced_key_geometry_plus_half.dispose();
+        this.cahced_key_geometry_double.dispose();
         this.cached_key_geometry = undefined;
-        this.first_row = undefined;
-        this.second_row = undefined;
-        this.third_row = undefined;
-        this.fourth_row = undefined;
-        this.fifth_row = undefined;
+        this.cahced_key_geometry_plus_half = undefined;
+        this.cahced_key_geometry_double = undefined;
+        //this.first_row = undefined;
+        //this.second_row = undefined;
+        //this.third_row = undefined;
+        //this.fourth_row = undefined;
+        //this.fifth_row = undefined;
     },
 
     _get_key_geometry: function(key_width) {
@@ -321,3 +221,108 @@ KeyboardModel.prototype = {
     }
 
 };
+
+// FOR REFERENCE :
+/*
+        this.first_row = [
+            ['esc', this.face_size, [-200, 100, 'Press to release mouse control.']],
+            ['1', this.face_size],
+            ['2', this.face_size],
+            ['3', this.face_size],
+            ['4', this.face_size],
+            ['5', this.face_size],
+            ['6', this.face_size],
+            ['7', this.face_size],
+            ['8', this.face_size],
+            ['9', this.face_size],
+            ['0', this.face_size],
+            ['-', this.face_size],
+            ['+', this.face_size],
+            ['backsapce', this.face_size * 2],
+        ];
+        this.second_row = [
+            ['tab', this.face_size * 1.5],
+            ['q', this.face_size],
+            ['w', this.face_size],
+            ['e', this.face_size],
+            ['r', this.face_size],
+            ['t', this.face_size],
+            ['y', this.face_size],
+            ['u', this.face_size],
+            ['i', this.face_size],
+            ['o', this.face_size],
+            ['p', this.face_size],
+            ['p', this.face_size],
+            ['[', this.face_size],
+            [']', this.face_size],
+            ['\\', this.face_size * 1.5],
+        ];
+        this.third_row = [
+            ['caps', this.face_size * 1.6],
+            ['a', this.face_size],
+            ['s', this.face_size],
+            ['d', this.face_size],
+            ['f', this.face_size],
+            ['g', this.face_size],
+            ['h', this.face_size],
+            ['j', this.face_size],
+            ['k', this.face_size],
+            ['l', this.face_size],
+            [';', this.face_size],
+            ['\'', this.face_size],
+            ['enter', this.face_size * 2]
+        ];
+        this.fourth_row = [
+            ['shift', this.face_size * 2],
+            ['z', this.face_size],
+            ['x', this.face_size],
+            ['c', this.face_size],
+            ['v', this.face_size],
+            ['b', this.face_size],
+            ['n', this.face_size],
+            ['m', this.face_size],
+            [',', this.face_size],
+            ['.', this.face_size],
+            ['/', this.face_size],
+            ['shift', this.face_size * 2]
+        ];
+        this.fifth_row = [
+            ['ctrl', this.face_size],
+            [' ', this.face_size],
+            ['alt', this.face_size],
+            ['space', this.face_size * 9],
+            ['alt', this.face_size],
+            [' ', this.face_size],
+            ['alt', this.face_size],
+            ['ctrl', this.face_size],
+        ];
+
+    createOLD: function() {
+        this._create_cache();
+
+        let row_height = this.key_depth * 2 + this.face_size;
+        this._create_row(this.first_row, 0);
+        this._create_row(this.second_row, -row_height);
+        this._create_row(this.third_row, -row_height * 2);
+        this._create_row(this.fourth_row, -row_height * 3);
+        this._create_row(this.fifth_row, -row_height * 4);
+
+        //this.mesh = new THREE.Mesh(this.geometry, new THREE.MeshNormalMaterial());
+        //this.mesh = new THREE.Mesh(this.geometry);
+
+        this.single_mesh = new THREE.Mesh(this.single_geometry, new THREE.MeshNormalMaterial());
+        this.object3D.add(this.single_mesh);
+
+        this._clear_cache();
+    },
+
+    _create_row: function(row, y_offset) {
+        let total_x_offset = 0;
+        let x;
+        for (x = 0; x < row.length; x++) {
+            let k = row[x];
+            this._create_key(k[0], k[1], total_x_offset, y_offset, k[2]);
+            total_x_offset += k[1] + this.key_depth * 2;
+        }
+    },
+ */
