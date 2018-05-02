243f047da7af41debceb9f2f582f8c57573ad3fa
utarsuno <utarsuno@gmail.com>
2018-05-01 19:55:16 -0500

    raycaster optimizations 01

--- quasar_site_django/static/js/custom/worlds/abstract/world_base.js
+++ quasar_site_django/static/js/custom/worlds/abstract/world_base.js
@@ -77,7 +77,7 @@ function World(world_entity) {
         for (i = 0; i < this.interactive_objects.length; i++) {
             // The true parameter indicates recursive search.
             //let intersections = this.raycaster.intersectObject(this.interactive_objects[i].object3D, true);
-            this.raycaster.intersectObject(this.interactive_objects[i].object3D, true, this._intersections);
+            this.raycaster.intersectObject(this.interactive_objects[i].object3D, false, this._intersections);
 
             let d;
             for (d = 0; d < this._intersections.length; d++) {
