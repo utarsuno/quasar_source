dc2ff0717c0151efa0e0c429ae85ccb984b4984d
utarsuno <utarsuno@gmail.com>
2018-05-06 01:04:28 -0500

    general optimizations/clean-ups 00

--- quasar_site_django/static/js/custom/controls/user_input/input_manager.js
+++ quasar_site_django/static/js/custom/controls/user_input/input_manager.js
@@ -153,9 +153,9 @@ InputManager.prototype = {
     },
 
     on_mouse_move: function(event) {
-        let movement_x = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-        let movement_y = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-        this._mouse_movement(movement_x, movement_y);
+        //let movement_x = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+        //let movement_y = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+        this._mouse_movement(event.movementX || event.mozMovementX || event.webkitMovementX || 0, event.movementY || event.mozMovementY || event.webkitMovementY || 0);
 
         // TODO : Not currently used for now.
         /*
