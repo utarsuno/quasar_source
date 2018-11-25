36714dbebcd7d0ad5f2fa099535860c245f25edc
utarsuno@gmail.com <utarsuno@gmail.com>
2018-11-25 01:21:37 -0600

    server script update

--- scripts/server/run_front_end_only.sh
+++ scripts/server/run_front_end_only.sh
@@ -10,10 +10,16 @@ cd ${DIR}/../..;
 
 # Script Variables ---------------------------------------------------------------------------
 SCRIPT_NAME="Run Nexus Local"
+IS_RUNNING="$(docker ps | grep quasar_source_front_end)"
 # --------------------------------------------------------------------------------------------
 
 start_script
 
-nohup scripts/local/front_end_only/run.sh &
+if [ -z "$(IS_RUNNING)" ]; then
+  echo "Nexus Local is already running!\n"
+else
+  echo "Nexus Local is spawning!\n"
+  nohup scripts/local/front_end_only/run.sh &
+fi
 
 finish_script_success
