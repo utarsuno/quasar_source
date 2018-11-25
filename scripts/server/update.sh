1e5d4bd3bc98a91dd40d03a4032aee31a585663b
utarsuno@gmail.com <utarsuno@gmail.com>
2018-11-25 01:56:01 -0600

    server update script feature testing

--- scripts/server/update.sh
+++ scripts/server/update.sh
@@ -10,6 +10,7 @@ cd ${DIR}/../..;
 
 # Script Variables ---------------------------------------------------------------------------
 SCRIPT_NAME="Update Server Code"
+BASE_DIR=$(DIR)/../../
 # --------------------------------------------------------------------------------------------
 
 start_script
@@ -18,9 +19,21 @@ git fetch
 git pull
 
 if [ -d "${DIR}/../local" ]; then
-  # Control will enter here if $DIRECTORY exists.
-  printf "Local directory exists!"
-  sudo rm -rf ${DIR}/../local
+  printf "Removing un-needed directory!\n"
+  sudo rm -rf ${BASE_DIR}local
 fi
+
+if [ -d "${BASE_DIR}theoretical_and_depreciated_code" ]; then
+  printf "Removing un-needed directory!\n"
+  sudo rm -rf ${BASE_DIR}theoretical_and_depreciated_code
+fi
+
+if [ -d "${BASE_DIR}generated_output" ]; then
+  mkdir -p ${BASE_DIR}generated_output/local/code_manager
+  mkdir -p ${BASE_DIR}generated_output/web_assets
+  #mkdir -p ${BASE_DIR}generated_output/third_party_libraries/three_js
+fi
+
+
 
 finish_script_success
