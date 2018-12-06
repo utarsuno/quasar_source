84228968d60eedab7563c5669801aa4863d16856
utarsuno@gmail.com <utarsuno@gmail.com>
2018-12-05 19:06:36 -0600

    add cache clear script

---
+++ scripts/local/nuke_build_cache.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Location of this script.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+# Load utility functions.
+source ${DIR}/../../scripts/utilities/script_utilities.sh
+source ${DIR}/../../scripts/utilities/docker_utilities.sh
+# Go to project base directory.
+cd ${DIR}/../..;
+
+# Script Variables ---------------------------------------------------------------------------
+SCRIPT_NAME="Front End Only"
+# Script Variables ---------------------------------------------------------------------------
+
+start_script
+
+sudo rm ./generated_output/local/code_manager/db.sqlite
+
+finish_script_success
